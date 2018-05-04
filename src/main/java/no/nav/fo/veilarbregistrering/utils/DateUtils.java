cc6e0106816f9d04f079638e3dc9e5248fe2d3f3
Ola <ola.galde@nav.no>
2018-05-04 13:59:13 +0200

    FO-953: Fjerner ubrukte metoder

--- src/main/java/no/nav/fo/veilarbregistrering/utils/DateUtils.java
+++ src/main/java/no/nav/fo/veilarbregistrering/utils/DateUtils.java
@@ -1,50 +1,18 @@
 package no.nav.fo.veilarbregistrering.utils;
 
-import lombok.SneakyThrows;
-
-import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 import java.sql.Timestamp;
-import java.time.*;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
 import java.util.GregorianCalendar;
 import java.util.Optional;
 
-import static java.util.Optional.ofNullable;
-
 public class DateUtils {
-
-
-    private static final DatatypeFactory DATATYPE_FACTORY = getDatatypeFactory();
-
-    @SneakyThrows
-    private static DatatypeFactory getDatatypeFactory() {
-        return DatatypeFactory.newInstance();
-    }
-
-    public static XMLGregorianCalendar xmlCalendar(Date date) {
-        return ofNullable(date).map(d->{
-            GregorianCalendar cal = new GregorianCalendar();
-            cal.setTime(date);
-            return DATATYPE_FACTORY.newXMLGregorianCalendar(cal);
-        }).orElse(null);
-    }
-
-    public static Date getDate(XMLGregorianCalendar xmlGregorianCalendar){
-        return ofNullable(xmlGregorianCalendar)
-                .map(XMLGregorianCalendar::toGregorianCalendar)
-                .map(GregorianCalendar::getTime)
-                .orElse(null);
-    }
 
     public static Timestamp toTimeStamp(String utc) {
         return Timestamp.from(ZonedDateTime.parse(utc).toInstant());
     }
 
-    public static ZonedDateTime toZonedDateTime(Timestamp endretTimestamp) {
-        LocalDateTime localDateTime = endretTimestamp.toLocalDateTime();
-        return ZonedDateTime.of(localDateTime, ZoneId.systemDefault()).withZoneSameInstant(ZoneOffset.UTC);
-    }
 
     public static LocalDate xmlGregorianCalendarToLocalDate(XMLGregorianCalendar inaktiveringsdato) {
         return Optional.ofNullable(inaktiveringsdato)
@@ -55,12 +23,5 @@ public class DateUtils {
 
     public static boolean erDatoEldreEnnEllerLikAar(LocalDate dagensDato, LocalDate dato, int aar) {
         return FnrUtils.antallAarSidenDato(dato, dagensDato) >= aar;
-    }
-
-    @SneakyThrows
-    public static XMLGregorianCalendar now() {
-        DatatypeFactory factory = DatatypeFactory.newInstance();
-        GregorianCalendar calendar = new GregorianCalendar();
-        return factory.newXMLGregorianCalendar(calendar);
     }
 }
