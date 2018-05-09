6771faa72a013bf5e4c0b2794e8576de7614cfea
Ola <ola.galde@nav.no>
2018-05-09 12:33:12 +0200

    FO-953: Omskriver navn på property for aktøridtjeneste og oidc-redirect-url.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,9 @@ import no.nav.apiapp.ApiApp;
 import no.nav.fo.veilarbregistrering.config.ApplicationConfig;
 
 import static java.lang.System.setProperty;
+import static no.nav.brukerdialog.security.Constants.OIDC_REDIRECT_URL_PROPERTY_NAME;
+import static no.nav.dialogarena.aktor.AktorConfig.AKTOER_ENDPOINT_URL;
+import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
 
 public class Main {
 
@@ -13,6 +16,8 @@ public class Main {
         setProperty("https.proxyHost", "webproxy-utvikler.nav.no");
         setProperty("https.proxyPort", "8088");
 
+        setProperty(AKTOER_ENDPOINT_URL, getRequiredProperty("AKTOER_V2_ENDPOINTURL"));
+        setProperty(OIDC_REDIRECT_URL_PROPERTY_NAME, getRequiredProperty("VEILARBLOGIN_REDIRECT_URL_URL"));
 
         ApiApp.startApp(ApplicationConfig.class, args);
     }
