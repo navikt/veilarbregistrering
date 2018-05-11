76eb0e87ea8842d083f62d177f52c27589c61dee
Ola <ola.galde@nav.no>
2018-05-11 13:43:44 +0200

    KAIZEN-0: Diverse konfig-fikser for å få appen til å deploye og kjøre på nais.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,8 @@ import static no.nav.brukerdialog.security.Constants.OIDC_REDIRECT_URL_PROPERTY_
 import static no.nav.dialogarena.aktor.AktorConfig.AKTOER_ENDPOINT_URL;
 import static no.nav.sbl.dialogarena.common.abac.pep.CredentialConstants.SYSTEMUSER_PASSWORD;
 import static no.nav.sbl.dialogarena.common.abac.pep.CredentialConstants.SYSTEMUSER_USERNAME;
+import static no.nav.sbl.dialogarena.common.abac.pep.service.AbacService.ABAC_ENDPOINT_URL_PROPERTY_NAME;
+import static no.nav.sbl.dialogarena.common.cxf.StsSecurityConstants.STS_URL_KEY;
 import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
 
 public class Main {
@@ -18,10 +20,14 @@ public class Main {
         setProperty("https.proxyHost", "webproxy-utvikler.nav.no");
         setProperty("https.proxyPort", "8088");
 
-        setProperty(AKTOER_ENDPOINT_URL, getRequiredProperty("AKTOER_V2_ENDPOINTURL"));
-        setProperty(OIDC_REDIRECT_URL_PROPERTY_NAME, getRequiredProperty("VEILARBLOGIN_REDIRECT_URL_URL"));
         setProperty(SYSTEMUSER_USERNAME, getRequiredProperty("SRVVEILARBREGISTRERING_USERNAME"));
         setProperty(SYSTEMUSER_PASSWORD, getRequiredProperty("SRVVEILARBREGISTRERING_PASSWORD"));
+
+        setProperty(AKTOER_ENDPOINT_URL, getRequiredProperty("AKTOER_V2_ENDPOINTURL"));
+        setProperty(OIDC_REDIRECT_URL_PROPERTY_NAME, getRequiredProperty("VEILARBLOGIN_REDIRECT_URL_URL"));
+        setProperty(ABAC_ENDPOINT_URL_PROPERTY_NAME, getRequiredProperty("ABAC_PDP_ENDPOINT_URL"));
+        setProperty(STS_URL_KEY, getRequiredProperty("SECURITYTOKENSERVICE_URL"));
+
 
         ApiApp.startApp(ApplicationConfig.class, args);
     }
