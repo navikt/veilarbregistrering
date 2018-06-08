4cef21e405e001e8da0ec1998fb4697a6844db43
Ola <ola.galde@nav.no>
2018-06-08 13:24:30 +0200

    FO-1084: Legger på mock-metode for aktiverBruker.

--- src/test/java/no/nav/fo/veilarbregistrering/mock/OppfolgingClientMock.java
+++ src/test/java/no/nav/fo/veilarbregistrering/mock/OppfolgingClientMock.java
@@ -1,6 +1,8 @@
 package no.nav.fo.veilarbregistrering.mock;
 
 import no.nav.fo.veilarbregistrering.domain.AktivStatus;
+import no.nav.fo.veilarbregistrering.domain.AktiverBrukerData;
+import no.nav.fo.veilarbregistrering.domain.BrukerRegistrering;
 import no.nav.fo.veilarbregistrering.httpclient.OppfolgingClient;
 
 public class OppfolgingClientMock extends OppfolgingClient {
@@ -9,9 +11,14 @@ public class OppfolgingClientMock extends OppfolgingClient {
         super(null);
     }
 
-
+    @Override
     public AktivStatus hentOppfolgingsstatus(String fnr) {
         return new AktivStatus().withInaktiveringDato(null).withUnderOppfolging(false).withAktiv(false);
+    }
+
+    @Override
+    public BrukerRegistrering aktiverBruker(AktiverBrukerData fnr) {
+        return new BrukerRegistrering();
     }
 
 }
