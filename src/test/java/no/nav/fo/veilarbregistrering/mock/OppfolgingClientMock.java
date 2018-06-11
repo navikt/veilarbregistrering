263ffcbe102bc66d7354ca6ebdfad5756e0f415a
Ola <ola.galde@nav.no>
2018-06-11 14:22:43 +0200

    KAIZEN-0: Utvider mock

--- src/test/java/no/nav/fo/veilarbregistrering/mock/OppfolgingClientMock.java
+++ src/test/java/no/nav/fo/veilarbregistrering/mock/OppfolgingClientMock.java
@@ -1,8 +1,12 @@
 package no.nav.fo.veilarbregistrering.mock;
 
+import no.nav.apiapp.feil.FeilDTO;
 import no.nav.fo.veilarbregistrering.domain.AktivStatus;
 import no.nav.fo.veilarbregistrering.domain.AktiverBrukerData;
 import no.nav.fo.veilarbregistrering.httpclient.OppfolgingClient;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
 
 public class OppfolgingClientMock extends OppfolgingClient {
 
@@ -17,6 +21,15 @@ public class OppfolgingClientMock extends OppfolgingClient {
 
     @Override
     public void aktiverBruker(AktiverBrukerData fnr) {
+        //sendException("BRUKER_ER_UKJENT");
+        //sendException("BRUKER_KAN_IKKE_REAKTIVERES");
+        //sendException("BRUKER_ER_DOD_UTVANDRET_ELLER_FORSVUNNET");
+        //sendException("BRUKER_MANGLER_ARBEIDSTILLATELSE");
+    }
+
+    private void sendException(String feilType) {
+        FeilDTO feilDTO = new FeilDTO("1", feilType, new FeilDTO.Detaljer(feilType, "", ""));
+        throw new WebApplicationException(Response.serverError().entity(feilDTO).build());
     }
 
 }
