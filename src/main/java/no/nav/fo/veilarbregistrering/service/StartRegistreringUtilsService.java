ab72d536cbf355ee8eb76164a51c071211869b07
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2018-06-27 12:00:07 +0200

    FO-1100 Slett ubrukt metode

--- src/main/java/no/nav/fo/veilarbregistrering/service/StartRegistreringUtilsService.java
+++ src/main/java/no/nav/fo/veilarbregistrering/service/StartRegistreringUtilsService.java
@@ -2,15 +2,12 @@ package no.nav.fo.veilarbregistrering.service;
 
 
 import lombok.extern.slf4j.Slf4j;
-import no.nav.fo.veilarbregistrering.domain.AktivStatus;
 import no.nav.fo.veilarbregistrering.domain.Arbeidsforhold;
 import no.nav.fo.veilarbregistrering.utils.ArbeidsforholdUtils;
-import no.nav.fo.veilarbregistrering.utils.FnrUtils;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.function.Supplier;
 
 import static no.nav.fo.veilarbregistrering.utils.DateUtils.erDatoEldreEnnEllerLikAar;
@@ -21,16 +18,6 @@ public class StartRegistreringUtilsService {
     private static final int ANTALL_AAR_ISERV = 2;
     public static final String MIN_ALDER_AUTOMATISK_REGISTRERING = "MIN_ALDER_AUTOMATISK_REGISTRERING";
     public static final String MAX_ALDER_AUTOMATISK_REGISTRERING = "MAKS_ALDER_AUTOMATISK_REGISTRERING";
-
-    public boolean oppfyllerKravOmAutomatiskRegistrering(String fnr, Supplier<List<Arbeidsforhold>> arbeidsforholdSupplier,
-                                                         AktivStatus aktivStatus, LocalDate dagensDato) {
-        LocalDate fodselsdato = FnrUtils.utledFodselsdatoForFnr(fnr);
-        int alder = FnrUtils.antallAarSidenDato(fodselsdato, dagensDato);
-        LocalDate inaktiveringsdato = Optional.of(aktivStatus).map(AktivStatus::getInaktiveringDato).orElse(null);
-
-        return oppfyllerBetingelseOmInaktivitet(dagensDato, inaktiveringsdato) &&
-                ArbeidsforholdUtils.oppfyllerBetingelseOmArbeidserfaring(arbeidsforholdSupplier.get(), dagensDato);
-    }
 
     public boolean oppfyllerBetingelseOmArbeidserfaring(
             Supplier<List<Arbeidsforhold>> arbeidsforholdSupplier,
