b77605edf3a1f3a32022b9fe23df97a3d30ab7e5
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2018-08-31 16:10:21 +0200

    FO-1467 Legg til noen tester

--- src/test/java/no/nav/fo/veilarbregistrering/service/ValideringUtilsTest.java
+++ src/test/java/no/nav/fo/veilarbregistrering/service/ValideringUtilsTest.java
@@ -1,15 +1,43 @@
 package no.nav.fo.veilarbregistrering.service;
 
 import no.nav.fo.veilarbregistrering.domain.BrukerRegistrering;
+import no.nav.fo.veilarbregistrering.domain.besvarelse.DinSituasjonSvar;
+import no.nav.fo.veilarbregistrering.domain.besvarelse.SisteStillingSvar;
 import org.junit.jupiter.api.Test;
 
 import static no.nav.fo.veilarbregistrering.service.ValideringUtils.validerBrukerRegistrering;
-import static no.nav.fo.veilarbregistrering.utils.TestUtils.gyldigBesvarelse;
-import static no.nav.fo.veilarbregistrering.utils.TestUtils.gyldigBrukerRegistrering;
-import static no.nav.fo.veilarbregistrering.utils.TestUtils.gyldigStilling;
+import static no.nav.fo.veilarbregistrering.utils.TestUtils.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValideringUtilsTest {
+
+    @Test
+    void skalHaIngenYrkesbakgrunnHvisViVetAtBrukerIkkeHarHattJobb() {
+        BrukerRegistrering brukerRegistrering = gyldigBrukerRegistreringUtenJobb().setSisteStilling(gyldigStilling());
+        assertThrows(RuntimeException.class, () -> validerBrukerRegistrering(brukerRegistrering));
+    }
+
+    @Test
+    void skalIkkeSvarePaaSpmOmSisteStillingHvisViVetAtBrukerIkkeHarHattJobb() {
+        BrukerRegistrering brukerRegistrering = gyldigBrukerRegistreringUtenJobb().setBesvarelse(
+                gyldigBesvarelseUtenJobb().setSisteStilling(SisteStillingSvar.HAR_IKKE_HATT_JOBB)
+        );
+        assertThrows(RuntimeException.class, () -> validerBrukerRegistrering(brukerRegistrering));
+    }
+
+    @Test
+    void skalIkkeSvarePaaSpmOmSisteStillingHvisViAlleredeVetAtBrukerHarHattJobb() {
+        BrukerRegistrering brukerRegistrering = gyldigBrukerRegistrering().setBesvarelse(gyldigBesvarelse()
+                .setDinSituasjon(DinSituasjonSvar.MISTET_JOBBEN)
+                .setSisteStilling(SisteStillingSvar.INGEN_SVAR)
+        );
+        validerBrukerRegistrering(brukerRegistrering);
+
+        brukerRegistrering.setBesvarelse(brukerRegistrering.getBesvarelse()
+                .setSisteStilling(SisteStillingSvar.HAR_HATT_JOBB)
+        );
+        assertThrows(RuntimeException.class, () -> validerBrukerRegistrering(brukerRegistrering));
+    }
 
     @Test
     void valideringSkalGodkjenneGyldigeObjekter() {
