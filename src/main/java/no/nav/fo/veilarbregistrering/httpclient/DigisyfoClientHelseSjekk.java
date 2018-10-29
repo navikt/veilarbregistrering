2e1a68f0c409970e8f45d8875ed736b02877fdbe
Joselito Poblete <joselito.poblete@gmail.com>
2018-10-29 12:16:03 +0100

    [KAIZEN-0] La til api-key i helsesjekk digisyfo

--- src/main/java/no/nav/fo/veilarbregistrering/httpclient/DigisyfoClientHelseSjekk.java
+++ src/main/java/no/nav/fo/veilarbregistrering/httpclient/DigisyfoClientHelseSjekk.java
@@ -4,6 +4,7 @@ import no.nav.apiapp.selftest.Helsesjekk;
 import no.nav.apiapp.selftest.HelsesjekkMetadata;
 import no.nav.fo.veilarbregistrering.config.RemoteFeatureConfig;
 
+import static no.nav.fo.veilarbregistrering.httpclient.DigisyfoClient.API_KEY_FASIT_KEY;
 import static no.nav.fo.veilarbregistrering.httpclient.DigisyfoClient.DIGISYFO_BASE_URL_PROPERTY_NAME;
 import static no.nav.sbl.rest.RestUtils.withClient;
 import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
@@ -12,9 +13,11 @@ public class DigisyfoClientHelseSjekk implements Helsesjekk {
 
     private String digiSyfoPingUrl = getRequiredProperty(DIGISYFO_BASE_URL_PROPERTY_NAME) + "internal/isAlive";
     private RemoteFeatureConfig.SykemeldtRegistreringFeature sykemeldtRegistreringFeature;
+    private static String apiKey = "";
 
     public DigisyfoClientHelseSjekk(RemoteFeatureConfig.SykemeldtRegistreringFeature sykemeldtRegistreringFeature) {
         this.sykemeldtRegistreringFeature = sykemeldtRegistreringFeature;
+        this.apiKey = getRequiredProperty(API_KEY_FASIT_KEY);
     }
 
     @Override
@@ -25,6 +28,7 @@ public class DigisyfoClientHelseSjekk implements Helsesjekk {
         int status = withClient(c ->
                 c.target(digiSyfoPingUrl)
                         .request()
+                        .header("x-nav-apiKey", apiKey)
                         .get()
                         .getStatus());
         if (!(status >= 200 && status < 300)) {
