a5447d509a600722449e9e06f5937b4996b60383
Joselito Poblete <joselito.poblete@gmail.com>
2018-12-04 15:31:12 +0100

    [FO-1697] Endre url til cluster istedenfor nais

--- src/main/java/no/nav/fo/veilarbregistrering/httpclient/SykmeldtInfoClient.java
+++ src/main/java/no/nav/fo/veilarbregistrering/httpclient/SykmeldtInfoClient.java
@@ -15,7 +15,7 @@ import static no.nav.sbl.rest.RestUtils.withClient;
 @Slf4j
 public class SykmeldtInfoClient extends BaseClient {
 
-    public static final String INFOTRYGDAPI_URL_PROPERTY_NAME = "http://infotrygd-sykepenger.default.svc.nais.local";
+    public static final String INFOTRYGDAPI_URL_PROPERTY_NAME = "http://infotrygd-sykepenger.default.svc.cluster.local";
 
     @Inject
     public SykmeldtInfoClient(Provider<HttpServletRequest> httpServletRequestProvider) {
