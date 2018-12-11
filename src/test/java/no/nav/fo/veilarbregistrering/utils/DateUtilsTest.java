1d2aefbd30adc732d00bc3f9d0eb94bf44286a69
Izis Frøyhov <izis.froyhov@nav.no>
2018-12-11 10:03:36 +0100

    FO-1697: tester

--- src/test/java/no/nav/fo/veilarbregistrering/utils/DateUtilsTest.java
+++ src/test/java/no/nav/fo/veilarbregistrering/utils/DateUtilsTest.java
@@ -27,42 +27,56 @@ public class DateUtilsTest {
     }
 
     @Test
-    public void skalVaereSykmeldtOverEllerLik39Uker()  {
+    public void skalVaereSykmeldtOverEllerLik39Uker() {
         String maksDato = "2018-10-01";
         LocalDate dagenDato = LocalDate.of(2018, Month.JUNE, 26);
-        assertEquals(DateUtils.beregnSykmeldtOver39uker(maksDato, dagenDato), true);
+        assertEquals(true, DateUtils.beregnSykmeldtOver39uker(maksDato, dagenDato));
     }
 
     @Test
-    public void skalIkkeVaereSykmeldtOver39Uker()  {
+    public void skalVaereSykmeldtAkkurat52Uker() {
+        String maksDato = "2018-12-11";
+        LocalDate dagenDato = LocalDate.of(2017, Month.DECEMBER, 12);
+        assertEquals(true, DateUtils.beregnSykmeldtOver39uker(maksDato, dagenDato));
+    }
+
+    @Test
+    public void skalVaereSykmeldtNesten52Uker() {
+        String maksDato = "2018-12-11";
+        LocalDate dagenDato = LocalDate.of(2017, Month.DECEMBER, 15);
+        assertEquals(true, DateUtils.beregnSykmeldtOver39uker(maksDato, dagenDato));
+    }
+
+    @Test
+    public void skalIkkeVaereSykmeldtOver39Uker() {
         String maksDato = "2018-10-01";
         LocalDate dagenDato = LocalDate.of(2018, Month.APRIL, 9);
-        assertEquals(DateUtils.beregnSykmeldtOver39uker(maksDato, dagenDato), false);
+        assertEquals(false, DateUtils.beregnSykmeldtOver39uker(maksDato, dagenDato));
     }
 
     @Test
-    public void skalIkkeVaereSykmeldtOver39UkerNarMaksDatoErUnderDagensDato()  {
+    public void skalIkkeVaereSykmeldtOver39UkerNarMaksDatoErUnderDagensDato() {
         String maksDato = "2018-10-01";
         LocalDate dagenDato = LocalDate.of(2019, Month.APRIL, 9);
-        assertEquals(DateUtils.beregnSykmeldtOver39uker(maksDato, dagenDato), false);
+        assertEquals(false, DateUtils.beregnSykmeldtOver39uker(maksDato, dagenDato));
     }
 
     @Test
-    public void skalHandtereNullVedBeregnSykmeldtOver39uker()  {
+    public void skalHandtereNullVedBeregnSykmeldtOver39uker() {
         String maksDato = null;
         LocalDate dagenDato = LocalDate.of(2019, Month.APRIL, 9);
-        assertEquals(DateUtils.beregnSykmeldtOver39uker(maksDato, dagenDato), false);
+        assertEquals(false, DateUtils.beregnSykmeldtOver39uker(maksDato, dagenDato));
     }
 
     @Test
     public void skalFormatterMaksDato() {
         String maksDato = "2018-10-01";
-        assertEquals(DateUtils.formatterMaksDato(maksDato), "01.10.2018");
+        assertEquals("01.10.2018", DateUtils.formatterMaksDato(maksDato));
     }
 
     @Test
     public void skalHandtereNullVedFormatteringAvMaksDato() {
         String maksDato = null;
-        assertEquals(DateUtils.formatterMaksDato(maksDato), "");
+        assertEquals("", DateUtils.formatterMaksDato(maksDato));
     }
 }
