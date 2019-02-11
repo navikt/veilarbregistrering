66c33bc9ae308a1fb00876142f5cb564baecda13
Joselito Poblete <joselito.poblete@gmail.com>
2019-02-11 15:34:25 +0100

    KAIZEN-0 Legge til self-test mot infotrygd

---
+++ src/main/java/no/nav/fo/veilarbregistrering/httpclient/SykmeldtInfoClientHelseSjekk.java
@@ -0,0 +1,36 @@
+package no.nav.fo.veilarbregistrering.httpclient;
+
+import no.nav.apiapp.selftest.Helsesjekk;
+import no.nav.apiapp.selftest.HelsesjekkMetadata;
+
+import static no.nav.fo.veilarbregistrering.httpclient.SykmeldtInfoClient.INFOTRYGDAPI_URL_PROPERTY_NAME;
+import static no.nav.sbl.rest.RestUtils.withClient;
+import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
+
+public class SykmeldtInfoClientHelseSjekk implements Helsesjekk {
+
+    private String infotrygdFOPingUrl = getRequiredProperty(INFOTRYGDAPI_URL_PROPERTY_NAME) + "/rest/internal/isAlive";
+
+    @Override
+    public void helsesjekk() throws Throwable {
+        int status = withClient(c ->
+                c.target(infotrygdFOPingUrl)
+                        .request()
+                        .get()
+                        .getStatus());
+        if (!(status >= 200 && status < 300)) {
+            throw new IllegalStateException("HTTP status " + status);
+        }
+    }
+
+    @Override
+    public HelsesjekkMetadata getMetadata() {
+        return new HelsesjekkMetadata(
+                "infotrygd-fo",
+                infotrygdFOPingUrl,
+                "Ping av infotrygd-fo",
+                true
+        );
+    }
+
+}
