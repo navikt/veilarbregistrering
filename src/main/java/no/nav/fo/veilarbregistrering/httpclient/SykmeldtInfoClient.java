2b893320059e05d856993a8b3d906a29f0908641
Alexander Gård <alexander.gard@nav.no>
2019-02-18 08:59:24 +0100

    FO-1876 Byttet til bruk av konstanter

--- src/main/java/no/nav/fo/veilarbregistrering/httpclient/SykmeldtInfoClient.java
+++ src/main/java/no/nav/fo/veilarbregistrering/httpclient/SykmeldtInfoClient.java
@@ -2,22 +2,17 @@ package no.nav.fo.veilarbregistrering.httpclient;
 
 import lombok.extern.slf4j.Slf4j;
 import no.nav.brukerdialog.security.jaspic.TokenLocator;
-import no.nav.brukerdialog.security.oidc.provider.IssoOidcProvider;
-import no.nav.common.auth.SubjectHandler;
 import no.nav.fo.veilarbregistrering.domain.InfotrygdData;
-import no.nav.fo.veilarbregistrering.domain.SykmeldtInfoData;
 import no.nav.fo.veilarbregistrering.utils.AutentiseringUtils;
 import no.nav.sbl.rest.RestUtils;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.InternalServerErrorException;
 
-import java.util.Enumeration;
-
-import static io.vavr.collection.Array.*;
+import static no.nav.brukerdialog.security.Constants.ID_TOKEN_COOKIE_NAME;
+import static no.nav.brukerdialog.security.Constants.REFRESH_TOKEN_COOKIE_NAME;
 import static javax.ws.rs.core.HttpHeaders.COOKIE;
 import static no.nav.brukerdialog.security.oidc.provider.AzureADB2CProvider.AZUREADB2C_OIDC_COOKIE_NAME;
 import static no.nav.sbl.rest.RestUtils.withClient;
@@ -27,7 +22,7 @@ public class SykmeldtInfoClient extends BaseClient {
 
     public static final String INFOTRYGDAPI_URL_PROPERTY_NAME = "http://infotrygd-fo.default.svc.nais.local";
 
-    private final TokenLocator issoTokenLocator = new TokenLocator("ID_token", "refresh_token");
+    private final TokenLocator issoTokenLocator = new TokenLocator(ID_TOKEN_COOKIE_NAME, REFRESH_TOKEN_COOKIE_NAME);
 
     private final TokenLocator essoTokenLocator = new TokenLocator(AZUREADB2C_OIDC_COOKIE_NAME, null);
 
