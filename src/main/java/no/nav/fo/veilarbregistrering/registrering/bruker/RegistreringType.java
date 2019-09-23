d905e9272b65aa37c44a9d1850ea44c364e05ba3
Terje Heen <terje.heen@nav.no>
2019-09-23 23:17:23 +0200

    [KAIZEN-0] Flytter factory method til RegistreringType.
    Flytter samtidig logikk ut fra Util som på sikt skal inn i en Gateway for å skille mellom intern og ekstern modell.

--- src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/RegistreringType.java
+++ src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/RegistreringType.java
@@ -1,5 +1,30 @@
 package no.nav.fo.veilarbregistrering.registrering.bruker;
 
+import no.nav.fo.veilarbregistrering.oppfolging.adapter.OppfolgingStatusData;
+import no.nav.fo.veilarbregistrering.sykemelding.SykmeldtInfoData;
+
+import static java.util.Optional.ofNullable;
+
 public enum RegistreringType {
-    REAKTIVERING, SPERRET, ALLEREDE_REGISTRERT, SYKMELDT_REGISTRERING, ORDINAER_REGISTRERING
+    REAKTIVERING, SPERRET, ALLEREDE_REGISTRERT, SYKMELDT_REGISTRERING, ORDINAER_REGISTRERING;
+
+    protected static RegistreringType beregnRegistreringType(OppfolgingStatusData oppfolgingStatusData, SykmeldtInfoData sykeforloepMetaData) {
+        if (oppfolgingStatusData.isUnderOppfolging() && !ofNullable(oppfolgingStatusData.getKanReaktiveres()).orElse(false)) {
+            return ALLEREDE_REGISTRERT;
+        } else if (ofNullable(oppfolgingStatusData.getKanReaktiveres()).orElse(false)) {
+            return REAKTIVERING;
+        } else if (ofNullable(oppfolgingStatusData.erSykmeldtMedArbeidsgiver).orElse(false)
+                && erSykmeldtMedArbeidsgiverOver39Uker(sykeforloepMetaData)) {
+            return SYKMELDT_REGISTRERING;
+        } else if (ofNullable(oppfolgingStatusData.erSykmeldtMedArbeidsgiver).orElse(false)
+                && !erSykmeldtMedArbeidsgiverOver39Uker(sykeforloepMetaData)) {
+            return SPERRET;
+        } else {
+            return ORDINAER_REGISTRERING;
+        }
+    }
+
+    private static boolean erSykmeldtMedArbeidsgiverOver39Uker(SykmeldtInfoData sykeforloepMetaData) {
+        return sykeforloepMetaData != null && sykeforloepMetaData.erArbeidsrettetOppfolgingSykmeldtInngangAktiv;
+    }
 }
--- src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/StartRegistreringUtils.java
+++ src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/StartRegistreringUtils.java
@@ -2,21 +2,13 @@ package no.nav.fo.veilarbregistrering.registrering.bruker;
 
 import no.nav.fo.veilarbregistrering.arbeidsforhold.Arbeidsforhold;
 import no.nav.fo.veilarbregistrering.arbeidsforhold.ArbeidsforholdUtils;
-import no.nav.fo.veilarbregistrering.oppfolging.adapter.OppfolgingStatusData;
-import no.nav.fo.veilarbregistrering.oppfolging.adapter.SykmeldtBrukerType;
 import no.nav.fo.veilarbregistrering.profilering.Innsatsgruppe;
 import no.nav.fo.veilarbregistrering.profilering.Profilering;
 import no.nav.fo.veilarbregistrering.registrering.bruker.besvarelse.*;
-import no.nav.fo.veilarbregistrering.sykemelding.SykmeldtInfoData;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.function.Supplier;
-
-import static java.util.Optional.ofNullable;
-import static no.nav.fo.veilarbregistrering.oppfolging.adapter.SykmeldtBrukerType.SKAL_TIL_NY_ARBEIDSGIVER;
-import static no.nav.fo.veilarbregistrering.oppfolging.adapter.SykmeldtBrukerType.SKAL_TIL_SAMME_ARBEIDSGIVER;
-import static no.nav.fo.veilarbregistrering.registrering.bruker.RegistreringType.*;
 
 public class StartRegistreringUtils {
 
@@ -25,26 +17,6 @@ public class StartRegistreringUtils {
             LocalDate dagensDato
     ) {
         return ArbeidsforholdUtils.oppfyllerBetingelseOmArbeidserfaring(arbeidsforholdSupplier.get(), dagensDato);
-    }
-
-    protected static RegistreringType beregnRegistreringType(OppfolgingStatusData oppfolgingStatusData, SykmeldtInfoData sykeforloepMetaData) {
-        if (oppfolgingStatusData.isUnderOppfolging() && !ofNullable(oppfolgingStatusData.getKanReaktiveres()).orElse(false)) {
-            return ALLEREDE_REGISTRERT;
-        } else if (ofNullable(oppfolgingStatusData.getKanReaktiveres()).orElse(false)) {
-            return REAKTIVERING;
-        } else if (ofNullable(oppfolgingStatusData.erSykmeldtMedArbeidsgiver).orElse(false)
-                && erSykmeldtMedArbeidsgiverOver39Uker(sykeforloepMetaData)) {
-            return SYKMELDT_REGISTRERING;
-        } else if (ofNullable(oppfolgingStatusData.erSykmeldtMedArbeidsgiver).orElse(false)
-                && !erSykmeldtMedArbeidsgiverOver39Uker(sykeforloepMetaData)) {
-            return SPERRET;
-        } else {
-            return ORDINAER_REGISTRERING;
-        }
-    }
-
-    private static boolean erSykmeldtMedArbeidsgiverOver39Uker(SykmeldtInfoData sykeforloepMetaData) {
-        return sykeforloepMetaData != null && sykeforloepMetaData.erArbeidsrettetOppfolgingSykmeldtInngangAktiv;
     }
 
     public Profilering profilerBruker(
@@ -88,16 +60,4 @@ public class StartRegistreringUtils {
                 && AndreForholdSvar.NEI.equals(besvarelse.getAndreForhold());
     }
 
-    public SykmeldtBrukerType finnSykmeldtBrukerType(SykmeldtRegistrering sykmeldtRegistrering) {
-        FremtidigSituasjonSvar fremtidigSituasjon = sykmeldtRegistrering.getBesvarelse().getFremtidigSituasjon();
-        if  (fremtidigSituasjon == FremtidigSituasjonSvar.SAMME_ARBEIDSGIVER
-                || fremtidigSituasjon == FremtidigSituasjonSvar.SAMME_ARBEIDSGIVER_NY_STILLING
-                || fremtidigSituasjon == FremtidigSituasjonSvar.INGEN_PASSER
-        ) {
-            return SKAL_TIL_SAMME_ARBEIDSGIVER;
-        } else {
-            return SKAL_TIL_NY_ARBEIDSGIVER;
-        }
-
-    }
 }
