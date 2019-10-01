379ecf872e865937b72c13aeb2255fbea304dc4a
Terje Heen <terje.heen@nav.no>
2019-10-01 15:42:23 +0200

    [KAIZEN-0] Skiller ut logikk knyttet til maksdato til egen klasse.

---
+++ src/main/java/no/nav/fo/veilarbregistrering/sykemelding/Maksdato.java
@@ -0,0 +1,32 @@
+package no.nav.fo.veilarbregistrering.sykemelding;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Maksdato {
+
+    private final String maksdato;
+
+    private Maksdato(String maksdato) {
+        this.maksdato = maksdato;
+    }
+
+    static Maksdato of(String maksdato) {
+        return new Maksdato(maksdato);
+    }
+
+    boolean beregnSykmeldtMellom39Og52Uker(LocalDate dagenDato) {
+        if (maksdato == null) {
+            return false;
+        }
+        LocalDate dato = LocalDate.parse(maksdato);
+        long GJENSTAENDE_UKER = 13;
+
+        return ChronoUnit.WEEKS.between(dagenDato, dato) >= 0 &&
+                ChronoUnit.WEEKS.between(dagenDato, dato) <= GJENSTAENDE_UKER;
+    }
+
+    String asString() {
+        return maksdato;
+    }
+}
