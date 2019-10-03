1dcb9df8fabdf76671ff8ad9fd78dd85c127bf92
Terje Heen <terje.heen@nav.no>
2019-10-03 22:36:15 +0200

    [KAIZEN-0] Sletter metoder som ikke er i bruk.

--- src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/ValideringUtils.java
+++ src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/ValideringUtils.java
@@ -90,39 +90,12 @@ public class ValideringUtils {
                 || besvarelse.getAndreForhold() == null;
     }
 
-    private static boolean gyldigBesvarelseFremtidigSituasjonLoep1(SykmeldtRegistrering bruker) {
-        Besvarelse besvarelse = bruker.getBesvarelse();
-        return besvarelse != null
-                && besvarelse.getFremtidigSituasjon() != null
-                && besvarelse.getTilbakeIArbeid() != null
-                && besvarelse.getUtdanning() == null
-                && besvarelse.getUtdanningGodkjent() == null
-                && besvarelse.getUtdanningBestatt() == null
-                && besvarelse.getAndreForhold() == null;
-    }
-    private static boolean gyldigBesvarelseFremtidigSituasjonLoep234(SykmeldtRegistrering bruker) {
-        Besvarelse besvarelse = bruker.getBesvarelse();
-        return besvarelse != null
-                && besvarelse.getFremtidigSituasjon() == null
-                && besvarelse.getTilbakeIArbeid() == null
-                && besvarelse.getUtdanning() != null
-                && besvarelse.getUtdanningGodkjent() != null
-                && besvarelse.getUtdanningBestatt() != null
-                && besvarelse.getAndreForhold() != null;
-    }
-
     private static boolean brukerHarYrkesbakgrunn(OrdinaerBrukerRegistrering bruker) {
         return !bruker.getSisteStilling().equals(Stilling.ingenYrkesbakgrunn());
     }
 
     private static void assertBothTrueOrBothFalse(boolean value1, boolean value2) {
         assertTrue(value1 == value2);
-    }
-
-    private static void assertBothTrueOrThrowException(boolean value1, boolean value2) {
-        if (value1 != value2) {
-            throw new RuntimeException("Registreringsinformasjonen er ugyldig.");
-        }
     }
 
     private static void assertTrue(boolean value) {
