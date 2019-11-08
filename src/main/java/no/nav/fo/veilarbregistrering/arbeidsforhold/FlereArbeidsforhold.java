3233bf08359e082808dfae5dae8d92f28981f400
Terje Heen <terje.heen@nav.no>
2019-11-08 22:21:44 +0100

    [KAIZEN-0] Flytter logikk fra Utils -> FlereArbeidsforhold
    Flytter logikken fra Utils til domeneklassen. Funksjonaliteten var knyttet til FlereArbeidsforhold, så ved å flytte metoden kunne den bruke sine egne data.

--- src/main/java/no/nav/fo/veilarbregistrering/arbeidsforhold/FlereArbeidsforhold.java
+++ src/main/java/no/nav/fo/veilarbregistrering/arbeidsforhold/FlereArbeidsforhold.java
@@ -11,6 +11,10 @@ import static java.util.Comparator.nullsLast;
 
 public class FlereArbeidsforhold {
 
+    static int antallMnd = 12;
+    static int minAntallMndSammenhengendeJobb = 6;
+    static int dagIMnd = 1;
+    
     private final List<Arbeidsforhold> flereArbeidsforhold;
 
     private FlereArbeidsforhold(List<Arbeidsforhold> flereArbeidsforhold) {
@@ -19,6 +23,29 @@ public class FlereArbeidsforhold {
 
     public static FlereArbeidsforhold of(List<Arbeidsforhold> flereArbeidsforhold) {
         return new FlereArbeidsforhold(flereArbeidsforhold != null ? flereArbeidsforhold : Collections.emptyList());
+    }
+
+    /**
+     * En bruker som har jobbet sammenhengende i seks av de siste tolv månedene oppfyller betingelsen om arbeidserfaring
+     */
+    public boolean harJobbetSammenhengendeSeksAvTolvSisteManeder(LocalDate dagensDato) {
+
+        int antallSammenhengendeMandeder = 0;
+        int mndFraDagensMnd = 0;
+        LocalDate innevaerendeMnd = LocalDate.of(dagensDato.getYear(), dagensDato.getMonthValue(), dagIMnd);
+
+        while (antallSammenhengendeMandeder < minAntallMndSammenhengendeJobb && mndFraDagensMnd < antallMnd) {
+
+            if (harArbeidsforholdPaaDato(innevaerendeMnd)) {
+                antallSammenhengendeMandeder += 1;
+            } else {
+                antallSammenhengendeMandeder = 0;
+            }
+
+            innevaerendeMnd = innevaerendeMnd.minusMonths(1);
+            mndFraDagensMnd += 1;
+        }
+        return antallSammenhengendeMandeder >= minAntallMndSammenhengendeJobb;
     }
 
     public Arbeidsforhold siste() {
