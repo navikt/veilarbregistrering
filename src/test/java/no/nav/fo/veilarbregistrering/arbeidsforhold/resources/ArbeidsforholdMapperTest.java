be685979ba3836c03469a6b56c1b8eba31392a2b
Terje Heen <terje.heen@nav.no>
2019-11-22 13:00:37 +0100

    [KAIZEN-0] Arbeidsforhold for dto-klasser for REST
    Arbeidsforhold eksponerte den interne datamodellen rett ut. Vi ønsker å ha et tydeligere skille mellom intern og ekstern modell. Det gjør at vi lettere og tryggere kan gjøre endringer internt, som ikke vil knekke den ytre.

---
+++ src/test/java/no/nav/fo/veilarbregistrering/arbeidsforhold/resources/ArbeidsforholdMapperTest.java
@@ -0,0 +1,31 @@
+package no.nav.fo.veilarbregistrering.arbeidsforhold.resources;
+
+import no.nav.fo.veilarbregistrering.arbeidsforhold.Arbeidsforhold;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class ArbeidsforholdMapperTest {
+
+    @Test
+    public void skalMappeAlleVerdier() {
+        LocalDate fom = LocalDate.of(2017,12,1);
+        LocalDate tom = LocalDate.of(2017,12,30);
+        Arbeidsforhold arbeidsforhold = new Arbeidsforhold()
+                .setFom(fom)
+                .setTom(tom)
+                .setStyrk("STK")
+                .setArbeidsgiverOrgnummer("453542352");
+
+        ArbeidsforholdDto arbeidsforholdDto = ArbeidsforholdMapper.map(arbeidsforhold);
+
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.assertThat(arbeidsforholdDto.getArbeidsgiverOrgnummer()).isEqualTo("453542352");
+        softAssertions.assertThat(arbeidsforholdDto.getStyrk()).isEqualTo("STK");
+        softAssertions.assertThat(arbeidsforholdDto.getFom()).isEqualTo(fom);
+        softAssertions.assertThat(arbeidsforholdDto.getTom()).isEqualTo(tom);
+
+        softAssertions.assertAll();
+    }
+}
