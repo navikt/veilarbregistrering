f5621b1d55f7da0878f0e49cc863b58322abc350
Terje Heen <terje.heen@nav.no>
2019-12-20 10:26:21 +0100

    [KAIZEN-0] Henter ut Oracle creds fra Vault

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 
 import static java.lang.System.setProperty;
 import static no.nav.dialogarena.aktor.AktorConfig.AKTOER_ENDPOINT_URL;
+import static no.nav.fo.veilarbregistrering.config.DatabaseConfig.VEILARBREGISTRERINGDB_PASSWORD;
+import static no.nav.fo.veilarbregistrering.config.DatabaseConfig.VEILARBREGISTRERINGDB_USERNAME;
 import static no.nav.sbl.dialogarena.common.abac.pep.service.AbacServiceConfig.ABAC_ENDPOINT_URL_PROPERTY_NAME;
 import static no.nav.sbl.dialogarena.common.cxf.StsSecurityConstants.STS_URL_KEY;
 import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
@@ -21,6 +23,11 @@ public class Main {
         LOG.info("Benytter serviceuser_creds -> USERNAME: " + serviceuser_creds.username);
         setProperty("SRVVEILARBREGISTRERING_USERNAME", serviceuser_creds.username);
         setProperty("SRVVEILARBREGISTRERING_PASSWORD", serviceuser_creds.password);
+
+        NaisUtils.Credentials oracle_creds_creds = NaisUtils.getCredentials("oracle_creds");
+        LOG.info("Benytter oracle_creds_creds -> USERNAME: " + oracle_creds_creds.username);
+        setProperty(VEILARBREGISTRERINGDB_USERNAME, oracle_creds_creds.username);
+        setProperty(VEILARBREGISTRERINGDB_PASSWORD, oracle_creds_creds.password);
 
         setProperty(AKTOER_ENDPOINT_URL, getRequiredProperty("AKTOER_V2_ENDPOINTURL"));
         setProperty(ABAC_ENDPOINT_URL_PROPERTY_NAME, getRequiredProperty("ABAC_PDP_ENDPOINT_URL"));
