0e92ffe25a75257395881f40ab3783c50aaf0a3d
Terje Heen <terje.heen@nav.no>
2020-02-10 10:48:20 +0100

    Forenkler Optional-logikk i repository
    Optional-logikken var skrevet etter mønsteret, isPresent ? A : B, i stedet for map(a -> b).orElse(null).

--- src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/db/SykmeldtRegistreringMapper.java
+++ src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/db/SykmeldtRegistreringMapper.java
@@ -5,44 +5,30 @@ import no.nav.fo.veilarbregistrering.registrering.bruker.SykmeldtRegistrering;
 import no.nav.fo.veilarbregistrering.besvarelse.*;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import static java.util.Optional.ofNullable;
+import static no.nav.fo.veilarbregistrering.registrering.bruker.db.BrukerRegistreringRepositoryImpl.*;
 
 class SykmeldtRegistreringMapper {
 
     @SneakyThrows
     static SykmeldtRegistrering map(ResultSet rs) {
         return new SykmeldtRegistrering()
-                .setId(rs.getLong(BrukerRegistreringRepositoryImpl.SYKMELDT_REGISTRERING_ID))
-                .setOpprettetDato(rs.getTimestamp(BrukerRegistreringRepositoryImpl.OPPRETTET_DATO).toLocalDateTime())
-                .setTeksterForBesvarelse(BrukerRegistreringRepositoryImpl.tilTeksterForBesvarelse(rs.getString(BrukerRegistreringRepositoryImpl.TEKSTER_FOR_BESVARELSE)))
-                .setBesvarelse(new Besvarelse()
-                        .setFremtidigSituasjon(
-                                ofNullable(rs.getString(BrukerRegistreringRepositoryImpl.FREMTIDIG_SITUASJON)).isPresent()
-                                        ? FremtidigSituasjonSvar.valueOf(rs.getString(BrukerRegistreringRepositoryImpl.FREMTIDIG_SITUASJON))
-                                        : null
-                        )
-                        .setTilbakeIArbeid(
-                                ofNullable(rs.getString(BrukerRegistreringRepositoryImpl.TILBAKE_ETTER_52_UKER)).isPresent()
-                                        ? TilbakeIArbeidSvar.valueOf(rs.getString(BrukerRegistreringRepositoryImpl.TILBAKE_ETTER_52_UKER))
-                                        : null
-                        )
-                        .setUtdanning(UtdanningUtils.mapTilUtdanning(rs.getString(BrukerRegistreringRepositoryImpl.NUS_KODE)))
-                        .setUtdanningBestatt(
-                                ofNullable(rs.getString(BrukerRegistreringRepositoryImpl.UTDANNING_BESTATT)).isPresent()
-                                        ? UtdanningBestattSvar.valueOf(rs.getString(BrukerRegistreringRepositoryImpl.UTDANNING_BESTATT))
-                                        : null
-                        )
-                        .setUtdanningGodkjent(
-                                ofNullable(rs.getString(BrukerRegistreringRepositoryImpl.UTDANNING_GODKJENT_NORGE)).isPresent()
-                                        ? UtdanningGodkjentSvar.valueOf(rs.getString(BrukerRegistreringRepositoryImpl.UTDANNING_GODKJENT_NORGE))
-                                        : null
-                        )
-                        .setAndreForhold(
-                                ofNullable(rs.getString(BrukerRegistreringRepositoryImpl.ANDRE_UTFORDRINGER)).isPresent()
-                                        ? AndreForholdSvar.valueOf(rs.getString(BrukerRegistreringRepositoryImpl.ANDRE_UTFORDRINGER))
-                                        : null
-                        )
-                );
+                .setId(rs.getLong(SYKMELDT_REGISTRERING_ID))
+                .setOpprettetDato(rs.getTimestamp(OPPRETTET_DATO).toLocalDateTime())
+                .setTeksterForBesvarelse(BrukerRegistreringRepositoryImpl.tilTeksterForBesvarelse(rs.getString(TEKSTER_FOR_BESVARELSE)))
+                .setBesvarelse(besvarelse(rs));
     }
+
+    private static Besvarelse besvarelse(ResultSet rs) throws SQLException {
+        return new Besvarelse()
+                .setFremtidigSituasjon(ofNullable(rs.getString(FREMTIDIG_SITUASJON)).map(FremtidigSituasjonSvar::valueOf).orElse(null))
+                .setTilbakeIArbeid(ofNullable(rs.getString(TILBAKE_ETTER_52_UKER)).map(TilbakeIArbeidSvar::valueOf).orElse(null))
+                .setUtdanning(UtdanningUtils.mapTilUtdanning(rs.getString(BrukerRegistreringRepositoryImpl.NUS_KODE)))
+                .setUtdanningBestatt(ofNullable(rs.getString(UTDANNING_BESTATT)).map(UtdanningBestattSvar::valueOf).orElse(null))
+                .setUtdanningGodkjent(ofNullable(rs.getString(UTDANNING_GODKJENT_NORGE)).map(UtdanningGodkjentSvar::valueOf).orElse(null))
+                .setAndreForhold(ofNullable(rs.getString(ANDRE_UTFORDRINGER)).map(AndreForholdSvar::valueOf).orElse(null));
+    }
+
 }
