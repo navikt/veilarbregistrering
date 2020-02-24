aadaf0b28a40d5d3c91a17db3fa752d70d95f4ee
Terje Heen <terje.heen@nav.no>
2020-02-24 11:48:37 +0100

    Tar i bruk BrukerIntern for oppfølging

--- src/main/java/no/nav/fo/veilarbregistrering/oppfolging/OppfolgingGateway.java
+++ src/main/java/no/nav/fo/veilarbregistrering/oppfolging/OppfolgingGateway.java
@@ -1,15 +1,16 @@
 package no.nav.fo.veilarbregistrering.oppfolging;
 
+import no.nav.fo.veilarbregistrering.bruker.Foedselsnummer;
 import no.nav.fo.veilarbregistrering.profilering.Innsatsgruppe;
 import no.nav.fo.veilarbregistrering.besvarelse.Besvarelse;
 
 public interface OppfolgingGateway {
 
-    Oppfolgingsstatus hentOppfolgingsstatus(String fodselsnummer);
+    Oppfolgingsstatus hentOppfolgingsstatus(Foedselsnummer fodselsnummer);
 
-    void aktiverBruker(String foedselsnummer, Innsatsgruppe innsatsgruppe);
+    void aktiverBruker(Foedselsnummer foedselsnummer, Innsatsgruppe innsatsgruppe);
 
-    void reaktiverBruker(String fodselsnummer);
+    void reaktiverBruker(Foedselsnummer fodselsnummer);
 
-    void settOppfolgingSykmeldt(String fodselsnummer, Besvarelse besvarelse);
+    void settOppfolgingSykmeldt(Foedselsnummer fodselsnummer, Besvarelse besvarelse);
 }
