b3c1c3bbf3d9adab67198591675a8848aa3169ec
Terje Heen <terje.heen@nav.no>
2020-03-17 22:37:17 +0100

    Konfigurerer opp SchedulerLock
    Konfigurerer opp SchedulerLock, og tar det i bruk på task for uttesting.

--- src/main/java/no/nav/fo/veilarbregistrering/registrering/scheduler/OverforTilArenaTask.java
+++ src/main/java/no/nav/fo/veilarbregistrering/registrering/scheduler/OverforTilArenaTask.java
@@ -1,5 +1,6 @@
 package no.nav.fo.veilarbregistrering.registrering.scheduler;
 
+import net.javacrumbs.shedlock.core.SchedulerLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,6 +12,7 @@ public class OverforTilArenaTask {
     private static final Logger LOG = LoggerFactory.getLogger(OverforTilArenaTask.class);
 
     @Scheduled(cron = "0 */5 * * * *")
+    @SchedulerLock(name = "overforTilArena")
     public void sendRegistreringerTilArenaCronJob() {
 
         LOG.info("Test av Job for å overføre registreringer");
