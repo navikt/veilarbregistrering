17cf4ebdf946370c9d1671cb9e4df0334fe4247b
Terje Heen <terje.heen@nav.no>
2020-03-19 21:33:03 +0100

    Innfører OverfoerArenaService
    OverfoerArenaService vil få oppgaven med å koordinere delene som skal gjøres for å overføre en registrering til Arena. Første del er nå på plass, hvor vi henter Innsatsgruppe fra Profilering, samt håndterer feil fra Arena.

---
+++ src/test/java/no/nav/fo/veilarbregistrering/registrering/bruker/ArenaOverforingServiceTest.java
@@ -0,0 +1,53 @@
+package no.nav.fo.veilarbregistrering.registrering.bruker;
+
+import no.nav.fo.veilarbregistrering.bruker.Foedselsnummer;
+import no.nav.fo.veilarbregistrering.oppfolging.OppfolgingGateway;
+import no.nav.fo.veilarbregistrering.profilering.ProfileringRepository;
+import org.junit.Before;
+import org.junit.Test;
+
+import static no.nav.fo.veilarbregistrering.profilering.Innsatsgruppe.STANDARD_INNSATS;
+import static no.nav.fo.veilarbregistrering.profilering.ProfileringTestdataBuilder.lagProfilering;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+public class ArenaOverforingServiceTest {
+
+    private static final Foedselsnummer FOEDSELSNUMMER = Foedselsnummer.of("12345678911");
+    private static final long BRUKER_REGISTRERING_ID = 2334L;
+
+    private ArenaOverforingService arenaOverforingService;
+    private ProfileringRepository profileringRepository;
+    private OppfolgingGateway oppfolgingClient;
+
+
+    @Before
+    public void setUp() {
+        profileringRepository = mock(ProfileringRepository.class);
+        oppfolgingClient = mock(OppfolgingGateway.class);
+
+        arenaOverforingService = new ArenaOverforingService(profileringRepository, oppfolgingClient);
+    }
+
+    @Test
+    public void gitt_at_overfoer_gikk_bra_skal_status_vaere_ARENA_OK() {
+        when(profileringRepository.hentProfileringForId(BRUKER_REGISTRERING_ID)).thenReturn(lagProfilering());
+
+        Status status = arenaOverforingService.overfoerRegistreringTilArena(FOEDSELSNUMMER, BRUKER_REGISTRERING_ID);
+
+        verify(oppfolgingClient, times(1)).aktiverBruker(FOEDSELSNUMMER, STANDARD_INNSATS);
+        assertThat(status).isEqualTo(Status.ARENA_OK);
+    }
+
+    @Test
+    public void gitt_at_overfoer_feiler_teknisk_skal_status_vaere_TEKNISK_FEIL() {
+        when(profileringRepository.hentProfileringForId(BRUKER_REGISTRERING_ID)).thenReturn(lagProfilering());
+        doThrow(new RuntimeException("Noe teknisk feilet"))
+                .when(oppfolgingClient).aktiverBruker(FOEDSELSNUMMER, STANDARD_INNSATS);
+
+        Status status = arenaOverforingService.overfoerRegistreringTilArena(FOEDSELSNUMMER, BRUKER_REGISTRERING_ID);
+
+        assertThat(status).isEqualTo(Status.TEKNISK_FEIL);
+    }
+    
+}
