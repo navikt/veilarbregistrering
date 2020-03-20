aa448446bd25201836ab81302ccb7705e6ad23b5
Richard Borge <Richard.Borge@nav.no>
2020-03-20 14:38:36 +0100

    Legger til request

--- src/main/java/no/nav/fo/veilarbregistrering/bruker/PdlOppslagService.java
+++ src/main/java/no/nav/fo/veilarbregistrering/bruker/PdlOppslagService.java
@@ -3,9 +3,15 @@ package no.nav.fo.veilarbregistrering.bruker;
 import no.nav.fo.veilarbregistrering.bruker.pdl.PdlPerson;
 import no.nav.fo.veilarbregistrering.bruker.pdl.PdlRequest;
 import no.nav.fo.veilarbregistrering.bruker.pdl.PdlResponse;
+import no.nav.fo.veilarbregistrering.bruker.pdl.Variables;
 import no.nav.sbl.rest.RestUtils;
 
 import javax.ws.rs.client.Entity;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class PdlOppslagService {
 
@@ -16,8 +22,8 @@ public class PdlOppslagService {
     public static final String ALLE_TEMA_HEADERVERDI = "GEN";
 
     public PdlPerson hentPerson(String fnr) {
-        // TODO Lag request, kall metode, returner
-        return null;
+        PdlRequest request = new PdlRequest(hentQuery(), new Variables(fnr, false));
+        return kall(fnr, request);
     }
 
     private PdlResponse kall(String fnr, PdlRequest request) {
@@ -33,5 +39,16 @@ public class PdlOppslagService {
                         .post(Entity.json(request), String.class));
 
         return new PdlResponse(); // TODO Gjør om responsejson til objekt
+    }
+
+    private String hentQuery() {
+        try {
+            byte[] bytes = Files.readAllBytes(Paths.get(PdlOppslagService.class.getResource("/pdl/hentPerson.graphql").toURI()));
+            return new String(bytes).replaceAll("[\n\r]]", "");
+        } catch (IOException e) {
+           throw new RuntimeException(e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
