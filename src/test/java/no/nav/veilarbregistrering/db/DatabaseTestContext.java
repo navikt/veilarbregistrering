9332419b463ae5c9775827d0f1601e468fc33f01
Terje Heen <terje.heen@nav.no>
2020-03-26 20:35:13 +0100

    Forenkler database-konfig lokalt
    Vi har ikke lenger muligheten til å gå mot ulike databaser lokalt, og skriver oss bort fra den muligheten. Hvis vi har behov senere, løser vi det i stedet med ulik config for ulik miljø/profiler.

--- src/test/java/no/nav/veilarbregistrering/db/DatabaseTestContext.java
+++ src/test/java/no/nav/veilarbregistrering/db/DatabaseTestContext.java
@@ -1,38 +1,11 @@
 package no.nav.veilarbregistrering.db;
 
-import lombok.val;
-import no.nav.fasit.DbCredentials;
-import no.nav.fasit.FasitUtils;
-import no.nav.fasit.TestEnvironment;
-
-import java.util.Optional;
-
-import static no.nav.fo.veilarbregistrering.config.ApplicationConfig.APPLICATION_NAME;
 import static no.nav.fo.veilarbregistrering.config.DatabaseConfig.*;
 
 public class DatabaseTestContext {
 
-    public static void setupContext(String miljo) {
-        val dbCredential = Optional.ofNullable(miljo)
-                .map(TestEnvironment::valueOf)
-                .map(testEnvironment -> FasitUtils.getDbCredentials(testEnvironment, APPLICATION_NAME));
-
-        if (dbCredential.isPresent()) {
-            setDataSourceProperties(dbCredential.get());
-        } else {
-            setInMemoryDataSourceProperties();
-        }
-    }
-
     public static void setupInMemoryDatabaseContext() {
-        setupContext(null);
-    }
-
-    private static void setDataSourceProperties(DbCredentials dbCredentials) {
-        System.setProperty(VEILARBREGISTRERINGDB_URL, dbCredentials.url);
-        System.setProperty(VEILARBREGISTRERINGDB_USERNAME, dbCredentials.getUsername());
-        System.setProperty(VEILARBREGISTRERINGDB_PASSWORD, dbCredentials.getPassword());
-
+        setInMemoryDataSourceProperties();
     }
 
     private static void setInMemoryDataSourceProperties() {
