3b5bfa8c79d22fd604b974707e115a1d9b250551
alexandraback <alexandra1.back@gmail.com>
2020-04-01 16:32:06 +0200

    KAIZEN-0: Legg til och ta i bruk jobutils. Legg til leaderElection. Endre pagineringsspørrigen

--- src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/PubliseringAvHistorikkTask.java
+++ src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/PubliseringAvHistorikkTask.java
@@ -1,6 +1,7 @@
 package no.nav.fo.veilarbregistrering.registrering.bruker;
 
 import no.nav.fo.veilarbregistrering.besvarelse.DinSituasjonSvar;
+import no.nav.jobutils.JobUtils;
 import no.nav.sbl.featuretoggle.unleash.UnleashService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,13 +9,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
-public class PubliseringAvHistorikkTask {
+public class PubliseringAvHistorikkTask implements Runnable {
 
     private static final Logger LOG = LoggerFactory.getLogger(PubliseringAvHistorikkTask.class);
 
     private final BrukerRegistreringRepository brukerRegistreringRepository;
     private final ArbeidssokerRegistrertProducer arbeidssokerRegistrertProducer;
     private final UnleashService unleashService;
+    private static final int PAGESIZE = 100;
 
     public PubliseringAvHistorikkTask(
             BrukerRegistreringRepository brukerRegistreringRepository,
@@ -24,33 +26,43 @@ public class PubliseringAvHistorikkTask {
         this.brukerRegistreringRepository = brukerRegistreringRepository;
         this.arbeidssokerRegistrertProducer = arbeidssokerRegistrertProducer;
         this.unleashService = unleashService;
+        JobUtils.runAsyncJobOnLeader(this);
+
     }
 
-    public void utfoer() {
+    public void run() {
+        int antalRaderIDatabasen = 100000; //TODO FINN UT HVA DENNE FAKTISKT ER
         int initPage = 0;
-        Pageable initPageRequest = PageRequest.of(initPage, 100);
+
+        do {
+            hentRegistreringer(initPage).forEach(this::publiserPaKafka);
+            initPage+= PAGESIZE;
+        }
+        while(this.sjekkFeatureErPa() && initPage < antalRaderIDatabasen);
+    }
+
+    private Page<ArbeidssokerRegistrertEventDto> hentRegistreringer(int initPage) {
+
+        Pageable initPageRequest = PageRequest.of(initPage, PAGESIZE);
         Page<ArbeidssokerRegistrertEventDto> registreringer =
                 brukerRegistreringRepository.findRegistreringByPage(initPageRequest);
 
         int totalPages = registreringer.getTotalPages();
         long totalElements = registreringer.getTotalElements();
 
-        LOG.info("Starter publisering av historiske registreringer av totalt {} elementer og {} sider", totalElements, totalPages);
+        LOG.info("Hentet historiske registreringer av totalt {} elementer og {} sider", totalElements, totalPages);
+        return registreringer;
+    }
 
-        registreringer.forEach(dto -> {
-            arbeidssokerRegistrertProducer.publiserArbeidssokerRegistrert(
-                    dto.getAktor_id(),
-                    //TODO: Sjekk om alle verdiene vi har i databasen er støttet
-                    DinSituasjonSvar.valueOf(dto.getBegrunnelse_for_registrering()),
-                    dto.getOpprettet_dato());
-        });
+    private void publiserPaKafka(ArbeidssokerRegistrertEventDto dto) {
+        arbeidssokerRegistrertProducer.publiserArbeidssokerRegistrert(
+                dto.getAktor_id(),
+                //TODO: Sjekk om alle verdiene vi har i databasen er støttet
+                DinSituasjonSvar.valueOf(dto.getBegrunnelse_for_registrering()),
+                dto.getOpprettet_dato());
+    }
 
-        initPage++;
-
-        Pageable nestePageRequest = PageRequest.of(initPage, 100);
-        Page<ArbeidssokerRegistrertEventDto> nesteRegistreringer =
-                brukerRegistreringRepository.findRegistreringByPage(nestePageRequest);
-
-        //TODO: Denne løkken bør kjøres rekursivt
+    private boolean sjekkFeatureErPa () {
+        return this.unleashService.isEnabled("kjekt-feature-navn"); // TODO FINN UT HVA FEATUREN HETER
     }
 }
