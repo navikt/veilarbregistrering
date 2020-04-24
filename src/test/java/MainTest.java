87d9210655f8cf39409ff653c86608a5b0160866
Terje Heen <terje.heen@nav.no>
2020-04-24 11:48:44 +0200

    Oppgraderer til 1.2020.03.10_16.55-ca8a7f989623 av common-java
    Oppgraderer common-java, og tilpasser config i test-scope slik at appen startet OK lokalt.

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,16 +1,17 @@
 import no.nav.apiapp.ApiApp;
 import no.nav.fo.veilarbregistrering.config.ApplicationTestConfig;
 import no.nav.testconfig.ApiAppTest;
-import no.nav.veilarbregistrering.TestContext;
 import no.nav.veilarbregistrering.db.DatabaseTestContext;
 
 public class MainTest {
     public static final String TEST_PORT = "8810";
 
     public static void main(String[] args) {
-        ApiAppTest.setupTestContext(ApiAppTest.Config.builder().applicationName("veilarbregistrering").build());
+        ApiAppTest.setupTestContext(ApiAppTest.Config.builder()
+                .applicationName("veilarbregistrering")
+                .environment("q0")
+                .build());
         DatabaseTestContext.setupInMemoryDatabaseContext();
-        TestContext.setup();
         String[] arguments = {TEST_PORT};
         ApiApp.runApp(ApplicationTestConfig.class, arguments);
     }
--- src/test/java/no/nav/fo/veilarbregistrering/bruker/pdl/PdlOppslagConfig.java
+++ src/test/java/no/nav/fo/veilarbregistrering/bruker/pdl/PdlOppslagConfig.java
@@ -1,16 +1,20 @@
 package no.nav.fo.veilarbregistrering.bruker.pdl;
 
+import no.nav.fo.veilarbregistrering.bruker.PdlOppslagGateway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PdlOppslagConfig {
 
-    private final String PDL_PROPERTY_NAME = "PDL_URL";
-
     @Bean
-    PdlOppslagClient pdlOppslagService() {
+    PdlOppslagClient pdlOppslagClient() {
         return new PdlOppslagClient(null, null, null) {
         };
+    }
+
+    @Bean
+    PdlOppslagGateway pdlOppslagGateway(PdlOppslagClient pdlOppslagClient) {
+        return new PdlOppslagGatewayImpl(pdlOppslagClient);
     }
 }
