5df04227e7481ae09e69a1fa0f27e21f8af5f1c4
Terje Heen <terje.heen@nav.no>
2020-04-27 10:40:15 +0200

    Legger til funksjonalitet for å hente token fra Cookie før evt. Header

--- src/main/java/no/nav/fo/veilarbregistrering/sykemelding/adapter/SykmeldtInfoClient.java
+++ src/main/java/no/nav/fo/veilarbregistrering/sykemelding/adapter/SykmeldtInfoClient.java
@@ -1,16 +1,21 @@
 package no.nav.fo.veilarbregistrering.sykemelding.adapter;
 
 import no.nav.common.oidc.Constants;
-import no.nav.common.oidc.utils.TokenLocator;
+import no.nav.common.oidc.utils.TokenUtils;
 import no.nav.fo.veilarbregistrering.bruker.Foedselsnummer;
 import no.nav.fo.veilarbregistrering.httpclient.BaseClient;
 import no.nav.sbl.rest.RestUtils;
 
 import javax.inject.Provider;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.InternalServerErrorException;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 import static javax.ws.rs.core.HttpHeaders.COOKIE;
+import static no.nav.common.oidc.Constants.AZURE_AD_B2C_ID_TOKEN_COOKIE_NAME;
 import static no.nav.sbl.rest.RestUtils.withClient;
 
 public class SykmeldtInfoClient extends BaseClient {
@@ -26,17 +31,35 @@ public class SykmeldtInfoClient extends BaseClient {
     private InfotrygdData getSykeforloepMetadata(String url) {
         HttpServletRequest request = httpServletRequestProvider.get();
 
-        TokenLocator tokenLocator = new TokenLocator(Constants.AZURE_AD_B2C_ID_TOKEN_COOKIE_NAME);
-
         try {
             return withClient(RestUtils.RestConfig.builder().readTimeout(HTTP_READ_TIMEOUT).build(),
                     c -> c.target(url)
                             .request()
                             .header(COOKIE, request.getHeader(COOKIE))
-                            .header("Authorization", "Bearer " + tokenLocator.getIdToken(request).orElse(null))
+                            .header("Authorization", "Bearer " + getToken(request))
                             .get(InfotrygdData.class));
         } catch (Exception e) {
             throw new InternalServerErrorException("Hent maksdato fra Infotrygd feilet.", e);
         }
+    }
+
+    private String getToken(HttpServletRequest request) {
+        Optional<String> tokenFromCookie = getTokenFromCookie(request, AZURE_AD_B2C_ID_TOKEN_COOKIE_NAME);
+
+        if (tokenFromCookie.isPresent()) {
+            return tokenFromCookie.get();
+        }
+
+        return TokenUtils.getTokenFromHeader(request).orElse(null);
+    }
+
+    private Optional<String> getTokenFromCookie(HttpServletRequest request, String cookieName) {
+        return Optional.ofNullable(request.getCookies())
+                .flatMap(cookies -> Arrays
+                        .stream(cookies)
+                        .filter(cookie -> cookie.getName().equals(cookieName) && cookie.getValue() != null)
+                        .findFirst()
+                        .map(Cookie::getValue)
+                );
     }
 }
