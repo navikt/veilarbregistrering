dddcd004807fe9c9705bd75c92384b17f4886e82
Terje Heen <terje.heen@nav.no>
2020-05-07 15:53:27 +0200

    Utvider integrasjon mot PDL med fodsel og telefonnummer
    Vi trenger fodsel for å utlede alder, mens vi trenger telefonnummer som kontaktinfo

---
+++ src/main/java/no/nav/fo/veilarbregistrering/bruker/pdl/PdlFoedsel.java
@@ -0,0 +1,19 @@
+package no.nav.fo.veilarbregistrering.bruker.pdl;
+
+import java.util.Date;
+
+public class PdlFoedsel {
+
+    private Date foedselsdato;
+
+    public PdlFoedsel() {
+    }
+
+    public Date getFoedselsdato() {
+        return foedselsdato;
+    }
+
+    public void setFoedselsdato(Date foedselsdato) {
+        this.foedselsdato = foedselsdato;
+    }
+}
