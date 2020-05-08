81b4472d087f3fbc2f0505e12227d719bbb0f323
Terje Heen <terje.heen@nav.no>
2020-05-08 12:24:55 +0200

    Endrer endepunktet for kontaktinfo litt før vi går live med tjenesten
    Innfører `/person` før `/kontaktinfo` og tydeliggjør hvem som er kilden til telefonnummeret, da vi tenker å utvide med telefonnummer fra NAV også.

--- src/main/java/no/nav/fo/veilarbregistrering/bruker/resources/KontaktinfoDto.java
+++ src/main/java/no/nav/fo/veilarbregistrering/bruker/resources/KontaktinfoDto.java
@@ -7,6 +7,7 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 public class KontaktinfoDto {
 
-    private String telefon;
+    private String telefonnummerHosKrr;
+    private String telefonnummerHosNav;
 
 }
--- src/main/java/no/nav/fo/veilarbregistrering/bruker/resources/KontaktinfoResource.java
+++ src/main/java/no/nav/fo/veilarbregistrering/bruker/resources/KontaktinfoResource.java
@@ -2,6 +2,8 @@ package no.nav.fo.veilarbregistrering.bruker.resources;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import no.nav.apiapp.feil.Feil;
 import no.nav.apiapp.feil.FeilType;
 import no.nav.apiapp.security.veilarbabac.VeilarbAbacPepClient;
@@ -19,7 +21,7 @@ import javax.ws.rs.Produces;
 import static no.nav.fo.veilarbregistrering.bruker.BrukerAdapter.map;
 
 @Component
-@Path("/")
+@Path("/person")
 @Produces("application/json")
 @Api(value = "KontaktinfoResource", description = "Tjeneste for henting av kontaktinfo fra Kontakt og reservasjonsregisteret.")
 public class KontaktinfoResource {
@@ -29,20 +31,14 @@ public class KontaktinfoResource {
     private final VeilarbAbacPepClient pepClient;
     private final UnleashService unleashService;
 
-    public KontaktinfoResource(
-            VeilarbAbacPepClient pepClient,
-            UserService userService,
-            KrrGateway krrGateway,
-            UnleashService unleashService) {
-        this.pepClient = pepClient;
-        this.userService = userService;
-        this.krrGateway = krrGateway;
-        this.unleashService = unleashService;
-    }
-
     @GET
     @Path("/kontaktinfo")
-    @ApiOperation(value = "Henter informasjon om brukers kontaktinfo.")
+    @ApiOperation(value = "Henter kontaktinformasjon (telefonnummer) til bruker.")
+    @ApiResponses({
+            @ApiResponse(code = 403, message = "Ingen tilgang"),
+            @ApiResponse(code = 404, message = "Ikke funnet"),
+            @ApiResponse(code = 500, message = "Ukjent feil")
+    })
     public KontaktinfoDto hentSisteArbeidsforhold() {
         final Bruker bruker = userService.hentBruker();
 
@@ -54,6 +50,17 @@ public class KontaktinfoResource {
 
         Kontaktinfo kontaktinfo = krrGateway.hentKontaktinfo(bruker);
         return KontaktinfoMapper.map(kontaktinfo);
+    }
+
+    public KontaktinfoResource(
+            VeilarbAbacPepClient pepClient,
+            UserService userService,
+            KrrGateway krrGateway,
+            UnleashService unleashService) {
+        this.pepClient = pepClient;
+        this.userService = userService;
+        this.krrGateway = krrGateway;
+        this.unleashService = unleashService;
     }
 
 }
