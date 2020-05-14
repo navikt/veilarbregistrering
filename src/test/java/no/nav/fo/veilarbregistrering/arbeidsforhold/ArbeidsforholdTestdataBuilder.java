2a88d3128df2c2b98dd3299733122b997acd9929
Terje Heen <terje.heen@nav.no>
2020-05-14 21:31:15 +0200

    Legger til testtilfeller for OppgaveRouter

--- src/test/java/no/nav/fo/veilarbregistrering/arbeidsforhold/ArbeidsforholdTestdataBuilder.java
+++ src/test/java/no/nav/fo/veilarbregistrering/arbeidsforhold/ArbeidsforholdTestdataBuilder.java
@@ -7,19 +7,19 @@ public class ArbeidsforholdTestdataBuilder {
     public static Arbeidsforhold paagaaende() {
         LocalDate fom0 = LocalDate.of(2017,11,1);
         LocalDate tom0 = null;
-        return new Arbeidsforhold().setFom(fom0).setTom(tom0);
+        return new Arbeidsforhold().setFom(fom0).setTom(tom0).setArbeidsgiverOrgnummer("555555555");
     }
 
     public static Arbeidsforhold siste() {
         LocalDate fom1 = LocalDate.of(2017,11,1);
         LocalDate tom1 = LocalDate.of(2017,11,30);
-        return new Arbeidsforhold().setFom(fom1).setTom(tom1);
+        return new Arbeidsforhold().setFom(fom1).setTom(tom1).setArbeidsgiverOrgnummer("123456789");
     }
 
     public static Arbeidsforhold nestSiste() {
         LocalDate fom2 = LocalDate.of(2017,9,1);
         LocalDate tom2 = LocalDate.of(2017,9,30);
-        return new Arbeidsforhold().setFom(fom2).setTom(tom2);
+        return new Arbeidsforhold().setFom(fom2).setTom(tom2).setArbeidsgiverOrgnummer("987654321");
     }
 
     public static Arbeidsforhold eldre() {
---
+++ src/test/java/no/nav/fo/veilarbregistrering/oppgave/OppgaveRouterTest.java
@@ -0,0 +1,94 @@
+package no.nav.fo.veilarbregistrering.oppgave;
+
+import no.nav.fo.veilarbregistrering.arbeidsforhold.*;
+import no.nav.fo.veilarbregistrering.bruker.AktorId;
+import no.nav.fo.veilarbregistrering.bruker.Bruker;
+import no.nav.fo.veilarbregistrering.bruker.Foedselsnummer;
+import no.nav.fo.veilarbregistrering.bruker.Periode;
+import no.nav.fo.veilarbregistrering.enhet.EnhetGateway;
+import no.nav.fo.veilarbregistrering.enhet.Forretningsadresse;
+import no.nav.fo.veilarbregistrering.enhet.Kommunenummer;
+import no.nav.fo.veilarbregistrering.enhet.Organisasjonsdetaljer;
+import no.nav.fo.veilarbregistrering.orgenhet.Enhetsnr;
+import no.nav.fo.veilarbregistrering.orgenhet.NorgGateway;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Optional;
+
+import static no.nav.fo.veilarbregistrering.arbeidsforhold.FlereArbeidsforholdTestdataBuilder.flereArbeidsforholdTilfeldigSortert;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OppgaveRouterTest {
+
+    private static final Bruker BRUKER = Bruker.of(
+            Foedselsnummer.of("12345678911"), AktorId.valueOf("32235352"));
+
+    @Test
+    public void ingen_arbeidsforhold_skal_gi_empty_enhetsnummer() {
+        ArbeidsforholdGateway arbeidsforholdGateway = fnr -> FlereArbeidsforhold.of(Collections.emptyList());
+        EnhetGateway enhetGateway = organisasjonsnummer -> Optional.empty();
+        NorgGateway norgGateway = kommunenummer -> Optional.empty();
+
+        OppgaveRouter oppgaveRouter = new OppgaveRouter(arbeidsforholdGateway, enhetGateway, norgGateway);
+
+        Optional<Enhetsnr> enhetsnr = oppgaveRouter.hentEnhetsnummerForSisteArbeidsforholdTil(BRUKER);
+
+        assertThat(enhetsnr).isEmpty();
+    }
+
+    @Test
+    public void ingen_enhet_for_orgnummer_skal_gi_empty_enhetsnummer() {
+        ArbeidsforholdGateway arbeidsforholdGateway = fnr -> flereArbeidsforholdTilfeldigSortert();
+        EnhetGateway enhetGateway = organisasjonsnummer -> Optional.empty();
+        NorgGateway norgGateway = kommunenummer -> Optional.empty();
+
+        OppgaveRouter oppgaveRouter = new OppgaveRouter(arbeidsforholdGateway, enhetGateway, norgGateway);
+
+        Optional<Enhetsnr> enhetsnr = oppgaveRouter.hentEnhetsnummerForSisteArbeidsforholdTil(BRUKER);
+
+        assertThat(enhetsnr).isEmpty();
+    }
+
+    @Test
+    public void ingen_navenhet_for_organisasjon_skal_gi_empty_enhetsnummer() {
+        ArbeidsforholdGateway arbeidsforholdGateway = fnr -> flereArbeidsforholdTilfeldigSortert();
+
+        Forretningsadresse forretningsadresse = new Forretningsadresse(
+                Kommunenummer.of("1241"),
+                Periode.of(LocalDate.of(2020, 1, 1), null));
+
+        EnhetGateway enhetGateway = organisasjonsnummer -> Optional.of(Organisasjonsdetaljer.of(
+                Arrays.asList(forretningsadresse), Collections.emptyList()));
+
+        NorgGateway norgGateway = kommunenummer -> Optional.empty();
+
+        OppgaveRouter oppgaveRouter = new OppgaveRouter(arbeidsforholdGateway, enhetGateway, norgGateway);
+
+        Optional<Enhetsnr> enhetsnr = oppgaveRouter.hentEnhetsnummerForSisteArbeidsforholdTil(BRUKER);
+
+        assertThat(enhetsnr).isEmpty();
+    }
+
+    @Test
+    public void enhetsnummer_skal_returneres_nar_alle_koblingen_til_arbeidsforhold_er_komplett() {
+        ArbeidsforholdGateway arbeidsforholdGateway = fnr -> flereArbeidsforholdTilfeldigSortert();
+
+        Forretningsadresse forretningsadresse = new Forretningsadresse(
+                Kommunenummer.of("1241"),
+                Periode.of(LocalDate.of(2020, 1, 1), null));
+
+        EnhetGateway enhetGateway = organisasjonsnummer -> Optional.of(Organisasjonsdetaljer.of(
+                Arrays.asList(forretningsadresse), Collections.emptyList()));
+
+        NorgGateway norgGateway = kommunenummer -> Optional.of(Enhetsnr.of(232));
+
+        OppgaveRouter oppgaveRouter = new OppgaveRouter(arbeidsforholdGateway, enhetGateway, norgGateway);
+
+        Optional<Enhetsnr> enhetsnr = oppgaveRouter.hentEnhetsnummerForSisteArbeidsforholdTil(BRUKER);
+
+        assertThat(enhetsnr).hasValue(Enhetsnr.of(232));
+    }
+}
