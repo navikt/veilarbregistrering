48774ff105d2a355baa64965dfe2be44439f5107
Terje Heen <terje.heen@nav.no>
2020-05-25 13:02:50 +0200

    Legger på målinger ifm. OppgaveRouting

---
+++ src/main/java/no/nav/fo/veilarbregistrering/oppgave/RoutingStep.java
@@ -0,0 +1,26 @@
+package no.nav.fo.veilarbregistrering.oppgave;
+
+import no.nav.fo.veilarbregistrering.metrics.Metric;
+
+public enum  RoutingStep implements Metric {
+
+    GeografiskTilknytning_Feilet,
+    GeografiskTilknytning_IkkeFunnet,
+    Enhetsnummer_Feilet,
+    SisteArbeidsforhold_IkkeFunnet,
+    OrgNummer_ikkeFunnet,
+    OrgDetaljer_IkkeFunnet,
+    Kommunenummer_IkkeFunnet,
+    Enhetsnummer_IkkeFunnet,
+    Enhetsnummer_Funnet;
+
+    @Override
+    public String fieldName() {
+        return "routingStep";
+    }
+
+    @Override
+    public Object value() {
+        return this.toString();
+    }
+}
