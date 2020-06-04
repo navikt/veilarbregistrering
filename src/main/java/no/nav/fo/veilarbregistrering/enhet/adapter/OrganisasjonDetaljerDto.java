2291094d0694a0ab14db32b9ad081f037a7b508c
Terje Heen <terje.heen@nav.no>
2020-06-04 18:33:44 +0200

    Retter opp i NullPointerException ifm. at forretningsadresse ikke var satt
    Pga. usikkerhet rundt hvordan gson velger å instansiere objektet, constructor, seter eller refelction, så virker det tryggest å løse dette ved en mer robust get-metode. Flere/bedre tester burde kunne rydde opp i denne usikkerheten.

--- src/main/java/no/nav/fo/veilarbregistrering/enhet/adapter/OrganisasjonDetaljerDto.java
+++ src/main/java/no/nav/fo/veilarbregistrering/enhet/adapter/OrganisasjonDetaljerDto.java
@@ -1,14 +1,36 @@
 package no.nav.fo.veilarbregistrering.enhet.adapter;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.List;
 
-@Data
-@NoArgsConstructor
+import static java.util.Collections.emptyList;
+
 public class OrganisasjonDetaljerDto {
 
     private List<ForretningsAdresseDto> forretningsadresser;
     private List<PostadresseDto> postadresser;
+
+    OrganisasjonDetaljerDto() {
+        //default constructor for Gson
+    }
+
+    public OrganisasjonDetaljerDto(List<ForretningsAdresseDto> forretningsadresser, List<PostadresseDto> postadresser) {
+        this.forretningsadresser = forretningsadresser;
+        this.postadresser = postadresser;
+    }
+
+    public void setForretningsadresser(List<ForretningsAdresseDto> forretningsadresser) {
+        this.forretningsadresser = forretningsadresser;
+    }
+
+    public List<ForretningsAdresseDto> getForretningsadresser() {
+        return forretningsadresser != null ? forretningsadresser : emptyList();
+    }
+
+    public void setPostadresser(List<PostadresseDto> postadresser) {
+        this.postadresser = postadresser != null ? postadresser : emptyList();
+    }
+
+    public List<PostadresseDto> getPostadresser() {
+        return postadresser != null ? postadresser : emptyList();
+    }
 }
