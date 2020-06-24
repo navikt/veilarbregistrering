6fb1bea79a7da4538f05d2161fff72b1c9435e30
Johan Rusvik <johan.rusvik@nav.no>
2020-06-24 13:57:56 +0200

    Oppretter bean for FormidlingsgruppeGateway

--- src/main/java/no/nav/fo/veilarbregistrering/arbeidssoker/adapter/FormidlingsgruppeGatewayImpl.java
+++ src/main/java/no/nav/fo/veilarbregistrering/arbeidssoker/adapter/FormidlingsgruppeGatewayImpl.java
@@ -4,13 +4,9 @@ import no.nav.fo.veilarbregistrering.arbeidssoker.Arbeidssokerperiode;
 import no.nav.fo.veilarbregistrering.arbeidssoker.FormidlingsgruppeGateway;
 import no.nav.fo.veilarbregistrering.bruker.Foedselsnummer;
 import no.nav.fo.veilarbregistrering.bruker.Periode;
-import no.nav.fo.veilarbregistrering.oppfolging.Formidlingsgruppe;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
-
-import static java.util.stream.Collectors.toList;
 
 public class FormidlingsgruppeGatewayImpl implements FormidlingsgruppeGateway {
 
