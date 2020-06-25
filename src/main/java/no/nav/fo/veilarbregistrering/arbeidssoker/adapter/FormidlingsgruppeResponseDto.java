53f39d4e91e69d3eee85ea9e5b8b6ccd31e36c4c
Terje Heen <terje.heen@nav.no>
2020-06-25 20:34:47 +0200

    Legger til null-sjekk for å unngå NPE

--- src/main/java/no/nav/fo/veilarbregistrering/arbeidssoker/adapter/FormidlingsgruppeResponseDto.java
+++ src/main/java/no/nav/fo/veilarbregistrering/arbeidssoker/adapter/FormidlingsgruppeResponseDto.java
@@ -1,6 +1,9 @@
 package no.nav.fo.veilarbregistrering.arbeidssoker.adapter;
 
+import java.util.Collections;
 import java.util.List;
+
+import static java.util.Collections.emptyList;
 
 public class FormidlingsgruppeResponseDto {
 
@@ -8,10 +11,13 @@ public class FormidlingsgruppeResponseDto {
     private String fodselsnr;
     private List<FormidlingshistorikkDto> formidlingshistorikk;
 
-    public FormidlingsgruppeResponseDto(String personId, String fodselsnr, List<FormidlingshistorikkDto> formidlingshistorikk) {
+    public FormidlingsgruppeResponseDto(
+            String personId,
+            String fodselsnr,
+            List<FormidlingshistorikkDto> formidlingshistorikk) {
         this.personId = personId;
         this.fodselsnr = fodselsnr;
-        this.formidlingshistorikk = formidlingshistorikk;
+        this.formidlingshistorikk = formidlingshistorikk != null ? formidlingshistorikk : emptyList();
     }
 
     public String getPersonId() {
