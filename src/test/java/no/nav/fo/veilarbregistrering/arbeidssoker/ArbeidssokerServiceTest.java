1456cec5a2e20d2a1b1cd59dfeefa1535e84e935
Terje Heen <terje.heen@nav.no>
2020-07-01 13:10:49 +0200

    Legger til en ekstra test for ArbeiddsokerService

--- src/test/java/no/nav/fo/veilarbregistrering/arbeidssoker/ArbeidssokerServiceTest.java
+++ src/test/java/no/nav/fo/veilarbregistrering/arbeidssoker/ArbeidssokerServiceTest.java
@@ -8,9 +8,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
+import static java.util.Arrays.asList;
 import static no.nav.fo.veilarbregistrering.arbeidssoker.ArbeidssokerService.VEILARBREGISTRERING_FORMIDLINGSGRUPPE_LOCALCACHE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -18,17 +18,18 @@ import static org.mockito.Mockito.when;
 
 public class ArbeidssokerServiceTest {
 
-    public static final Foedselsnummer FOEDSELSNUMMER = Foedselsnummer.of("123445678911");
+    public static final Foedselsnummer FOEDSELSNUMMER = Foedselsnummer.of("12345678911");
+    public static final Foedselsnummer FOEDSELSNUMMER_2 = Foedselsnummer.of("11234567890");
 
-    ArbeidssokerService arbeidssokerService;
+    private ArbeidssokerService arbeidssokerService;
+    private UnleashService unleashService;
 
     @BeforeEach
     public void setup() {
-        UnleashService unleashService = mock(UnleashService.class);
-        when(unleashService.isEnabled(VEILARBREGISTRERING_FORMIDLINGSGRUPPE_LOCALCACHE)).thenReturn(true);
-        this.arbeidssokerService = new ArbeidssokerService(
-                new CustomArbeidssokerRepository(),
-                (foedselsnummer, periode) -> null,
+        unleashService = mock(UnleashService.class);
+        arbeidssokerService = new ArbeidssokerService(
+                new StubArbeidssokerRepository(),
+                new StubFormidlingsgruppeGateway(),
                 unleashService);
     }
 
@@ -39,16 +40,33 @@ public class ArbeidssokerServiceTest {
                 LocalDate.of(2020, 1, 2),
                 LocalDate.of(2020, 5, 1));
 
+        when(unleashService.isEnabled(VEILARBREGISTRERING_FORMIDLINGSGRUPPE_LOCALCACHE)).thenReturn(true);
+
         List<Arbeidssokerperiode> arbeidssokerperiodes = arbeidssokerService.hentArbeidssokerperioder(FOEDSELSNUMMER, forespurtPeriode);
         assertThat(arbeidssokerperiodes).hasSize(4);
         assertThat(arbeidssokerperiodes).containsSequence(
-                CustomArbeidssokerRepository.ARBEIDSSOKERPERIODE_1,
-                CustomArbeidssokerRepository.ARBEIDSSOKERPERIODE_2,
-                CustomArbeidssokerRepository.ARBEIDSSOKERPERIODE_3,
-                CustomArbeidssokerRepository.ARBEIDSSOKERPERIODE_4);
+                StubArbeidssokerRepository.ARBEIDSSOKERPERIODE_1,
+                StubArbeidssokerRepository.ARBEIDSSOKERPERIODE_2,
+                StubArbeidssokerRepository.ARBEIDSSOKERPERIODE_3,
+                StubArbeidssokerRepository.ARBEIDSSOKERPERIODE_4);
     }
 
-    private static class CustomArbeidssokerRepository implements ArbeidssokerRepository {
+    @Test
+    public void hentArbeidssokerperioder_skal_hente_fra_ords_toggle_ikke_er_skrudd_paa() {
+        Periode forespurtPeriode = Periode.of(
+                LocalDate.of(2020, 1, 2),
+                LocalDate.of(2020, 5, 1));
+
+        when(unleashService.isEnabled(VEILARBREGISTRERING_FORMIDLINGSGRUPPE_LOCALCACHE)).thenReturn(false);
+
+        List<Arbeidssokerperiode> arbeidssokerperiodes = arbeidssokerService.hentArbeidssokerperioder(FOEDSELSNUMMER, forespurtPeriode);
+
+        assertThat(arbeidssokerperiodes).hasSize(1);
+        assertThat(arbeidssokerperiodes).contains(StubArbeidssokerRepository.ARBEIDSSOKERPERIODE_1);
+
+    }
+
+    private static class StubArbeidssokerRepository implements ArbeidssokerRepository {
 
         public static final Arbeidssokerperiode ARBEIDSSOKERPERIODE_1 = new Arbeidssokerperiode(
                 Formidlingsgruppe.of("ARBS"),
@@ -70,11 +88,27 @@ public class ArbeidssokerServiceTest {
 
         @Override
         public Arbeidssokerperioder finnFormidlingsgrupper(Foedselsnummer foedselsnummer) {
-            return new Arbeidssokerperioder(Arrays.asList(
+            Map<Foedselsnummer, Arbeidssokerperioder> map = new HashMap<>();
+
+            Arbeidssokerperioder arbeidssokerperioder = new Arbeidssokerperioder(asList(
                     ARBEIDSSOKERPERIODE_3,
                     ARBEIDSSOKERPERIODE_1,
                     ARBEIDSSOKERPERIODE_4,
                     ARBEIDSSOKERPERIODE_2));
+            map.put(FOEDSELSNUMMER, arbeidssokerperioder);
+            map.put(FOEDSELSNUMMER_2, new Arbeidssokerperioder(Collections.emptyList()));
+
+            return map.get(foedselsnummer);
+        }
+    }
+
+    private class StubFormidlingsgruppeGateway implements FormidlingsgruppeGateway {
+
+        @Override
+        public Arbeidssokerperioder finnArbeissokerperioder(Foedselsnummer foedselsnummer, Periode periode) {
+            Map<Foedselsnummer, Arbeidssokerperioder> map = new HashMap<>();
+            map.put(FOEDSELSNUMMER, new Arbeidssokerperioder(asList(StubArbeidssokerRepository.ARBEIDSSOKERPERIODE_1)));
+            return map.get(foedselsnummer);
         }
     }
 }
