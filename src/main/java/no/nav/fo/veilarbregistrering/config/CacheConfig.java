84db0c479b4e26e48b104b4bc0cf0ed6ae50c89a
Terje Heen <terje.heen@nav.no>
2020-08-17 15:38:43 +0200

    Fjerner `HENT_ENHET`-cache da den ikke er i bruk
    Fant ingen spor etter caching med navn `HENT_ENHET` så fjerner denne.

--- src/main/java/no/nav/fo/veilarbregistrering/config/CacheConfig.java
+++ src/main/java/no/nav/fo/veilarbregistrering/config/CacheConfig.java
@@ -16,14 +16,8 @@ import static no.nav.sbl.dialogarena.common.abac.pep.context.AbacContext.ABAC_CA
 @EnableCaching
 public class CacheConfig {
 
-    public static final String HENT_ENHET = "hentEnhet";
     public static final String HENT_ARBEIDSFORHOLD = "hentArbeidsforhold";
     public static final String HENT_ALLE_ENHETER = "hentAlleEnheter";
-
-    private static final CacheConfiguration HENT_ENHET_CACHE = new CacheConfiguration(HENT_ENHET, 10000)
-            .memoryStoreEvictionPolicy(LRU)
-            .timeToIdleSeconds(3600)
-            .timeToLiveSeconds(3600);
 
     private static final CacheConfiguration HENT_ARBEIDSFORHOLD_CACHE = new CacheConfiguration(HENT_ARBEIDSFORHOLD, 100000)
             .memoryStoreEvictionPolicy(LRU)
@@ -38,7 +32,6 @@ public class CacheConfig {
     @Bean
     public CacheManager cacheManager() {
         net.sf.ehcache.config.Configuration config = new net.sf.ehcache.config.Configuration();
-        config.addCache(HENT_ENHET_CACHE);
         config.addCache(ABAC_CACHE);
         config.addCache(AKTOR_ID_FROM_FNR_CACHE);
         config.addCache(FNR_FROM_AKTOR_ID_CACHE);
