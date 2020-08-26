f961b5d4bc21212ea3b9ec8a61b6cfc3d4e9923e
Johan Rusvik <johan.rusvik@nav.no>
2020-08-26 15:08:24 +0200

    Inkluderer mer data i flogging av lagring av formidlingsgruppeendring som feiler

--- src/main/java/no/nav/fo/veilarbregistrering/db/arbeidssoker/ArbeidssokerRepositoryImpl.java
+++ src/main/java/no/nav/fo/veilarbregistrering/db/arbeidssoker/ArbeidssokerRepositoryImpl.java
@@ -6,6 +6,7 @@ import no.nav.fo.veilarbregistrering.arbeidssoker.EndretFormidlingsgruppeCommand
 import no.nav.fo.veilarbregistrering.arbeidssoker.Formidlingsgruppe;
 import no.nav.fo.veilarbregistrering.bruker.Foedselsnummer;
 import no.nav.sbl.sql.SqlUtils;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -33,25 +34,31 @@ public class ArbeidssokerRepositoryImpl implements ArbeidssokerRepository {
     @Override
     public long lagre(EndretFormidlingsgruppeCommand endretFormidlingsgruppeCommand) {
         long id = nesteFraSekvens("FORMIDLINGSGRUPPE_SEQ");
-        SqlUtils.insert(jdbcTemplate, "FORMIDLINGSGRUPPE")
-                .value("ID", id)
-                .value("FOEDSELSNUMMER", endretFormidlingsgruppeCommand.getFoedselsnummer()
-                        .orElseThrow(() -> new IllegalStateException("Foedselsnummer var ikke satt. Skulle vært filtrert bort i forkant!"))
-                        .stringValue())
-                .value("PERSON_ID", endretFormidlingsgruppeCommand.getPersonId())
-                .value("PERSON_ID_STATUS", endretFormidlingsgruppeCommand.getPersonIdStatus())
-                .value("OPERASJON", endretFormidlingsgruppeCommand.getOperation().name())
-                .value("FORMIDLINGSGRUPPE", endretFormidlingsgruppeCommand.getFormidlingsgruppe().stringValue())
-                .value("FORMIDLINGSGRUPPE_ENDRET", Timestamp.valueOf(endretFormidlingsgruppeCommand.getFormidlingsgruppeEndret()))
-                .value("FORR_FORMIDLINGSGRUPPE", endretFormidlingsgruppeCommand.getForrigeFormidlingsgruppe()
-                        .map(Formidlingsgruppe::stringValue)
-                        .orElse(null))
-                .value("FORR_FORMIDLINGSGRUPPE_ENDRET", endretFormidlingsgruppeCommand.getForrigeFormidlingsgruppeEndret()
-                        .map(Timestamp::valueOf)
-                       .orElse(null))
-                .value("FORMIDLINGSGRUPPE_LEST", Timestamp.valueOf(LocalDateTime.now()))
-                .execute();
-
+        try {
+            SqlUtils.insert(jdbcTemplate, "FORMIDLINGSGRUPPE")
+                    .value("ID", id)
+                    .value("FOEDSELSNUMMER", endretFormidlingsgruppeCommand.getFoedselsnummer()
+                            .orElseThrow(() -> new IllegalStateException("Foedselsnummer var ikke satt. Skulle vært filtrert bort i forkant!"))
+                            .stringValue())
+                    .value("PERSON_ID", endretFormidlingsgruppeCommand.getPersonId())
+                    .value("PERSON_ID_STATUS", endretFormidlingsgruppeCommand.getPersonIdStatus())
+                    .value("OPERASJON", endretFormidlingsgruppeCommand.getOperation().name())
+                    .value("FORMIDLINGSGRUPPE", endretFormidlingsgruppeCommand.getFormidlingsgruppe().stringValue())
+                    .value("FORMIDLINGSGRUPPE_ENDRET", Timestamp.valueOf(endretFormidlingsgruppeCommand.getFormidlingsgruppeEndret()))
+                    .value("FORR_FORMIDLINGSGRUPPE", endretFormidlingsgruppeCommand.getForrigeFormidlingsgruppe()
+                            .map(Formidlingsgruppe::stringValue)
+                            .orElse(null))
+                    .value("FORR_FORMIDLINGSGRUPPE_ENDRET", endretFormidlingsgruppeCommand.getForrigeFormidlingsgruppeEndret()
+                            .map(Timestamp::valueOf)
+                            .orElse(null))
+                    .value("FORMIDLINGSGRUPPE_LEST", Timestamp.valueOf(LocalDateTime.now()))
+                    .execute();
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityViolationException(
+                    String.format("Lagring av følgende formidlingsgruppeendring feilet: %s", endretFormidlingsgruppeCommand),
+                    e
+            );
+        }
         return id;
     }
 
