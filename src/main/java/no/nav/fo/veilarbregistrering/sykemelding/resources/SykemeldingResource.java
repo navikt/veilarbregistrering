23c219039ef987fd5ecba23167fa8f37d881bbb5
Terje Heen <terje.heen@nav.no>
2020-09-02 20:50:37 +0200

    Fjerner toggle ifm. sykemelding og PDL
    Integrasjon mot PDL er nå testet ok, og vi kan fjerne toggle.

--- src/main/java/no/nav/fo/veilarbregistrering/sykemelding/resources/SykemeldingResource.java
+++ src/main/java/no/nav/fo/veilarbregistrering/sykemelding/resources/SykemeldingResource.java
@@ -6,12 +6,13 @@ import no.nav.fo.veilarbregistrering.bruker.BrukerAdapter;
 import no.nav.fo.veilarbregistrering.bruker.UserService;
 import no.nav.fo.veilarbregistrering.sykemelding.SykemeldingService;
 import no.nav.fo.veilarbregistrering.sykemelding.SykmeldtInfoData;
-import no.nav.sbl.featuretoggle.unleash.UnleashService;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
+import static no.nav.fo.veilarbregistrering.bruker.UserService.Kilde.PDL;
 
 @Component
 @Path("/")
@@ -21,31 +22,24 @@ public class SykemeldingResource implements SykemeldingApi {
     private final SykemeldingService sykemeldingService;
     private final UserService userService;
     private final VeilarbAbacPepClient pepClient;
-    private final UnleashService unleashService;
 
     public SykemeldingResource(
             VeilarbAbacPepClient pepClient,
             UserService userService,
-            SykemeldingService sykemeldingService,
-            UnleashService unleashService) {
+            SykemeldingService sykemeldingService) {
         this.pepClient = pepClient;
         this.userService = userService;
         this.sykemeldingService = sykemeldingService;
-        this.unleashService = unleashService;
     }
 
     @GET
     @Path("/sykmeldtinfodata")
     @Override
     public SykmeldtInfoData hentSykmeldtInfoData() {
-        final Bruker bruker = userService.hentBrukerFra(velgKilde());
+        final Bruker bruker = userService.hentBrukerFra(PDL);
 
         pepClient.sjekkLesetilgangTilBruker(BrukerAdapter.map(bruker));
 
         return sykemeldingService.hentSykmeldtInfoData(bruker.getGjeldendeFoedselsnummer());
-    }
-
-    private UserService.Kilde velgKilde() {
-        return unleashService.isEnabled("veilarbregistrering.sykemelding.kildePdl") ? UserService.Kilde.PDL : UserService.Kilde.AKTOR;
     }
 }
