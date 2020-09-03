826782870544ee89e059153a66ed3af063d08d44
Terje Heen <terje.heen@nav.no>
2020-09-03 09:54:41 +0200

    Fjerner toggle for Oppgave ifm. PDL-integrasjon
    PDL-integrasjon er testet og verifisert OK.

--- src/main/java/no/nav/fo/veilarbregistrering/bruker/UserService.java
+++ src/main/java/no/nav/fo/veilarbregistrering/bruker/UserService.java
@@ -31,7 +31,7 @@ public class UserService {
         }
     }
 
-    public Bruker finnBrukerGjennomPdl() {
+    private Bruker finnBrukerGjennomPdl() {
         Foedselsnummer fnr = hentFnrFraUrlEllerToken();
         return finnBrukerGjennomPdl(fnr);
     }
