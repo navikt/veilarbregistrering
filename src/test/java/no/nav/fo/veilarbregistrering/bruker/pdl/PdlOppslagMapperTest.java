64a709ed9c0f2a929953994e81d04a633522c0f5
Terje Heen <terje.heen@nav.no>
2020-09-14 15:09:23 +0200

    Tilbyr geografiks tilknytning på Person-objektet

--- src/test/java/no/nav/fo/veilarbregistrering/bruker/pdl/PdlOppslagMapperTest.java
+++ src/test/java/no/nav/fo/veilarbregistrering/bruker/pdl/PdlOppslagMapperTest.java
@@ -1,9 +1,6 @@
 package no.nav.fo.veilarbregistrering.bruker.pdl;
 
-import no.nav.fo.veilarbregistrering.bruker.Gruppe;
-import no.nav.fo.veilarbregistrering.bruker.Identer;
-import no.nav.fo.veilarbregistrering.bruker.Opphold;
-import no.nav.fo.veilarbregistrering.bruker.Person;
+import no.nav.fo.veilarbregistrering.bruker.*;
 import no.nav.fo.veilarbregistrering.bruker.pdl.hentIdenter.PdlGruppe;
 import no.nav.fo.veilarbregistrering.bruker.pdl.hentIdenter.PdlIdent;
 import no.nav.fo.veilarbregistrering.bruker.pdl.hentIdenter.PdlIdenter;
@@ -86,5 +83,66 @@ public class PdlOppslagMapperTest {
         assertThat(identer.getIdenter().get(0).getIdent()).isEqualTo("12345678910");
         assertThat(identer.getIdenter().get(0).isHistorisk()).isEqualTo(false);
         assertThat(identer.getIdenter().get(0).getGruppe()).isEqualTo(Gruppe.FOLKEREGISTERIDENT);
+    }
+
+    @Test
+    public void skal_mappe_utland_uten_gtLand_til_NOR() {
+        PdlPersonOpphold pdlPersonOpphold = new PdlPersonOpphold();
+        pdlPersonOpphold.setType(Oppholdstype.PERMANENT);
+
+        PdlStatsborgerskap statsborgerskap = new PdlStatsborgerskap();
+        statsborgerskap.setLand("NOR");
+
+        PdlPerson pdlPerson = new PdlPerson();
+        pdlPerson.setOpphold(singletonList(pdlPersonOpphold));
+        pdlPerson.setStatsborgerskap(singletonList(statsborgerskap));
+
+        PdlFoedsel pdlFoedsel = new PdlFoedsel();
+        pdlFoedsel.setFoedselsdato(LocalDate.of(1970, 3, 23));
+        pdlPerson.setFoedsel(singletonList(pdlFoedsel));
+
+        PdlTelefonnummer pdlTelefonnummer = new PdlTelefonnummer();
+        pdlTelefonnummer.setLandskode("0047");
+        pdlTelefonnummer.setNummer("94242425");
+        pdlPerson.setTelefonnummer(singletonList(pdlTelefonnummer));
+
+        PdlGeografiskTilknytning geografiskTilknytning = new PdlGeografiskTilknytning();
+        geografiskTilknytning.setGtType(GtType.UTLAND);
+        pdlPerson.setGeografiskTilknytning(geografiskTilknytning);
+
+        Person person = PdlOppslagMapper.map(pdlPerson);
+
+        assertThat(person.getGeografiskTilknytning()).hasValue(GeografiskTilknytning.of("NOR"));
+    }
+
+    @Test
+    public void skal_mappe_utland_med_gtLand_til_landkode() {
+        PdlPersonOpphold pdlPersonOpphold = new PdlPersonOpphold();
+        pdlPersonOpphold.setType(Oppholdstype.PERMANENT);
+
+        PdlStatsborgerskap statsborgerskap = new PdlStatsborgerskap();
+        statsborgerskap.setLand("NOR");
+
+        PdlPerson pdlPerson = new PdlPerson();
+        pdlPerson.setOpphold(singletonList(pdlPersonOpphold));
+        pdlPerson.setStatsborgerskap(singletonList(statsborgerskap));
+
+        PdlFoedsel pdlFoedsel = new PdlFoedsel();
+        pdlFoedsel.setFoedselsdato(LocalDate.of(1970, 3, 23));
+        pdlPerson.setFoedsel(singletonList(pdlFoedsel));
+
+        PdlTelefonnummer pdlTelefonnummer = new PdlTelefonnummer();
+        pdlTelefonnummer.setLandskode("0047");
+        pdlTelefonnummer.setNummer("94242425");
+        pdlPerson.setTelefonnummer(singletonList(pdlTelefonnummer));
+
+        PdlGeografiskTilknytning geografiskTilknytning = new PdlGeografiskTilknytning();
+        geografiskTilknytning.setGtType(GtType.UTLAND);
+        geografiskTilknytning.setGtLand("POL");
+        pdlPerson.setGeografiskTilknytning(geografiskTilknytning);
+
+        Person person = PdlOppslagMapper.map(pdlPerson);
+
+        assertThat(person.getGeografiskTilknytning()).hasValue(GeografiskTilknytning.of("POL"));
     }
 }
