213659b06a7dff690124beeea56724cd92e45082
Terje Heen <terje.heen@nav.no>
2020-09-28 16:14:16 +0200

    Toggle for bruk av Norg2 via Rest
    Legger til toggle for å kunne sammenligne svar fra Norg2 via SOAP og Rest. Hvis Rest-tjenesten fungerer, og svarene er like, vil vi kunne bytte til Rest, og fjerne SOAP-integrasjonen.

--- src/main/java/no/nav/fo/veilarbregistrering/registrering/manuell/ManuellRegistreringService.java
+++ src/main/java/no/nav/fo/veilarbregistrering/registrering/manuell/ManuellRegistreringService.java
@@ -5,6 +5,7 @@ import no.nav.fo.veilarbregistrering.orgenhet.HentEnheterGateway;
 import no.nav.fo.veilarbregistrering.orgenhet.NavEnhet;
 import no.nav.fo.veilarbregistrering.orgenhet.Norg2Gateway;
 import no.nav.fo.veilarbregistrering.registrering.BrukerRegistreringType;
+import no.nav.sbl.featuretoggle.unleash.UnleashService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,14 +19,17 @@ public class ManuellRegistreringService {
     private final ManuellRegistreringRepository manuellRegistreringRepository;
     private final HentEnheterGateway hentEnheterGateway;
     private final Norg2Gateway norg2Gateway;
+    private final UnleashService unleashService;
 
     public ManuellRegistreringService(
             ManuellRegistreringRepository manuellRegistreringRepository,
             HentEnheterGateway hentEnheterGateway,
-            Norg2Gateway norg2Gateway) {
+            Norg2Gateway norg2Gateway,
+            UnleashService unleashService) {
         this.manuellRegistreringRepository = manuellRegistreringRepository;
         this.hentEnheterGateway = hentEnheterGateway;
         this.norg2Gateway = norg2Gateway;
+        this.unleashService = unleashService;
     }
 
     public void lagreManuellRegistrering(
@@ -54,9 +58,22 @@ public class ManuellRegistreringService {
 
         Optional<NavEnhet> enhet = finnEnhet(Enhetnr.of(registrering.getVeilederEnhetId()));
 
+        if (norg2ViaRest()) {
+            try {
+                Optional<NavEnhet> navEnhet = finnEnhetViaRest(Enhetnr.of(registrering.getVeilederEnhetId()));
+                LOG.info(String.format("Lik nav-enhet: %s, SOAP: %s, Rest: %s", enhet.equals(navEnhet) ? "Ja" : "Nei", enhet, navEnhet));
+            } catch (RuntimeException e) {
+                LOG.error("Sammenligning av NavEnhet feilet. Har ingen betydning", e);
+            }
+        }
+
         return new Veileder()
                 .setEnhet(enhet.orElse(null))
                 .setIdent(registrering.getVeilederIdent());
+    }
+
+    private boolean norg2ViaRest() {
+        return unleashService.isEnabled("veilarbregistrering.registrering.norg2viaRest");
     }
 
     Optional<NavEnhet> finnEnhet(Enhetnr enhetId) {
