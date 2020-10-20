353c729c37cebc2ce9f6ec0643d82aa868dcc143
theagilepadawan <michael.gundersen@nav.no>
2020-10-20 16:47:16 +0200

    Null checking
    Mapper også fra eksternt til internt API et steg "før" for en mer clean builder

--- src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/ArbeidssokerRegistrertInternalEvent.java
+++ src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/ArbeidssokerRegistrertInternalEvent.java
@@ -1,12 +1,11 @@
 package no.nav.fo.veilarbregistrering.registrering.bruker;
 
 import no.nav.fo.veilarbregistrering.besvarelse.DinSituasjonSvar;
-import no.nav.fo.veilarbregistrering.besvarelse.UtdanningBestattSvar;
-import no.nav.fo.veilarbregistrering.besvarelse.UtdanningGodkjentSvar;
-import no.nav.fo.veilarbregistrering.besvarelse.UtdanningSvar;
 import no.nav.fo.veilarbregistrering.bruker.AktorId;
 
 import java.time.LocalDateTime;
+
+import static java.util.Optional.ofNullable;
 
 public class ArbeidssokerRegistrertInternalEvent {
 
@@ -26,20 +25,66 @@ public class ArbeidssokerRegistrertInternalEvent {
         return brukerRegistrering.getBrukersSituasjon();
     }
 
-    public UtdanningSvar getUtdanningSvar() {
-        return brukerRegistrering.getUtdanningSvar();
+    public no.nav.arbeid.soker.registrering.UtdanningSvar getUtdanningSvar() {
+        return ofNullable(brukerRegistrering.getUtdanningSvar()).map(ArbeidssokerRegistrertInternalEvent::map).orElse(null);
     }
 
-    public UtdanningBestattSvar getUtdanningBestattSvar() {
-        return brukerRegistrering.getUtdanningBestattSvar();
+    public no.nav.arbeid.soker.registrering.UtdanningBestattSvar getUtdanningBestattSvar() {
+        return ofNullable(brukerRegistrering.getUtdanningBestattSvar()).map(ArbeidssokerRegistrertInternalEvent::map).orElse(null);
     }
 
-    public UtdanningGodkjentSvar getUtdanningGodkjentSvar() {
-        return brukerRegistrering.getUtdanningGodkjentSvar();
+    public no.nav.arbeid.soker.registrering.UtdanningGodkjentSvar getUtdanningGodkjentSvar() {
+        return ofNullable(brukerRegistrering.getUtdanningGodkjentSvar()).map(ArbeidssokerRegistrertInternalEvent::map).orElse(null);
     }
 
 
     public LocalDateTime getOpprettetDato() {
         return brukerRegistrering.getOpprettetDato();
+    }
+
+
+    private static no.nav.arbeid.soker.registrering.UtdanningGodkjentSvar map(no.nav.fo.veilarbregistrering.besvarelse.UtdanningGodkjentSvar utdanningGodkjentSvar) {
+        switch (utdanningGodkjentSvar) {
+            case JA:
+                return no.nav.arbeid.soker.registrering.UtdanningGodkjentSvar.JA;
+            case NEI:
+                return no.nav.arbeid.soker.registrering.UtdanningGodkjentSvar.NEI;
+            case INGEN_SVAR:
+                return no.nav.arbeid.soker.registrering.UtdanningGodkjentSvar.INGEN_SVAR;
+        }
+        throw new IllegalArgumentException(String.format("Ukjent svar valgt %s", utdanningGodkjentSvar));
+    }
+
+    private static no.nav.arbeid.soker.registrering.UtdanningBestattSvar map(no.nav.fo.veilarbregistrering.besvarelse.UtdanningBestattSvar utdanningBestattSvar) {
+        switch (utdanningBestattSvar) {
+            case JA:
+                return no.nav.arbeid.soker.registrering.UtdanningBestattSvar.JA;
+            case NEI:
+                return no.nav.arbeid.soker.registrering.UtdanningBestattSvar.NEI;
+            case INGEN_SVAR:
+                return no.nav.arbeid.soker.registrering.UtdanningBestattSvar.INGEN_SVAR;
+        }
+        throw new IllegalArgumentException(String.format("Ukjent svar valgt %s", utdanningBestattSvar));
+    }
+
+    private static no.nav.arbeid.soker.registrering.UtdanningSvar map(no.nav.fo.veilarbregistrering.besvarelse.UtdanningSvar utdanningSvar) {
+        switch (utdanningSvar) {
+
+            case INGEN_UTDANNING:
+                return no.nav.arbeid.soker.registrering.UtdanningSvar.INGEN_UTDANNING;
+            case GRUNNSKOLE:
+                return no.nav.arbeid.soker.registrering.UtdanningSvar.GRUNNSKOLE;
+            case VIDEREGAENDE_GRUNNUTDANNING:
+                return no.nav.arbeid.soker.registrering.UtdanningSvar.VIDEREGAENDE_GRUNNUTDANNING;
+            case VIDEREGAENDE_FAGBREV_SVENNEBREV:
+                return no.nav.arbeid.soker.registrering.UtdanningSvar.VIDEREGAENDE_FAGBREV_SVENNEBREV;
+            case HOYERE_UTDANNING_1_TIL_4:
+                return no.nav.arbeid.soker.registrering.UtdanningSvar.HOYERE_UTDANNING_1_TIL_4;
+            case HOYERE_UTDANNING_5_ELLER_MER:
+                return no.nav.arbeid.soker.registrering.UtdanningSvar.HOYERE_UTDANNING_5_ELLER_MER;
+            case INGEN_SVAR:
+                return no.nav.arbeid.soker.registrering.UtdanningSvar.INGEN_SVAR;
+        }
+        throw new IllegalArgumentException(String.format("Ukjent utdanning valgt %s", utdanningSvar));
     }
 }
