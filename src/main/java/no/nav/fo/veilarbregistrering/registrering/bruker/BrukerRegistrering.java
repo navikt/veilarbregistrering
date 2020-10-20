ea2b3d8c8c45e08611947ca829113bd37bfae11f
theagilepadawan <michael.gundersen@nav.no>
2020-10-20 14:55:41 +0200

    Mapper utdanning til det interne event objektet som sendes på Kafka
    Deler mer informasjon av registreringen med PTO over kafka.

--- src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/BrukerRegistrering.java
+++ src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/BrukerRegistrering.java
@@ -2,6 +2,9 @@ package no.nav.fo.veilarbregistrering.registrering.bruker;
 
 import lombok.Data;
 import no.nav.fo.veilarbregistrering.besvarelse.DinSituasjonSvar;
+import no.nav.fo.veilarbregistrering.besvarelse.UtdanningBestattSvar;
+import no.nav.fo.veilarbregistrering.besvarelse.UtdanningGodkjentSvar;
+import no.nav.fo.veilarbregistrering.besvarelse.UtdanningSvar;
 import no.nav.fo.veilarbregistrering.registrering.BrukerRegistreringType;
 import no.nav.fo.veilarbregistrering.registrering.manuell.Veileder;
 
@@ -15,4 +18,10 @@ public abstract class BrukerRegistrering {
     public abstract long getId();
 
     public abstract DinSituasjonSvar getBrukersSituasjon();
+
+    public abstract UtdanningSvar getUtdanningSvar();
+
+    public abstract UtdanningBestattSvar getUtdanningBestattSvar();
+
+    public abstract UtdanningGodkjentSvar getUtdanningGodkjentSvar();
 }
