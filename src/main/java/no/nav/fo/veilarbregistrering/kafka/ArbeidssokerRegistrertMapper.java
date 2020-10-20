b4569907185164afeb8eccac2a7b9a830a4e5ad5
theagilepadawan <michael.gundersen@nav.no>
2020-10-20 16:47:16 +0200

    Null checking
    Mapper også fra eksternt til internt API et steg "før" for en mer clean builder

--- src/main/java/no/nav/fo/veilarbregistrering/kafka/ArbeidssokerRegistrertMapper.java
+++ src/main/java/no/nav/fo/veilarbregistrering/kafka/ArbeidssokerRegistrertMapper.java
@@ -1,9 +1,6 @@
 package no.nav.fo.veilarbregistrering.kafka;
 
 import no.nav.arbeid.soker.registrering.ArbeidssokerRegistrertEvent;
-import no.nav.arbeid.soker.registrering.UtdanningBestattSvar;
-import no.nav.arbeid.soker.registrering.UtdanningGodkjentSvar;
-import no.nav.arbeid.soker.registrering.UtdanningSvar;
 import no.nav.fo.veilarbregistrering.registrering.bruker.ArbeidssokerRegistrertInternalEvent;
 
 import java.time.ZoneId;
@@ -23,54 +20,9 @@ class ArbeidssokerRegistrertMapper {
                         ZonedDateTime.of(
                                 event.getOpprettetDato(),
                                 ZoneId.systemDefault()).format(ISO_ZONED_DATE_TIME))
-                .setUtdanning(map(event.getUtdanningSvar()))
-                .setUtdanningBestatt(map(event.getUtdanningBestattSvar()))
-                .setUtdanningGodkjent(map(event.getUtdanningGodkjentSvar()))
+                .setUtdanning(event.getUtdanningSvar())
+                .setUtdanningBestatt(event.getUtdanningBestattSvar())
+                .setUtdanningGodkjent(event.getUtdanningGodkjentSvar())
                 .build();
-    }
-
-    private static UtdanningGodkjentSvar map(no.nav.fo.veilarbregistrering.besvarelse.UtdanningGodkjentSvar utdanningGodkjentSvar) {
-        switch (utdanningGodkjentSvar) {
-            case JA:
-                return UtdanningGodkjentSvar.JA;
-            case NEI:
-                return UtdanningGodkjentSvar.NEI;
-            case INGEN_SVAR:
-                return UtdanningGodkjentSvar.INGEN_SVAR;
-        }
-        throw new IllegalArgumentException(String.format("Ukjent svar valgt %s", utdanningGodkjentSvar));
-    }
-
-    private static UtdanningBestattSvar map(no.nav.fo.veilarbregistrering.besvarelse.UtdanningBestattSvar utdanningBestattSvar) {
-        switch (utdanningBestattSvar) {
-            case JA:
-                return UtdanningBestattSvar.JA;
-            case NEI:
-                return UtdanningBestattSvar.NEI;
-            case INGEN_SVAR:
-                return UtdanningBestattSvar.INGEN_SVAR;
-        }
-        throw new IllegalArgumentException(String.format("Ukjent svar valgt %s", utdanningBestattSvar));
-    }
-
-    private static UtdanningSvar map(no.nav.fo.veilarbregistrering.besvarelse.UtdanningSvar utdanningSvar) {
-        switch (utdanningSvar) {
-
-            case INGEN_UTDANNING:
-                return UtdanningSvar.INGEN_UTDANNING;
-            case GRUNNSKOLE:
-                return UtdanningSvar.GRUNNSKOLE;
-            case VIDEREGAENDE_GRUNNUTDANNING:
-                return UtdanningSvar.VIDEREGAENDE_GRUNNUTDANNING;
-            case VIDEREGAENDE_FAGBREV_SVENNEBREV:
-                return UtdanningSvar.VIDEREGAENDE_FAGBREV_SVENNEBREV;
-            case HOYERE_UTDANNING_1_TIL_4:
-                return UtdanningSvar.HOYERE_UTDANNING_1_TIL_4;
-            case HOYERE_UTDANNING_5_ELLER_MER:
-                return UtdanningSvar.HOYERE_UTDANNING_5_ELLER_MER;
-            case INGEN_SVAR:
-                return UtdanningSvar.INGEN_SVAR;
-        }
-        throw new IllegalArgumentException(String.format("Ukjent utdanning valgt %s", utdanningSvar));
     }
 }
