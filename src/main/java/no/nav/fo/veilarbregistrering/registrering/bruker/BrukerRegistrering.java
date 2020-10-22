e11ff6376346bdf86ed0df1de26c30566ad7da2c
Terje Heen <terje.heen@nav.no>
2020-10-22 11:15:29 +0200

    Fjerner metoder som ikke lenger er i bruk.

--- src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/BrukerRegistrering.java
+++ src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/BrukerRegistrering.java
@@ -1,10 +1,6 @@
 package no.nav.fo.veilarbregistrering.registrering.bruker;
 
 import lombok.Data;
-import no.nav.fo.veilarbregistrering.besvarelse.DinSituasjonSvar;
-import no.nav.fo.veilarbregistrering.besvarelse.UtdanningBestattSvar;
-import no.nav.fo.veilarbregistrering.besvarelse.UtdanningGodkjentSvar;
-import no.nav.fo.veilarbregistrering.besvarelse.UtdanningSvar;
 import no.nav.fo.veilarbregistrering.registrering.BrukerRegistreringType;
 import no.nav.fo.veilarbregistrering.registrering.manuell.Veileder;
 
@@ -17,11 +13,4 @@ public abstract class BrukerRegistrering {
 
     public abstract long getId();
 
-    public abstract DinSituasjonSvar getBrukersSituasjon();
-
-    public abstract UtdanningSvar getUtdanningSvar();
-
-    public abstract UtdanningBestattSvar getUtdanningBestattSvar();
-
-    public abstract UtdanningGodkjentSvar getUtdanningGodkjentSvar();
 }
