ae53b09d1a72dcbd04b1d8bf17d123174abfb31f
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2020-10-30 15:52:55 +0100

    Kotlinifisering av testmetoder

--- src/test/kotlin/OppgaveRouterTest.kt
+++ src/test/kotlin/OppgaveRouterTest.kt
@@ -18,7 +18,6 @@ import no.nav.fo.veilarbregistrering.orgenhet.Norg2Gateway
 import no.nav.sbl.featuretoggle.unleash.UnleashService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito
 import java.time.LocalDate
 import java.util.*
 
@@ -26,146 +25,108 @@ class OppgaveRouterTest {
 
     @Test
     fun `ingen arbeidsforhold skal gi intern brukerstotte`() {
-        val arbeidsforholdGateway = ArbeidsforholdGateway { fnr: Foedselsnummer? -> FlereArbeidsforhold.of(emptyList()) }
-        val enhetGateway = EnhetGateway { organisasjonsnummer: Organisasjonsnummer? -> Optional.empty() }
-        val norg2Gateway: Norg2Gateway = StubNorg2Gateway()
-        val personGateway = PersonGateway { foedselsnummer: Foedselsnummer? -> Optional.empty() }
-        val unleashService = Mockito.mock(UnleashService::class.java)
-        val pdlOppslagGateway = Mockito.mock(PdlOppslagGateway::class.java)
-        val oppgaveRouter = oppgaveRouter(arbeidsforholdGateway, enhetGateway, norg2Gateway, personGateway, unleashService, pdlOppslagGateway)
+        val arbeidsforholdGateway = ArbeidsforholdGateway { FlereArbeidsforhold.of(emptyList()) }
+        val oppgaveRouter = oppgaveRouter(arbeidsforholdGateway = arbeidsforholdGateway)
         val enhetsnr = oppgaveRouter.hentEnhetsnummerFor(BRUKER)
         assertThat(enhetsnr).hasValue(Enhetnr.of("2930"))
     }
 
     @Test
     fun `ingen enhet for orgnummer skal gi intern brukerstotte`() {
-        val arbeidsforholdGateway = ArbeidsforholdGateway { fnr: Foedselsnummer? -> FlereArbeidsforholdTestdataBuilder.flereArbeidsforholdTilfeldigSortert() }
-        val enhetGateway = EnhetGateway { organisasjonsnummer: Organisasjonsnummer? -> Optional.empty() }
-        val norg2Gateway: Norg2Gateway = StubNorg2Gateway()
-        val personGateway = PersonGateway { foedselsnummer: Foedselsnummer? -> Optional.empty() }
-        val unleashService = Mockito.mock(UnleashService::class.java)
-        val pdlOppslagGateway = Mockito.mock(PdlOppslagGateway::class.java)
-        val oppgaveRouter = oppgaveRouter(arbeidsforholdGateway, enhetGateway, norg2Gateway, personGateway, unleashService, pdlOppslagGateway)
+        val oppgaveRouter = oppgaveRouter()
         val enhetsnr = oppgaveRouter.hentEnhetsnummerFor(BRUKER)
         assertThat(enhetsnr).hasValue(Enhetnr.of("2930"))
     }
 
     @Test
     fun `ingen navenhet for organisasjon skal gi intern brukerstotte`() {
-        val arbeidsforholdGateway = ArbeidsforholdGateway { fnr: Foedselsnummer? -> FlereArbeidsforholdTestdataBuilder.flereArbeidsforholdTilfeldigSortert() }
         val forretningsadresse = Forretningsadresse(
                 Kommunenummer.of("1240"),
                 Periode.of(LocalDate.of(2020, 1, 1), null))
-        val enhetGateway = EnhetGateway { organisasjonsnummer: Organisasjonsnummer? -> Optional.of(Organisasjonsdetaljer.of(listOf(forretningsadresse), emptyList())) }
-        val norg2Gateway: Norg2Gateway = StubNorg2Gateway()
-        val personGateway = PersonGateway { foedselsnummer: Foedselsnummer? -> Optional.empty() }
-        val unleashService = Mockito.mock(UnleashService::class.java)
-        val pdlOppslagGateway = Mockito.mock(PdlOppslagGateway::class.java)
-        val oppgaveRouter = oppgaveRouter(arbeidsforholdGateway, enhetGateway, norg2Gateway, personGateway, unleashService, pdlOppslagGateway)
+        val enhetGateway = EnhetGateway { Optional.of(Organisasjonsdetaljer.of(listOf(forretningsadresse), emptyList())) }
+        val oppgaveRouter = oppgaveRouter(enhetGateway = enhetGateway)
         val enhetsnr = oppgaveRouter.hentEnhetsnummerFor(BRUKER)
         assertThat(enhetsnr).hasValue(Enhetnr.of("2930"))
     }
 
     @Test
     fun `enhetsnummer skal returneres nar alle koblingen til arbeidsforhold er komplett`() {
-        val arbeidsforholdGateway = ArbeidsforholdGateway { fnr: Foedselsnummer? -> FlereArbeidsforholdTestdataBuilder.flereArbeidsforholdTilfeldigSortert() }
         val forretningsadresse = Forretningsadresse(
                 Kommunenummer.of("1241"),
                 Periode.of(LocalDate.of(2020, 1, 1), null))
-        val enhetGateway = EnhetGateway { organisasjonsnummer: Organisasjonsnummer? -> Optional.of(Organisasjonsdetaljer.of(listOf(forretningsadresse), emptyList())) }
-        val norg2Gateway: Norg2Gateway = StubNorg2Gateway()
-        val personGateway = PersonGateway { foedselsnummer: Foedselsnummer? -> Optional.empty() }
-        val unleashService = Mockito.mock(UnleashService::class.java)
-        val pdlOppslagGateway = Mockito.mock(PdlOppslagGateway::class.java)
-        val oppgaveRouter = oppgaveRouter(arbeidsforholdGateway, enhetGateway, norg2Gateway, personGateway, unleashService, pdlOppslagGateway)
+        val enhetGateway = EnhetGateway { Optional.of(Organisasjonsdetaljer.of(listOf(forretningsadresse), emptyList())) }
+        val oppgaveRouter = oppgaveRouter(enhetGateway = enhetGateway)
         val enhetsnr = oppgaveRouter.hentEnhetsnummerFor(BRUKER)
         assertThat(enhetsnr).hasValue(Enhetnr.of("232"))
     }
 
     @Test
     fun `geografisk tilknytning med by med bydel skal gi intern brukerstotte`() {
-        val arbeidsforholdGateway = ArbeidsforholdGateway { fnr: Foedselsnummer? -> FlereArbeidsforholdTestdataBuilder.flereArbeidsforholdTilfeldigSortert() }
-        val enhetGateway = EnhetGateway { organisasjonsnummer: Organisasjonsnummer? -> Optional.empty() }
-        val norg2Gateway: Norg2Gateway = StubNorg2Gateway()
-        val personGateway = PersonGateway { foedselsnummer: Foedselsnummer? -> Optional.of(GeografiskTilknytning.of("0301")) }
-        val unleashService = Mockito.mock(UnleashService::class.java)
-        val pdlOppslagGateway = Mockito.mock(PdlOppslagGateway::class.java)
-        val oppgaveRouter = oppgaveRouter(arbeidsforholdGateway, enhetGateway, norg2Gateway, personGateway, unleashService, pdlOppslagGateway)
+        val personGateway = PersonGateway { Optional.of(GeografiskTilknytning.of("0301")) }
+
+        val oppgaveRouter = oppgaveRouter(personGateway = personGateway)
         val enhetsnr = oppgaveRouter.hentEnhetsnummerFor(BRUKER)
         assertThat(enhetsnr).hasValue(Enhetnr.internBrukerstotte())
     }
 
     @Test
     fun `geografisk tilknytning med unntak av landkode skal gi empty enhetsnummer`() {
-        val arbeidsforholdGateway = ArbeidsforholdGateway { fnr: Foedselsnummer? -> FlereArbeidsforholdTestdataBuilder.flereArbeidsforholdTilfeldigSortert() }
-        val enhetGateway = EnhetGateway { organisasjonsnummer: Organisasjonsnummer? -> Optional.empty() }
-        val norg2Gateway: Norg2Gateway = StubNorg2Gateway()
-        val personGateway = PersonGateway { foedselsnummer: Foedselsnummer? -> Optional.of(GeografiskTilknytning.of("030106")) }
-        val unleashService = Mockito.mock(UnleashService::class.java)
-        val pdlOppslagGateway = Mockito.mock(PdlOppslagGateway::class.java)
-        val oppgaveRouter = oppgaveRouter(arbeidsforholdGateway, enhetGateway, norg2Gateway, personGateway, unleashService, pdlOppslagGateway)
+        val personGateway = PersonGateway { Optional.of(GeografiskTilknytning.of("030106")) }
+
+        val oppgaveRouter = oppgaveRouter(personGateway = personGateway)
         val enhetsnr = oppgaveRouter.hentEnhetsnummerFor(BRUKER)
         assertThat(enhetsnr).isEmpty
     }
 
     @Test
     fun `geografisk tilknytning med landkode skal bruke arbeidsforhold til routing`() {
-        val arbeidsforholdGateway = ArbeidsforholdGateway { fnr: Foedselsnummer? -> FlereArbeidsforholdTestdataBuilder.flereArbeidsforholdTilfeldigSortert() }
         val forretningsadresse = Forretningsadresse(
                 Kommunenummer.of("1241"),
                 Periode.of(LocalDate.of(2020, 1, 1), null))
-        val enhetGateway = EnhetGateway { organisasjonsnummer: Organisasjonsnummer? -> Optional.of(Organisasjonsdetaljer.of(listOf(forretningsadresse), emptyList())) }
-        val norg2Gateway: Norg2Gateway = StubNorg2Gateway()
-        val personGateway = PersonGateway { foedselsnummer: Foedselsnummer? -> Optional.of(GeografiskTilknytning.of("DNK")) }
-        val unleashService = Mockito.mock(UnleashService::class.java)
-        val pdlOppslagGateway = Mockito.mock(PdlOppslagGateway::class.java)
-        val oppgaveRouter = oppgaveRouter(arbeidsforholdGateway, enhetGateway, norg2Gateway, personGateway, unleashService, pdlOppslagGateway)
+        val enhetGateway = EnhetGateway { Optional.of(Organisasjonsdetaljer.of(listOf(forretningsadresse), emptyList())) }
+        val personGateway = PersonGateway { Optional.of(GeografiskTilknytning.of("DNK")) }
+
+        val oppgaveRouter = oppgaveRouter(enhetGateway = enhetGateway, personGateway = personGateway)
         val enhetsnr = oppgaveRouter.hentEnhetsnummerFor(BRUKER)
         assertThat(enhetsnr).hasValue(Enhetnr.of("232"))
     }
 
     @Test
     fun `kommunenummer tilhorende kommune med bydeler skal tildeles intern brukerstotte`() {
-        val arbeidsforholdGateway = ArbeidsforholdGateway { fnr: Foedselsnummer? -> FlereArbeidsforholdTestdataBuilder.flereArbeidsforholdTilfeldigSortert() }
         val forretningsadresse = Forretningsadresse(
                 Kommunenummer.of(KommuneMedBydel.STAVANGER),
                 Periode.of(LocalDate.of(2020, 1, 1), null))
-        val enhetGateway = EnhetGateway { organisasjonsnummer: Organisasjonsnummer? -> Optional.of(Organisasjonsdetaljer.of(listOf(forretningsadresse), emptyList())) }
-        val norg2Gateway: Norg2Gateway = StubNorg2Gateway()
-        val personGateway = PersonGateway { foedselsnummer: Foedselsnummer? -> Optional.of(GeografiskTilknytning.of("DNK")) }
-        val unleashService = Mockito.mock(UnleashService::class.java)
-        val pdlOppslagGateway = Mockito.mock(PdlOppslagGateway::class.java)
-        val oppgaveRouter = oppgaveRouter(arbeidsforholdGateway, enhetGateway, norg2Gateway, personGateway, unleashService, pdlOppslagGateway)
+        val enhetGateway = EnhetGateway { Optional.of(Organisasjonsdetaljer.of(listOf(forretningsadresse), emptyList())) }
+        val personGateway = PersonGateway { Optional.of(GeografiskTilknytning.of("DNK")) }
+
+        val oppgaveRouter = oppgaveRouter(enhetGateway = enhetGateway, personGateway = personGateway)
         val enhetsnr = oppgaveRouter.hentEnhetsnummerFor(BRUKER)
         assertThat(enhetsnr).hasValue(Enhetnr.internBrukerstotte())
     }
 
     @Test
     fun `brukere med adressebeskyttelse overlates til oppgave api`() {
-        val arbeidsforholdGateway = ArbeidsforholdGateway { fnr: Foedselsnummer? -> FlereArbeidsforholdTestdataBuilder.flereArbeidsforholdTilfeldigSortert() }
-        val enhetGateway = EnhetGateway { organisasjonsnummer: Organisasjonsnummer? -> Optional.empty() }
-        val norg2Gateway: Norg2Gateway = StubNorg2Gateway()
-        val personGateway = PersonGateway { foedselsnummer: Foedselsnummer? -> Optional.of(GeografiskTilknytning.of("030106")) }
+        val personGateway = PersonGateway { Optional.of(GeografiskTilknytning.of("030106")) }
         val unleashService = unleashServiceMedFeatures("veilarbregistrering.adressebeskyttelse")
-        val pdlOppslagGateway = Mockito.mock(PdlOppslagGateway::class.java)
         val person = Person.of(null, null, null, null, AdressebeskyttelseGradering.STRENGT_FORTROLIG)
-        whenever(pdlOppslagGateway.hentPerson(BRUKER.aktorId)).thenReturn(Optional.of(person))
-        val oppgaveRouter = oppgaveRouter(arbeidsforholdGateway, enhetGateway, norg2Gateway, personGateway, unleashService, pdlOppslagGateway)
+        val pdlOppslagGateway = StubPdlOppslagGateway(users = mapOf(BRUKER.aktorId to person))
+        val oppgaveRouter = oppgaveRouter(personGateway = personGateway, unleashService = unleashService, pdlOppslagGateway = pdlOppslagGateway)
+
         val enhetsnr = oppgaveRouter.hentEnhetsnummerFor(BRUKER)
         assertThat(enhetsnr).isEmpty
     }
 
     private fun oppgaveRouter(
-            arbeidsforholdGateway: ArbeidsforholdGateway,
-            enhetGateway: EnhetGateway,
-            norg2Gateway: Norg2Gateway,
-            personGateway: PersonGateway,
-            unleashService: UnleashService,
-            pdlOppslagGateway: PdlOppslagGateway
+            arbeidsforholdGateway: ArbeidsforholdGateway = StubArbeidsforholdGateway(),
+            enhetGateway: EnhetGateway = StubEnhetGateway(),
+            norg2Gateway: Norg2Gateway = StubNorg2Gateway(),
+            personGateway: PersonGateway = StubPersonGateway(),
+            unleashService: UnleashService = mock(),
+            pdlOppslagGateway: PdlOppslagGateway = StubPdlOppslagGateway()
     ) =
             OppgaveRouter(arbeidsforholdGateway, enhetGateway, norg2Gateway, personGateway, unleashService, pdlOppslagGateway)
 
-    internal class StubNorg2Gateway : Norg2Gateway {
+    private class StubNorg2Gateway : Norg2Gateway {
         override fun hentEnhetFor(kommunenummer: Kommunenummer): Optional<Enhetnr> {
             if (Kommunenummer.of("1241") == kommunenummer) {
                 return Optional.of(Enhetnr.of("232"))
@@ -176,6 +137,33 @@ class OppgaveRouterTest {
         }
 
         override fun hentAlleEnheter(): Map<Enhetnr, NavEnhet> = emptyMap()
+    }
+
+    private class StubArbeidsforholdGateway : ArbeidsforholdGateway {
+        override fun hentArbeidsforhold(fnr: Foedselsnummer?) =
+                FlereArbeidsforholdTestdataBuilder.flereArbeidsforholdTilfeldigSortert()
+    }
+
+    private class StubPersonGateway : PersonGateway {
+        override fun hentGeografiskTilknytning(foedselsnummer: Foedselsnummer?) = Optional.empty<GeografiskTilknytning>()
+    }
+
+    private class StubEnhetGateway : EnhetGateway {
+        override fun hentOrganisasjonsdetaljer(organisasjonsnummer: Organisasjonsnummer?) =
+                Optional.empty<Organisasjonsdetaljer>()
+    }
+
+    private class StubPdlOppslagGateway(private val users: Map<AktorId, Person> = emptyMap()) : PdlOppslagGateway {
+        override fun hentPerson(aktorId: AktorId?) = Optional.ofNullable(users[aktorId])
+
+        override fun hentIdenter(fnr: Foedselsnummer?): Identer {
+            TODO("Not yet implemented")
+        }
+
+        override fun hentIdenter(aktorId: AktorId?): Identer {
+            TODO("Not yet implemented")
+        }
+
     }
 
     private fun unleashServiceMedFeatures(vararg aktiverteFeatures: String) =
