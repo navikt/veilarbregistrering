bf5892bb3f791e5bb2ef764260340d2bfbace535
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2020-11-25 13:17:40 +0100

    Rett opp gauge-metric for telling av registrering_tilstand-statuser
    Gauge-metrikk-instanser må tas vare på for å unngå GCing. Dersom de forsvinner ut av minnet, vil verdien alltid bli "NaN" i prometheus

--- src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/PubliseringMetrics.java
+++ src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/PubliseringMetrics.java
@@ -4,14 +4,24 @@ import io.micrometer.core.instrument.Tag;
 import no.nav.metrics.MetricsFactory;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PubliseringMetrics {
 
-    static void rapporterRegistreringStatusAntall(Status status, int antall) {
-        MetricsFactory.getMeterRegistry().gauge("veilarbregistrering_registrert_status",
-                Arrays.asList(Tag.of("status", status.name())),
-                antall
-        );
+    private static final Map<Status, AtomicInteger> verdier = new HashMap<>();
 
+    public static void rapporterRegistreringStatusAntall(Status status, int antall) {
+        AtomicInteger registrertAntall = verdier.computeIfAbsent(status, (s) -> {
+            AtomicInteger atomiskAntall = new AtomicInteger();
+            MetricsFactory.getMeterRegistry().gauge("veilarbregistrering_registrert_status",
+                    Arrays.asList(Tag.of("status", status.name())),
+                    atomiskAntall, AtomicInteger::get
+            );
+            return atomiskAntall;
+        });
+
+        registrertAntall.set(antall);
     }
 }
