dee281e82a7a15de5cae810f1121fc6b1c7836ff
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2020-12-01 08:30:26 +0100

    Test et par helligdager i 2021

--- src/test/kotlin/no/nav/fo/veilarbregistrering/oppgave/HelligdagerTest.kt
+++ src/test/kotlin/no/nav/fo/veilarbregistrering/oppgave/HelligdagerTest.kt
@@ -19,4 +19,19 @@ class HelligdagerTest {
     fun `vanlig dag som 6 april 2020 er ikke en helligdag`() {
         assertThat(Helligdager.erHelligdag(LocalDate.of(2020, 4, 6))).isFalse()
     }
+
+    @Test
+    fun `nasjonaldagen 17 mai 2021 er en helligdag`() {
+        assertThat(Helligdager.erHelligdag(LocalDate.of(2021, 5, 17))).isTrue()
+    }
+
+    @Test
+    fun `langfredag 10 april 2021 er en helligdag`() {
+        assertThat(Helligdager.erHelligdag(LocalDate.of(2021, 4, 2))).isTrue()
+    }
+
+    @Test
+    fun `vanlig dag som 6 april 2021 er ikke en helligdag`() {
+        assertThat(Helligdager.erHelligdag(LocalDate.of(2021, 4, 6))).isFalse()
+    }
 }
