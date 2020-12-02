bf85612ac2d5f3ba56167df60ce858788f0af7af
Terje Heen <terje.heen@nav.no>
2020-12-02 15:20:06 +0100

    Forsøker å rydde i junit4-kode ifm. DbIntTester

--- src/main/java/no/nav/fo/veilarbregistrering/db/DatabaseConfig.java
+++ src/main/java/no/nav/fo/veilarbregistrering/db/DatabaseConfig.java
@@ -3,7 +3,6 @@ package no.nav.fo.veilarbregistrering.db;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import no.nav.fo.veilarbregistrering.db.DataSourceHelsesjekk;
 import no.nav.sbl.jdbc.Database;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
