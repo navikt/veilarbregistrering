dae9f318a6012929d273bdbcfd684d8edd7864a8
Terje Heen <terje.heen@nav.no>
2021-01-28 15:48:59 +0100

    Konverterer til kotlin for AaregRestClient

--- src/test/java/no/nav/fo/veilarbregistrering/arbeidsforhold/adapter/StubAaregRestClient.java
+++ src/test/java/no/nav/fo/veilarbregistrering/arbeidsforhold/adapter/StubAaregRestClient.java
@@ -1,12 +1,15 @@
 package no.nav.fo.veilarbregistrering.arbeidsforhold.adapter;
 
+import no.nav.common.sts.SystemUserTokenProvider;
 import no.nav.fo.veilarbregistrering.FileToJson;
 import no.nav.fo.veilarbregistrering.bruker.Foedselsnummer;
+
+import static org.mockito.Mockito.mock;
 
 class StubAaregRestClient extends AaregRestClient {
 
     StubAaregRestClient() {
-        super(null, null);
+        super("/test.nav.no", mock(SystemUserTokenProvider.class));
     }
 
     @Override
