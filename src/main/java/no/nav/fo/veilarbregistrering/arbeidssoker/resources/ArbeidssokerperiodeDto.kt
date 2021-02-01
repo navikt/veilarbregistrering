9d2d63ad7f7356b375824423d8b9d902ce5075e3
Terje Heen <terje.heen@nav.no>
2021-02-01 14:39:16 +0100

    Setter tilOgMedDato til Optional i periodeobjektet

--- src/main/java/no/nav/fo/veilarbregistrering/arbeidssoker/resources/ArbeidssokerperiodeDto.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/arbeidssoker/resources/ArbeidssokerperiodeDto.kt
@@ -1,3 +1,3 @@
 package no.nav.fo.veilarbregistrering.arbeidssoker.resources
 
-data class ArbeidssokerperiodeDto(val fraOgMedDato: String, val tilOgMedDato: String)
+data class ArbeidssokerperiodeDto(val fraOgMedDato: String, val tilOgMedDato: String?)
