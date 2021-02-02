738ab2023990a49a9688330b9fbd4e73b9df8f8d
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-02-02 14:32:04 +0100

    La til config og test for helsesjekk/selfttest ved lokal kjøring

---
+++ src/test/java/no/nav/fo/veilarbregistrering/SpringContextTest.kt
@@ -0,0 +1,35 @@
+package no.nav.fo.veilarbregistrering
+
+import no.nav.fo.veilarbregistrering.config.ApplicationTestConfig
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+
+@SpringBootTest
+@EnableAutoConfiguration
+@AutoConfigureMockMvc
+@SpringJUnitConfig(classes = [ApplicationTestConfig::class])
+class SpringContextTest(
+    @Autowired private val context: ApplicationContext,
+    @Autowired private val mvc: MockMvc,
+) {
+
+    @Test
+    fun `spring context lastes uten feil`() {
+        Assertions.assertThat(context).isNotNull
+    }
+
+    @Test
+    fun `selftest svarer med html`() {
+        mvc.get("/internal/selftest") {}.andExpect {
+            status { isOk }
+        }
+    }
+}
--- src/test/java/no/nav/fo/veilarbregistrering/helsesjekk/HelsesjekkConfig.kt
+++ src/test/java/no/nav/fo/veilarbregistrering/helsesjekk/HelsesjekkConfig.kt
@@ -1,14 +1,32 @@
 package no.nav.fo.veilarbregistrering.helsesjekk
 
+import no.nav.common.health.HealthCheck
+import no.nav.common.health.HealthCheckResult
+import no.nav.common.health.selftest.SelfTestCheck
 import no.nav.common.health.selftest.SelfTestChecks
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.core.JdbcTemplate
 
 @Configuration
 class HelsesjekkConfig {
     @Bean
     fun selfTestChecks(
+        jdbcTemplate: JdbcTemplate,
     ): SelfTestChecks {
-        return SelfTestChecks(emptyList())
+        return SelfTestChecks(listOf(
+            SelfTestCheck("Databasesjekk", true, checkDbHealth(jdbcTemplate))
+        ))
+    }
+
+    private fun checkDbHealth(jdbcTemplate: JdbcTemplate): HealthCheck {
+        return HealthCheck {
+            try {
+                jdbcTemplate.queryForObject("SELECT 1 FROM DUAL", Long::class.java)
+                HealthCheckResult.healthy()
+            } catch (e: Exception) {
+                HealthCheckResult.unhealthy("Fikk ikke kontakt med databasen", e)
+            }
+        }
     }
 }
