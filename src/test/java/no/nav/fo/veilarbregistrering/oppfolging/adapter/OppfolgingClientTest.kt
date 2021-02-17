55ef89a8b503d05de898425eea8309abc2e9e8e2
Terje Heen <terje.heen@nav.no>
2021-02-17 01:18:30 +0100

    Rydder i OppfølgingClientTest
    Rydder og sletter utkommenterte tester. Sørger for at testene testes gjennom Client APIet og ikke via diverse Services på et mye høyere nivå.

--- src/test/java/no/nav/fo/veilarbregistrering/oppfolging/adapter/OppfolgingClientTest.kt
+++ src/test/java/no/nav/fo/veilarbregistrering/oppfolging/adapter/OppfolgingClientTest.kt
@@ -5,22 +5,14 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkStatic
-import no.nav.common.featuretoggle.UnleashService
 import no.nav.common.sts.SystemUserTokenProvider
 import no.nav.fo.veilarbregistrering.FileToJson
-import no.nav.fo.veilarbregistrering.autorisasjon.AutorisasjonService
-import no.nav.fo.veilarbregistrering.bruker.AktorId
-import no.nav.fo.veilarbregistrering.bruker.Bruker
 import no.nav.fo.veilarbregistrering.bruker.Foedselsnummer
 import no.nav.fo.veilarbregistrering.config.RequestContext
 import no.nav.fo.veilarbregistrering.metrics.InfluxMetricsService
 import no.nav.fo.veilarbregistrering.profilering.Innsatsgruppe
-import no.nav.fo.veilarbregistrering.profilering.Profilering
-import no.nav.fo.veilarbregistrering.profilering.ProfileringService
-import no.nav.fo.veilarbregistrering.registrering.bruker.*
-import no.nav.fo.veilarbregistrering.sykemelding.SykemeldingService
-import no.nav.fo.veilarbregistrering.sykemelding.adapter.SykemeldingGatewayImpl
-import no.nav.fo.veilarbregistrering.sykemelding.adapter.SykmeldtInfoClient
+import no.nav.fo.veilarbregistrering.registrering.bruker.AktiverBrukerException
+import no.nav.fo.veilarbregistrering.registrering.bruker.AktiverBrukerFeil
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.*
 import org.mockserver.integration.ClientAndServer
@@ -30,7 +22,6 @@ import org.mockserver.model.MediaType
 import javax.servlet.http.HttpServletRequest
 
 internal class OppfolgingClientTest {
-    private lateinit var inaktivBrukerService: InaktivBrukerService
     private lateinit var oppfolgingClient: OppfolgingClient
     private lateinit var mockServer: ClientAndServer
 
@@ -42,37 +33,8 @@ internal class OppfolgingClientTest {
     @BeforeEach
     fun setup() {
         mockServer = ClientAndServer.startClientAndServer(MOCKSERVER_PORT)
-        val brukerRegistreringRepository: BrukerRegistreringRepository = mockk(relaxed = true)
-        val sykeforloepMetadataClient: SykmeldtInfoClient = mockk()
-        val profileringService: ProfileringService = mockk()
-        val unleashService: UnleashService = mockk(relaxed = true)
-        val autorisasjonService: AutorisasjonService = mockk(relaxed = true)
         val influxMetricsService: InfluxMetricsService = mockk(relaxed = true)
         oppfolgingClient = buildClient(influxMetricsService, jacksonObjectMapper().findAndRegisterModules())
-
-        val oppfolgingGateway = OppfolgingGatewayImpl(oppfolgingClient)
-        val brukerTilstandService = BrukerTilstandService(
-            oppfolgingGateway,
-            SykemeldingService(SykemeldingGatewayImpl(sykeforloepMetadataClient), autorisasjonService, influxMetricsService),
-            unleashService
-        )
-        inaktivBrukerService = InaktivBrukerService(
-            brukerTilstandService,
-            brukerRegistreringRepository,
-            oppfolgingGateway
-        )
-        every {
-            profileringService.profilerBruker(
-                any(),
-                any(),
-                any()
-            )
-        } returns
-                Profilering()
-                    .setInnsatsgruppe(Innsatsgruppe.STANDARD_INNSATS)
-                    .setAlder(50)
-                    .setJobbetSammenhengendeSeksAvTolvSisteManeder(true)
-
     }
 
     private fun buildClient(influxMetricsService: InfluxMetricsService, findAndRegisterModules: ObjectMapper): OppfolgingClient {
@@ -125,7 +87,8 @@ internal class OppfolgingClientTest {
                 HttpResponse.response().withBody(settOppfolgingOgReaktivering(true, false), MediaType.JSON_UTF_8)
                     .withStatusCode(200)
             )
-        Assertions.assertThrows(RuntimeException::class.java) { inaktivBrukerService.reaktiverBruker(BRUKER) }
+        Assertions.assertThrows(RuntimeException::class.java) {
+            oppfolgingClient.reaktiverBruker(Foedselsnummer.of("10108000398")) }
     }
 
     @Test
@@ -138,7 +101,7 @@ internal class OppfolgingClientTest {
         mockServer.`when`(HttpRequest.request().withMethod("POST").withPath("/oppfolging/reaktiverbruker")).respond(
             HttpResponse.response().withStatusCode(204)
         )
-        inaktivBrukerService.reaktiverBruker(BRUKER)
+        oppfolgingClient.reaktiverBruker(Foedselsnummer.of("10108000398"))
     }
 
     @Test
@@ -165,45 +128,6 @@ internal class OppfolgingClientTest {
         assertThat(exception.aktiverBrukerFeil).isEqualTo(AktiverBrukerFeil.BRUKER_KAN_IKKE_REAKTIVERES)
     }
 
-    //FIXME: Vurder å flytte disse testene til en ny testklasse med mindre kompleksitet hvor vi tester Client eller
-    // Gateway, men ikke alt annet samtidig.
-    /*
-    @Test
-    fun testAtGirInternalServerErrorExceptionDersomBrukerIkkkeHarTilgangTilOppfolgingStatus() {
-        mockServer.when(request().withMethod("GET").withPath("/oppfolging")).respond(response().withStatusCode(401));
-        assertThrows(InternalServerErrorException.class, () -> brukerRegistreringService.hentStartRegistreringStatus(BRUKER, null));
-    }
-
-    @Test
-    fun testAtGirInternalServerErrorExceptionDersomOppfolgingFeiler() {
-        mockServer.when(request().withMethod("GET").withPath("/oppfolging")).respond(response().withStatusCode(500));
-        assertThrows(InternalServerErrorException.class, () -> brukerRegistreringService.hentStartRegistreringStatus(BRUKER, null));
-    }
-
-    @Test
-    public void testAtGirWebApplicationExceptionExceptionDersomIngenTilgang() {
-        mockServer.when(request().withMethod("GET").withPath("/oppfolging")).respond(response().withStatusCode(403));
-        assertThrows(WebApplicationException.class, () -> brukerRegistreringService.hentStartRegistreringStatus(BRUKER, null));
-    }
-
-    @Test
-    fun testAtGirIngenExceptionsDersomKun200OK() {
-        mockServer.when(request().withMethod("GET").withPath("/oppfolging"))
-                .respond(response().withBody(settOppfolgingOgReaktivering(true, false), MediaType.JSON_UTF_8).withStatusCode(200));
-
-        assertNotNull(brukerRegistreringService.hentStartRegistreringStatus(BRUKER));
-    }
-
-    @Test
-    fun testAtGirIngenExceptionsDersomKun200MedKanReaktiveresNull() {
-        mockServer.when(request().withMethod("GET").withPath("/oppfolging"))
-                .respond(response().withBody(settOppfolgingOgReaktivering(null, null), MediaType.JSON_UTF_8).withStatusCode(200));
-
-        every { arbeidsforholdGateway.hentArbeidsforhold(any()) } returns flereArbeidsforholdTilfeldigSortert());
-
-        assertNotNull(brukerRegistreringService.hentStartRegistreringStatus(BRUKER));
-    }
-*/
     private fun settOppfolgingOgReaktivering(oppfolging: Boolean, reaktivering: Boolean): String {
         return "{\"kanReaktiveres\": $reaktivering, \"underOppfolging\": $oppfolging}"
     }
@@ -219,8 +143,6 @@ internal class OppfolgingClientTest {
     companion object {
         private const val MOCKSERVER_URL = "localhost"
         private const val MOCKSERVER_PORT = 1084
-        private val IDENT = Foedselsnummer.of("10108000398") //Aremark fiktivt fnr.";
         private val FNR = Fnr("10108000398") //Aremark fiktivt fnr.";
-        private val BRUKER = Bruker.of(IDENT, AktorId.of("AKTØRID"))
     }
 }
