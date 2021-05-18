3aa23ad3a020555cf7d1f08cb8e7e4d72dca69c5
Terje Heen <terje.heen@nav.no>
2021-05-18 13:23:09 +0200

    Fjerner siste rest av SPERRET-typen
    Registreringstype; SPERRET, er ikke lenger i bruk, og også fjernet fra frontend. Vi tar derfor å fjerner verdien fra APIet vårt også.

--- src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/RegistreringType.java
+++ src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/RegistreringType.java
@@ -3,7 +3,7 @@ package no.nav.fo.veilarbregistrering.registrering.bruker;
 import no.nav.fo.veilarbregistrering.metrics.Metric;
 
 public enum RegistreringType implements Metric {
-    REAKTIVERING, SPERRET, ALLEREDE_REGISTRERT, SYKMELDT_REGISTRERING, ORDINAER_REGISTRERING;
+    REAKTIVERING, ALLEREDE_REGISTRERT, SYKMELDT_REGISTRERING, ORDINAER_REGISTRERING;
 
     @Override
     public String fieldName() {
