17fc96f72e6a53f244dfe368e3713490737b9baf
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-06-09 12:24:40 +0200

    F Egen bean for å bruke gammel abac i parallell

--- src/main/java/no/nav/fo/veilarbregistrering/autorisasjon/PepConfig.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/autorisasjon/PepConfig.kt
@@ -27,6 +27,19 @@ class PepConfig {
                 SpringAuditRequestInfoSupplier())
     }
 
+    @Bean
+    fun veilarbPepGammel(): Pep {
+
+        val username = getVaultSecret("serviceuser_creds/username")
+        val password = getVaultSecret("serviceuser_creds/password")
+
+        return VeilarbPepFactory.get(
+            EnvironmentUtils.getRequiredProperty(ABAC_URL_PROPERTY_OLD),
+            username,
+            password,
+            SpringAuditRequestInfoSupplier())
+    }
+
     private fun getVaultSecret(path: String): String? {
         return try {
             String(Files.readAllBytes(Paths.get(Application.SECRETS_PATH, path)), StandardCharsets.UTF_8)
@@ -37,5 +50,6 @@ class PepConfig {
 
     companion object {
         private const val ABAC_URL_PROPERTY = "ABAC_PDP_ENDPOINT_URL"
+        private const val ABAC_URL_PROPERTY_OLD = "ABAC_PDP_ENDPOINT_URL_OLD"
     }
 }
