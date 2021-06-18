1f882b64d5dd22b9f79e94f1b0a479d88924e6cc
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-06-18 14:49:22 +0200

    F!! oppgrader fra springfox 2.9.2 til 3.0.0

--- src/main/java/no/nav/fo/veilarbregistrering/config/SwaggerConfig.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/config/SwaggerConfig.kt
@@ -4,11 +4,9 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
-import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 
 @Configuration
-@EnableSwagger2
 open class SwaggerConfig {
     //  Path to Swagger UI: /veilarbregistrering/swagger-ui.html
     @Bean
