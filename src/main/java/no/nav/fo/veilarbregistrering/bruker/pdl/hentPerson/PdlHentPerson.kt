174a2c2538de262e008b95f1f3fd041801a092f6
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-07-15 12:44:52 +0200

    R!! konverterer PdlAdressebeskyttelse til Kotlin

--- src/main/java/no/nav/fo/veilarbregistrering/bruker/pdl/hentPerson/PdlHentPerson.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/bruker/pdl/hentPerson/PdlHentPerson.kt
@@ -44,3 +44,16 @@ data class PdlTelefonnummer(val nummer: String? = null,
 
 data class PdlFoedsel(val foedselsdato: LocalDate)
 
+enum class PdlGradering(internal val niva: Int) {
+    STRENGT_FORTROLIG_UTLAND(3),  // Tilsvarer paragraf 19 i Bisys (henvisning til Forvaltningslovens §19)
+    STRENGT_FORTROLIG(2),  // Tidligere spesregkode kode 6 fra TPS
+    FORTROLIG(1),  // Tidligere spesregkode kode 7 fra TPS
+    UGRADERT(0);
+}
+
+data class PdlAdressebeskyttelse(val gradering: PdlGradering) : Comparable<PdlAdressebeskyttelse> {
+    override operator fun compareTo(other: PdlAdressebeskyttelse): Int {
+        return other.gradering.niva - gradering.niva
+    }
+}
+
