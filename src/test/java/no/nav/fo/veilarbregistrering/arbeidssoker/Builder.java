1a3cea4ba50480daf909c21d3311670f32be8c3f
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-07-19 13:32:44 +0200

    t konverter Builder til Kotlin

--- src/test/java/no/nav/fo/veilarbregistrering/arbeidssoker/Builder.java
+++
@@ -1,6 +0,0 @@
-package no.nav.fo.veilarbregistrering.arbeidssoker;
-
-public interface Builder<T> {
-
-    T build();
-}
---
+++ src/test/java/no/nav/fo/veilarbregistrering/arbeidssoker/Builder.kt
@@ -0,0 +1,5 @@
+package no.nav.fo.veilarbregistrering.arbeidssoker
+
+interface Builder<T> {
+    fun build(): T
+}
