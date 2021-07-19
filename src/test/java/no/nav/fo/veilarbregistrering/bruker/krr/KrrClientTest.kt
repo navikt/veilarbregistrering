f7f557d69e6b46b7d2b87563c57f416831d406d7
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-07-19 09:15:22 +0200

    t konverter KrrClientTest til Kotlin

---
+++ src/test/java/no/nav/fo/veilarbregistrering/bruker/krr/KrrClientTest.kt
@@ -0,0 +1,36 @@
+package no.nav.fo.veilarbregistrering.bruker.krr
+
+import no.nav.fo.veilarbregistrering.FileToJson.toJson
+import no.nav.fo.veilarbregistrering.bruker.Foedselsnummer
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+import java.lang.RuntimeException
+
+class KrrClientTest {
+
+    @Test
+    fun `skal mappe kontaktinfo med mobiltelefonnummer`() {
+        val json = toJson(OK_JSON)
+        val foedselsnummer = Foedselsnummer.of("23067844532")
+        val kontaktinfoDto = KrrClient.parse(json, foedselsnummer)
+        assertThat(kontaktinfoDto).isNotEmpty
+        assertThat(kontaktinfoDto.get().mobiltelefonnummer).isEqualTo("11111111")
+    }
+
+    @Test
+    fun `skal mappe feil til runtimeException`() {
+        val json = toJson(FEIL_JSON)
+        val foedselsnummer = Foedselsnummer.of("23067844539")
+        val runtimeException = assertThrows(
+            RuntimeException::class.java
+        ) { KrrClient.parse(json, foedselsnummer) }
+        assertThat(runtimeException.message)
+            .isEqualTo("Henting av kontaktinfo fra KRR feilet: fant ikke person")
+    }
+
+    companion object {
+        private const val OK_JSON = "/krr/hentKontaktinformasjonOk.json"
+        private const val FEIL_JSON = "/krr/hentKontaktinformasjonError.json"
+    }
+}
