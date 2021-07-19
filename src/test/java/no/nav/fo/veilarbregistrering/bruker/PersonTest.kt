20409687ed3bb4d38b7ddd71a536f18f6f2daa3f
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-07-19 09:43:09 +0200

    t konverter PersonTest til Kotlin

---
+++ src/test/java/no/nav/fo/veilarbregistrering/bruker/PersonTest.kt
@@ -0,0 +1,26 @@
+package no.nav.fo.veilarbregistrering.bruker
+
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class PersonTest {
+
+    @Test
+    fun `skal ha adressebeskyttelse`() {
+        assertTrue(personMedAdressebeskyttelseGradering(AdressebeskyttelseGradering.FORTROLIG).harAdressebeskyttelse())
+        assertTrue(personMedAdressebeskyttelseGradering(AdressebeskyttelseGradering.STRENGT_FORTROLIG).harAdressebeskyttelse())
+        assertTrue(personMedAdressebeskyttelseGradering(AdressebeskyttelseGradering.STRENGT_FORTROLIG_UTLAND).harAdressebeskyttelse())
+    }
+
+    @Test
+    fun `skal ikke ha adressebeskyttelse`() {
+        assertFalse(personMedAdressebeskyttelseGradering(null).harAdressebeskyttelse())
+        assertFalse(personMedAdressebeskyttelseGradering(AdressebeskyttelseGradering.UKJENT).harAdressebeskyttelse())
+        assertFalse(personMedAdressebeskyttelseGradering(AdressebeskyttelseGradering.UGRADERT).harAdressebeskyttelse())
+    }
+
+    private fun personMedAdressebeskyttelseGradering(gradering: AdressebeskyttelseGradering?): Person {
+        return Person.of(null, null, gradering)
+    }
+}
