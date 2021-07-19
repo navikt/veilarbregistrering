c9fd27056fa464acc190c5ad2c63a18d0f319b45
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-07-19 13:13:55 +0200

    t konverter BesvarelseTestdataBuilder til Kotlin

---
+++ src/test/java/no/nav/fo/veilarbregistrering/besvarelse/BesvarelseTestdataBuilder.kt
@@ -0,0 +1,28 @@
+package no.nav.fo.veilarbregistrering.besvarelse
+
+object BesvarelseTestdataBuilder {
+    @JvmStatic
+    fun gyldigBesvarelse(): Besvarelse {
+        return Besvarelse()
+            .setDinSituasjon(DinSituasjonSvar.JOBB_OVER_2_AAR)
+            .setSisteStilling(SisteStillingSvar.HAR_HATT_JOBB)
+            .setUtdanning(UtdanningSvar.HOYERE_UTDANNING_5_ELLER_MER)
+            .setUtdanningGodkjent(UtdanningGodkjentSvar.JA)
+            .setUtdanningBestatt(UtdanningBestattSvar.JA)
+            .setHelseHinder(HelseHinderSvar.NEI)
+            .setAndreForhold(AndreForholdSvar.NEI)
+    }
+
+    fun gyldigSykmeldtSkalTilbakeSammeJobbBesvarelse(): Besvarelse {
+        return Besvarelse()
+            .setFremtidigSituasjon(FremtidigSituasjonSvar.SAMME_ARBEIDSGIVER)
+            .setTilbakeIArbeid(TilbakeIArbeidSvar.JA_FULL_STILLING)
+    }
+
+    @JvmStatic
+    fun gyldigBesvarelseUtenJobb(): Besvarelse {
+        return gyldigBesvarelse()
+            .setDinSituasjon(DinSituasjonSvar.ALDRI_HATT_JOBB)
+            .setSisteStilling(SisteStillingSvar.INGEN_SVAR)
+    }
+}
