7d219b9ea4fb6976b4a4d719bf5b0e4ee1681f35
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-07-19 09:40:49 +0200

    t konverter PeriodeTest til Kotlin

---
+++ src/test/java/no/nav/fo/veilarbregistrering/bruker/PeriodeTest.kt
@@ -0,0 +1,64 @@
+package no.nav.fo.veilarbregistrering.bruker
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
+class PeriodeTest {
+
+    @Test
+    fun `fraDatoAs yyyyMMdd skal skrive ut fradato pa formatet yyyyMMdd`() {
+        val periode = Periode.of(LocalDate.of(2020, 1, 12), LocalDate.of(2020, 2, 20))
+        assertThat(periode.fraDatoAs_yyyyMMdd()).isEqualTo("2020-01-12")
+    }
+
+    @Test
+    fun `tilDatoAs yyyyMMdd skal skrive ut tildato pa formatet yyyyMMdd`() {
+        val periode = Periode.of(LocalDate.of(2020, 1, 12), LocalDate.of(2020, 2, 20))
+        assertThat(periode.tilDatoAs_yyyyMMdd()).isEqualTo("2020-02-20")
+    }
+
+    @Test
+    fun `tildato er innenfor forespurt periode`() {
+        // [januar februar mars april]
+        // -------[februar mars april mai]
+        val periodeMedTildato = Periode.of(FØRSTE_JANUAR, FØRSTE_APRIL)
+        val forespurtPeriode = Periode.of(FØRSTE_FEBRUAR, FØRSTE_MAI)
+        assertThat(periodeMedTildato.overlapperMed(forespurtPeriode)).isTrue
+    }
+
+    @Test
+    fun `periode er avs`() {
+        val periodeMedTildato = Periode.of(FØRSTE_JANUAR, LocalDate.of(2020, 1, 31))
+        val forespurtPeriode = Periode.of(FØRSTE_FEBRUAR, FØRSTE_MAI)
+        assertThat(periodeMedTildato.overlapperMed(forespurtPeriode)).isFalse
+    }
+
+    @Test
+    fun `periode ar avs`() {
+        val periodeMedTildato = Periode.of(LocalDate.of(2020, 6, 1), LocalDate.of(2020, 8, 31))
+        val forespurtPeriode = Periode.of(FØRSTE_FEBRUAR, FØRSTE_MAI)
+        assertThat(periodeMedTildato.overlapperMed(forespurtPeriode)).isFalse
+    }
+
+    @Test
+    fun `forespurtPeriode med åpen tildato`() {
+        val periodeMedTildato = Periode.of(FØRSTE_JANUAR, FØRSTE_APRIL)
+        val forespurtPeriode = Periode.of(FØRSTE_FEBRUAR, null)
+        assertThat(periodeMedTildato.overlapperMed(forespurtPeriode)).isTrue
+    }
+
+    @Test
+    fun `periode med åpen tildato`() {
+        val periodeMedTildato = Periode.of(FØRSTE_JANUAR, null)
+        val forespurtPeriode = Periode.of(FØRSTE_FEBRUAR, FØRSTE_APRIL)
+        assertThat(periodeMedTildato.overlapperMed(forespurtPeriode)).isTrue
+    }
+
+    companion object {
+        private val FØRSTE_JANUAR = LocalDate.of(2020, 1, 1)
+        private val FØRSTE_FEBRUAR = LocalDate.of(2020, 2, 1)
+        private val FØRSTE_APRIL = LocalDate.of(2020, 4, 1)
+        private val FØRSTE_MAI = LocalDate.of(2020, 5, 1)
+    }
+}
