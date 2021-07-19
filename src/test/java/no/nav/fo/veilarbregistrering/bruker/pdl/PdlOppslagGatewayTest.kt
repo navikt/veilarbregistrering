0749526e990745d73f04eacfdaaa229f89071f52
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-07-19 09:09:23 +0200

    t slår sammen HentIdenterPdlOppslagGatewayTest og HentPersonPdlOppslagGatewayTest

--- src/test/java/no/nav/fo/veilarbregistrering/bruker/pdl/PdlOppslagGatewayTest.kt
+++ src/test/java/no/nav/fo/veilarbregistrering/bruker/pdl/PdlOppslagGatewayTest.kt
@@ -3,6 +3,7 @@ package no.nav.fo.veilarbregistrering.bruker.pdl
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import no.nav.fo.veilarbregistrering.bruker.AktorId
 import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.config.BeanDefinition
 import org.springframework.beans.factory.support.BeanDefinitionBuilder
@@ -14,10 +15,14 @@ import no.nav.fo.veilarbregistrering.bruker.pdl.hentIdenter.PdlIdenter
 import no.nav.fo.veilarbregistrering.bruker.Foedselsnummer
 import no.nav.fo.veilarbregistrering.bruker.pdl.hentIdenter.PdlIdent
 import no.nav.fo.veilarbregistrering.bruker.pdl.hentIdenter.PdlGruppe
+import no.nav.fo.veilarbregistrering.bruker.pdl.hentPerson.PdlFoedsel
+import no.nav.fo.veilarbregistrering.bruker.pdl.hentPerson.PdlPerson
+import no.nav.fo.veilarbregistrering.bruker.pdl.hentPerson.PdlTelefonnummer
 import no.nav.fo.veilarbregistrering.config.CacheConfig
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
 
-internal class HentIdenterPdlOppslagGatewayTest {
+internal class PdlOppslagGatewayTest {
 
     private lateinit var pdlOppslagClient: PdlOppslagClient
     private lateinit var context: AnnotationConfigApplicationContext
@@ -69,5 +74,33 @@ internal class HentIdenterPdlOppslagGatewayTest {
         val pdlIdenter = PdlIdenter()
         pdlIdenter.identer = listOf(pdlIdent)
         return pdlIdenter
+    }
+
+    @Test
+    fun `skal cache ved kall på samme aktor id`() {
+        val pdlOppslagGateway = context.getBean(PdlOppslagGateway::class.java)
+        every { pdlOppslagClient.hentPerson(any<AktorId>())} returns dummyPdlPerson()
+
+        pdlOppslagGateway.hentPerson(AktorId.of("22222222222"))
+        pdlOppslagGateway.hentPerson(AktorId.of("22222222222"))
+
+        verify(exactly = 1) { pdlOppslagClient.hentPerson(any<AktorId>()) }
+    }
+
+    @Test
+    fun `skal ikke cache ved kall på forskjellig aktor id`() {
+        val pdlOppslagGateway = context.getBean(PdlOppslagGateway::class.java)
+        every { pdlOppslagClient.hentPerson(any<AktorId>())} returns dummyPdlPerson()
+
+        pdlOppslagGateway.hentPerson(AktorId.of("12345678910"))
+        pdlOppslagGateway.hentPerson(AktorId.of("109987654321"))
+
+        verify(exactly = 2) { pdlOppslagClient.hentPerson(any<AktorId>()) }
+    }
+
+    private fun dummyPdlPerson(): PdlPerson {
+        val pdlFoedsel = PdlFoedsel(LocalDate.of(1970, 3, 23))
+        val pdlTelefonnummer = PdlTelefonnummer("94242425", "0047", 0)
+        return PdlPerson(listOf(pdlTelefonnummer), listOf(pdlFoedsel), emptyList())
     }
 }
