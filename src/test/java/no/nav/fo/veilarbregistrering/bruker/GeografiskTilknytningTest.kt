9de91d98cf4b386762b6749dafec74dbd9d319ba
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-07-19 09:35:17 +0200

    t konverter GeografiskTilknytningTest til Kotlin

---
+++ src/test/java/no/nav/fo/veilarbregistrering/bruker/GeografiskTilknytningTest.kt
@@ -0,0 +1,80 @@
+package no.nav.fo.veilarbregistrering.bruker
+
+import org.assertj.core.api.Assertions.assertThat
+import java.lang.NullPointerException
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
+
+class GeografiskTilknytningTest {
+
+    @Test
+    fun `geografiskTilknytning kan ikke vaere null`() {
+        val nullPointerException = assertThrows<NullPointerException> {
+            GeografiskTilknytning.of(null)
+        }
+        assertThat(nullPointerException.message)
+            .isEqualTo("Geografisk tilknytning kan ikke være null. Bruk <code>ofNullable</code> hvis du er usikker.")
+    }
+
+    @Test
+    fun `fieldName skal bruke bydel ikke oslo hvis geografiskTilknytning er bydel men ikke oslo`() {
+        assertThat(GeografiskTilknytning.of("123456").value())
+            .isEqualTo("bydelIkkeOslo")
+    }
+
+    @Test
+    fun `fieldName skal bruke bydel oslo med bydelskode hvis geografiskTilknytning er bydel i oslo`() {
+        assertThat(GeografiskTilknytning.of("030106").value())
+            .isEqualTo("bydelOsloUllern")
+    }
+
+    @Test
+    fun `fieldName skal bruke fylke hvis geografiskTilknytning består av fire siffer`() {
+        assertThat(GeografiskTilknytning.of("1234").value()).isEqualTo("kommune")
+    }
+
+    @Test
+    fun `fieldName skal bruke utland hvis geografiskTilknytning består av tre bokstaver`() {
+        assertThat(GeografiskTilknytning.of("ABC").value()).isEqualTo("utland")
+    }
+
+    @Test
+    fun `exception skal kastes hvis geografiskTilknytning er ukjent`() {
+        val illegalArgumentException = assertThrows<IllegalArgumentException> {
+            GeografiskTilknytning.of("12").value()
+        }
+        assertThat(illegalArgumentException.message)
+            .isEqualTo("Geografisk tilknytning har ukjent format: 12")
+    }
+
+    @Test
+    fun `bydel bjerke i oslo er ikke by med bydel`() {
+        val bjerke = GeografiskTilknytning.of(GeografiskTilknytning.BydelOslo.Bjerke.kode())
+        assertThat(bjerke.byMedBydeler()).isFalse
+    }
+
+    @Test
+    fun `oslo er by med bydeler`() {
+        val oslo = GeografiskTilknytning.of(GeografiskTilknytning.ByMedBydeler.Oslo.kode())
+        assertThat(oslo.byMedBydeler()).isTrue
+    }
+
+    @Test
+    fun `stavanger er by med bydeler`() {
+        val oslo = GeografiskTilknytning.of(GeografiskTilknytning.ByMedBydeler.Stavanger.kode())
+        assertThat(oslo.byMedBydeler()).isTrue
+    }
+
+    @Test
+    fun `bergen er by med bydeler`() {
+        val oslo = GeografiskTilknytning.of(GeografiskTilknytning.ByMedBydeler.Bergen.kode())
+        assertThat(oslo.byMedBydeler()).isTrue
+    }
+
+    @Test
+    fun `trondheim er by med bydeler`() {
+        val oslo = GeografiskTilknytning.of(GeografiskTilknytning.ByMedBydeler.Trondheim.kode())
+        assertThat(oslo.byMedBydeler()).isTrue
+    }
+}
