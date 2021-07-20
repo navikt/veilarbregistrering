653e7bfddf4b6abb95042dec4f5ae8334f35c45e
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-07-20 08:43:43 +0200

    t konverter UnleashConfig til Kotlin

---
+++ src/test/java/no/nav/fo/veilarbregistrering/config/UnleashConfig.kt
@@ -0,0 +1,12 @@
+package no.nav.fo.veilarbregistrering.config
+
+import io.mockk.mockk
+import no.nav.common.featuretoggle.UnleashClient
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class UnleashConfig {
+    @Bean
+    fun unleashClient(): UnleashClient = mockk()
+}
