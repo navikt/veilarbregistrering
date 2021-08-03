04daa3f50de71af556ea36be2777e98c862b6e7e
Michael Gundersen <21148282+theagilepadawan@users.noreply.github.com>
2021-08-03 10:43:52 +0200

    F!! Enkelt db-api for migrering ########72 characters################################################## # Arlo's Commit Notation: # # Low risk: # t	Test only # d	Developer documentation changes (not end-user facing) # dt	Documentation & Tests (Often used when you have sample snippets # a	Automated formatting / generation # r	Provable Refactoring # c	Comments (add/delete) # e	Environment (non-code) changes that don't affect program behavior # # May change behavior: # F	Feature (<= 8 LoC) # B	Bug (<= 8 LoC) # R	Test-supported Procedural Refactoring # # Danger zone! # F!!	Feature (> 8 LoC) # B!!	Bug (> 8 LoC) # R!!	Non-provable refactoring # ***	Does not compile intermediate step

---
+++ src/main/java/no/nav/fo/veilarbregistrering/db/migrering_postgres/MigreringRepositoryImpl.kt
@@ -0,0 +1,28 @@
+package no.nav.fo.veilarbregistrering.db.migrering_postgres
+
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+
+enum class TabellNavn(val idKolonneNavn: String) {
+    BRUKER_REGISTRERING("BRUKER_REGISTRERING_ID"),
+    BRUKER_PROFILERING("BRUKER_REGISTRERING_ID"),
+    BRUKER_REAKTIVERING("BRUKER_REAKTIVERING_ID"),
+    SYKMELDT_REGISTRERING("SYKMELDT_REGISTRERING_ID"),
+    MANUELL_REGISTRERING("MANUELL_REGISTRERING_ID"),
+    REGISTRERING_TILSTAND("ID"),
+    OPPGAVE("ID"),
+    FORMIDLINGSGRUPPE("ID"),
+    BEGRUNNELSE_KODEVERK("BEGRUNNELSE_ID")
+}
+
+class MigreringRepositoryImpl(private val db: NamedParameterJdbcTemplate) {
+
+    fun nesteFraTabell(tabellNavn: TabellNavn, id: Long): List<Map<String, Any>> {
+            val sql = "SELECT * " +
+                    "FROM ${tabellNavn.name} " +
+                    "WHERE ${tabellNavn.idKolonneNavn} > :id " +
+                    "ORDER BY ${tabellNavn.idKolonneNavn} " +
+                    "FETCH NEXT 1000 ROWS ONLY;"
+            return db.queryForList(sql, mapOf("id" to id))
+    }
+}
+
