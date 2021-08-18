f21ef1dbc188573bda6a08c8665af3e17426e202
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-08-18 12:21:50 +0200

    r Ubrukte beans

--- src/main/java/no/nav/fo/veilarbregistrering/metrics/MetricsConfig.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/metrics/MetricsConfig.kt
@@ -1,21 +1,12 @@
 package no.nav.fo.veilarbregistrering.metrics
 
 import io.micrometer.core.instrument.MeterRegistry
-import no.nav.common.metrics.InfluxClient
-import no.nav.common.metrics.MetricsClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 
 @Configuration
 class MetricsConfig {
-    @Bean
-    fun metricsClient(): MetricsClient =
-        InfluxClient()
-
-    @Bean
-    fun influxMetricsService(metricsClient: MetricsClient): InfluxMetricsService =
-        InfluxMetricsService(metricsClient)
 
     @Bean
     fun prometheusMetricsService(meterRegistry: MeterRegistry): PrometheusMetricsService =
--- src/test/java/no/nav/fo/veilarbregistrering/metrics/MetricsConfig.kt
+++ src/test/java/no/nav/fo/veilarbregistrering/metrics/MetricsConfig.kt
@@ -1,20 +1,11 @@
 package no.nav.fo.veilarbregistrering.metrics
 
 import io.micrometer.core.instrument.MeterRegistry
-import io.mockk.mockk
-import no.nav.common.metrics.MetricsClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class MetricsConfig {
-    @Bean
-    fun metricsClient(): MetricsClient = mockk(relaxed = true)
-
-    @Bean
-    fun influxMetricsService(metricsClient: MetricsClient): InfluxMetricsService {
-        return InfluxMetricsService(metricsClient)
-    }
 
     @Bean
     fun prometheusMetricsService(meterRegistry: MeterRegistry): PrometheusMetricsService {
