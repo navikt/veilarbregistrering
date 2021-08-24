8878cb9392facf6b0257e4f31ed7a76a91877f35
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-08-24 13:44:07 +0200

    Reverter forrige

---
+++ src/main/java/no/nav/fo/veilarbregistrering/oppfolging/Oppfolgingsstatus.java
@@ -0,0 +1,56 @@
+package no.nav.fo.veilarbregistrering.oppfolging;
+
+import no.nav.fo.veilarbregistrering.arbeidssoker.Formidlingsgruppe;
+
+import java.util.Optional;
+
+import static java.util.Optional.ofNullable;
+
+public class Oppfolgingsstatus {
+
+    private boolean underOppfolging;
+    private Boolean kanReaktiveres;
+    private Boolean erSykmeldtMedArbeidsgiver;
+    private Formidlingsgruppe formidlingsgruppe;
+    private Servicegruppe servicegruppe;
+    private Rettighetsgruppe rettighetsgruppe;
+
+    public Oppfolgingsstatus(
+            boolean underOppfolging,
+            Boolean kanReaktiveres,
+            Boolean erSykmeldtMedArbeidsgiver,
+            Formidlingsgruppe formidlingsgruppe,
+            Servicegruppe servicegruppe,
+            Rettighetsgruppe rettighetsgruppe) {
+        this.underOppfolging = underOppfolging;
+        this.kanReaktiveres = kanReaktiveres;
+        this.erSykmeldtMedArbeidsgiver = erSykmeldtMedArbeidsgiver;
+        this.formidlingsgruppe = formidlingsgruppe;
+        this.servicegruppe = servicegruppe;
+        this.rettighetsgruppe = rettighetsgruppe;
+    }
+
+    public Optional<Boolean> getKanReaktiveres() {
+        return ofNullable(kanReaktiveres);
+    }
+
+    public Optional<Boolean> getErSykmeldtMedArbeidsgiver() {
+        return ofNullable(erSykmeldtMedArbeidsgiver);
+    }
+
+    public boolean isUnderOppfolging() {
+        return underOppfolging;
+    }
+
+    public Optional<Formidlingsgruppe> getFormidlingsgruppe() {
+        return ofNullable(formidlingsgruppe);
+    }
+
+    public Optional<Servicegruppe> getServicegruppe() {
+        return ofNullable(servicegruppe);
+    }
+
+    public Optional<Rettighetsgruppe> getRettighetsgruppe() {
+        return ofNullable(rettighetsgruppe);
+    }
+}
