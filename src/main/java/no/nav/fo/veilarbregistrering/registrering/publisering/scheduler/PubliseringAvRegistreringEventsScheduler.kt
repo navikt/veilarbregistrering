a5d369922fdfe911cc721cfe385ebfa531193c58
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-08-27 13:46:25 +0200

    r to kotlin

---
+++ src/main/java/no/nav/fo/veilarbregistrering/registrering/publisering/scheduler/PubliseringAvRegistreringEventsScheduler.kt
@@ -0,0 +1,37 @@
+package no.nav.fo.veilarbregistrering.registrering.publisering.scheduler
+
+import no.nav.common.featuretoggle.UnleashClient
+import no.nav.common.job.leader_election.LeaderElectionClient
+import no.nav.fo.veilarbregistrering.log.CallId
+import no.nav.fo.veilarbregistrering.registrering.publisering.PubliseringAvEventsService
+import no.nav.fo.veilarbregistrering.registrering.publisering.scheduler.PubliseringAvRegistreringEventsScheduler
+import org.slf4j.LoggerFactory
+import org.slf4j.MDC
+import org.springframework.scheduling.annotation.Scheduled
+
+class PubliseringAvRegistreringEventsScheduler(
+    private val publiseringAvEventsService: PubliseringAvEventsService,
+    private val leaderElectionClient: LeaderElectionClient,
+    private val unleashClient: UnleashClient
+) {
+    @Scheduled(cron = "0/10 * * * * *")
+    fun publiserRegistreringEvents() {
+        try {
+            CallId.leggTilCallId()
+            if (unleashClient.isEnabled("veilarbregistrering.publiserRegistreringEvents.toggleOff")) {
+                LOG.info("publisering av event er disablet for namespace")
+                return
+            }
+            if (!leaderElectionClient.isLeader) {
+                return
+            }
+            publiseringAvEventsService.publiserEvents()
+        } finally {
+            MDC.clear()
+        }
+    }
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(PubliseringAvRegistreringEventsScheduler::class.java)
+    }
+}
