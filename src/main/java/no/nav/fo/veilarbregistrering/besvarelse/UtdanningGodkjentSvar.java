8487458443a92fb89914fb9c472b5f839cecf98a
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-09-21 12:53:42 +0200

    r til kotlin

--- src/main/java/no/nav/fo/veilarbregistrering/besvarelse/UtdanningGodkjentSvar.java
+++
@@ -1,8 +0,0 @@
-package no.nav.fo.veilarbregistrering.besvarelse;
-
-public enum UtdanningGodkjentSvar {
-    JA,
-    NEI,
-    VET_IKKE,
-    INGEN_SVAR,
-}
---
+++ src/main/java/no/nav/fo/veilarbregistrering/besvarelse/UtdanningGodkjentSvar.kt
@@ -0,0 +1,5 @@
+package no.nav.fo.veilarbregistrering.besvarelse
+
+enum class UtdanningGodkjentSvar {
+    JA, NEI, VET_IKKE, INGEN_SVAR
+}
