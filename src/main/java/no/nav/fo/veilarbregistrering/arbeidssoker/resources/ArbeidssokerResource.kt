6508fc610793c154f3c2766e3819e25ac396d70d
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-09-24 10:29:34 +0200

    r +kotlin -optional -stream

---
+++ src/main/java/no/nav/fo/veilarbregistrering/arbeidssoker/resources/ArbeidssokerResource.kt
@@ -0,0 +1,54 @@
+package no.nav.fo.veilarbregistrering.arbeidssoker.resources
+
+import no.nav.fo.veilarbregistrering.arbeidssoker.ArbeidssokerService
+import no.nav.fo.veilarbregistrering.arbeidssoker.Arbeidssokerperiode
+import no.nav.fo.veilarbregistrering.autorisasjon.AutorisasjonService
+import no.nav.fo.veilarbregistrering.bruker.Periode
+import no.nav.fo.veilarbregistrering.bruker.UserService
+import org.slf4j.LoggerFactory
+import org.springframework.format.annotation.DateTimeFormat
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
+
+@RestController
+@RequestMapping("/api/arbeidssoker")
+class ArbeidssokerResource(
+    private val arbeidssokerService: ArbeidssokerService,
+    private val userService: UserService,
+    private val autorisasjonService: AutorisasjonService
+) : ArbeidssokerApi {
+
+    @GetMapping("/perioder")
+    override fun hentArbeidssokerperioder(
+        @RequestParam("fnr") fnr: String,
+        @RequestParam("fraOgMed") @DateTimeFormat(pattern = "yyyy-MM-dd") fraOgMed: LocalDate,
+        @RequestParam(value = "tilOgMed", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") tilOgMed: LocalDate
+    ): ArbeidssokerperioderDto {
+        val bruker = userService.finnBrukerGjennomPdl()
+        autorisasjonService.sjekkLesetilgangTilBruker(bruker.gjeldendeFoedselsnummer)
+        val arbeidssokerperiodes = arbeidssokerService.hentArbeidssokerperioder(
+            bruker, Periode.gyldigPeriode(fraOgMed, tilOgMed)
+        )
+        LOG.info("Ferdig med henting av arbeidssokerperioder - fant ${arbeidssokerperiodes.asList().size} perioder")
+        return map(arbeidssokerperiodes.eldsteFoerst())
+    }
+
+    private fun map(arbeidssokerperioder: List<Arbeidssokerperiode>): ArbeidssokerperioderDto {
+        val arbeidssokerperiodeDtoer = arbeidssokerperioder
+            .map { periode: Arbeidssokerperiode ->
+                ArbeidssokerperiodeDto(
+                    periode.periode.fra.toString(),
+                    periode.periode.til?.toString(),
+                )
+            }
+
+        return ArbeidssokerperioderDto(arbeidssokerperiodeDtoer)
+    }
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(ArbeidssokerResource::class.java)
+    }
+}
