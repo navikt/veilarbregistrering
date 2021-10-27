e15cc83584ce3f233188ae11c0b9b3c05df4e572
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-10-27 11:08:36 +0200

    r litt mer idiomatisk logikk her

--- src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/resources/BrukerRegistreringWrapperFactory.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/resources/BrukerRegistreringWrapperFactory.kt
@@ -8,16 +8,21 @@ object BrukerRegistreringWrapperFactory {
     fun create(
         ordinaerBrukerRegistrering: OrdinaerBrukerRegistrering?,
         sykmeldtBrukerRegistrering: SykmeldtRegistrering?
-    ): BrukerRegistreringWrapper? {
-        if (ordinaerBrukerRegistrering == null && sykmeldtBrukerRegistrering == null) {
-            return null
+    ): BrukerRegistreringWrapper? =
+        when {
+            ordinaerBrukerRegistrering != null && sykmeldtBrukerRegistrering != null -> wrapNyesteRegistrering(
+                ordinaerBrukerRegistrering,
+                sykmeldtBrukerRegistrering
+            )
+            ordinaerBrukerRegistrering != null -> BrukerRegistreringWrapper(ordinaerBrukerRegistrering)
+            sykmeldtBrukerRegistrering != null -> BrukerRegistreringWrapper(sykmeldtBrukerRegistrering)
+            else -> null
         }
 
-        if (ordinaerBrukerRegistrering == null) {
-            return BrukerRegistreringWrapper(sykmeldtBrukerRegistrering!!)
-        } else if (sykmeldtBrukerRegistrering == null) {
-            return BrukerRegistreringWrapper(ordinaerBrukerRegistrering)
-        }
+    private fun wrapNyesteRegistrering(
+        ordinaerBrukerRegistrering: OrdinaerBrukerRegistrering,
+        sykmeldtBrukerRegistrering: SykmeldtRegistrering
+    ): BrukerRegistreringWrapper {
         val profilertBrukerRegistreringDato = ordinaerBrukerRegistrering.opprettetDato
         val sykmeldtRegistreringDato = sykmeldtBrukerRegistrering.opprettetDato
         return if (profilertBrukerRegistreringDato.isAfter(sykmeldtRegistreringDato)) {
