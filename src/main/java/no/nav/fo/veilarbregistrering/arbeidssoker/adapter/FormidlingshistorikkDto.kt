cc5b257ad17dd73217a38c603d5984d2dfe2d3ff
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-10-30 21:10:25 +0200

    Konverterer til kotlin data class

--- src/main/java/no/nav/fo/veilarbregistrering/arbeidssoker/adapter/FormidlingshistorikkDto.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/arbeidssoker/adapter/FormidlingshistorikkDto.kt
@@ -1,52 +1,10 @@
-package no.nav.fo.veilarbregistrering.arbeidssoker.adapter;
+package no.nav.fo.veilarbregistrering.arbeidssoker.adapter
 
-import java.time.LocalDate;
-import java.util.Objects;
+import java.time.LocalDate
 
-public class FormidlingshistorikkDto {
-
-    private final String formidlingsgruppeKode;
-    private final String modDato;
-    private final LocalDate fraDato;
-    private final LocalDate tilDato;
-
-
-    public FormidlingshistorikkDto(String formidlingsgruppeKode, String modDato, LocalDate fraDato, LocalDate tilDato) {
-        this.formidlingsgruppeKode = formidlingsgruppeKode;
-        this.modDato = modDato;
-        this.fraDato = fraDato;
-        this.tilDato = tilDato;
-    }
-
-    public String getFormidlingsgruppeKode() {
-        return formidlingsgruppeKode;
-    }
-
-    public String getModDato() {
-        return modDato;
-    }
-
-    public LocalDate getFraDato() {
-        return fraDato;
-    }
-
-    public LocalDate getTilDato() {
-        return tilDato;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        FormidlingshistorikkDto that = (FormidlingshistorikkDto) o;
-        return Objects.equals(formidlingsgruppeKode, that.formidlingsgruppeKode) &&
-                Objects.equals(modDato, that.modDato) &&
-                Objects.equals(fraDato, that.fraDato) &&
-                Objects.equals(tilDato, that.tilDato);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(formidlingsgruppeKode, modDato, fraDato, tilDato);
-    }
-}
+data class FormidlingshistorikkDto(
+    val formidlingsgruppeKode: String,
+    val modDato: String,
+    val fraDato: LocalDate,
+    val tilDato: LocalDate?
+)
