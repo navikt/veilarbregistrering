64ce72e6ef834d6e3e2226df6a141a7a37db3169
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-11-04 19:29:47 +0100

    r kaller krr med nytt token side om side med sts kall

--- src/main/java/no/nav/fo/veilarbregistrering/bruker/krr/KrrClient.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/bruker/krr/KrrClient.kt
@@ -23,17 +23,22 @@ class KrrClient internal constructor(
     private val tokenProvider: () -> String
 ) : HealthCheck {
     internal fun hentKontaktinfo(foedselsnummer: Foedselsnummer): KrrKontaktinfoDto? {
-        val request = Request.Builder()
-            .url(
-                HttpUrl.parse(baseUrl)!!.newBuilder()
-                    .addPathSegments("v1/personer/kontaktinformasjon")
-                    .addQueryParameter("inkluderSikkerDigitalPost", "false")
-                    .build()
-            )
-            .header(HttpHeaders.AUTHORIZATION, "Bearer " + systemUserTokenProvider.systemUserToken)
-            .header("Nav-Consumer-Id", "srvveilarbregistrering")
-            .header("Nav-Personidenter", foedselsnummer.stringValue())
-            .build()
+        val kontaktinfoPath = "v1/personer/kontaktinformasjon"
+        val request = buildRequest(kontaktinfoPath, foedselsnummer)
+        val aadRequest = buildAADRequest(kontaktinfoPath, foedselsnummer)
+
+        try {
+            RestClient.baseClient().newCall(aadRequest).execute().use { response ->
+                if (!response.isSuccessful || response.code() == HttpStatus.NOT_FOUND.value()) {
+                    LOG.warn("Fant ikke kontaktinfo på person i kontakt og reservasjonsregisteret med bruk av AADtoken")
+                }
+                val kontaktinfoDto = parse(RestUtils.getBodyStr(response).orElseThrow { RuntimeException() }, foedselsnummer)
+                LOG.info("Fant kontaktinfo i krr ved kall med AADtoken: {}", kontaktinfoDto)
+            }
+        } catch (e: IOException) {
+            LOG.warn("Feil oppsto ved kall mot krr ved bruk av AADtoken")
+        }
+
         try {
             RestClient.baseClient().newCall(request).execute().use { response ->
                 if (!response.isSuccessful || response.code() == HttpStatus.NOT_FOUND.value()) {
@@ -46,6 +51,35 @@ class KrrClient internal constructor(
             throw RuntimeException(e)
         }
     }
+
+    private fun buildRequest(path: String, foedselsnummer: Foedselsnummer): Request {
+        return Request.Builder()
+            .url(
+                HttpUrl.parse(baseUrl)!!.newBuilder()
+                    .addPathSegments(path)
+                    .addQueryParameter("inkluderSikkerDigitalPost", "false")
+                    .build()
+            )
+            .header(HttpHeaders.AUTHORIZATION, "Bearer " + systemUserTokenProvider.systemUserToken)
+            .header("Nav-Consumer-Id", "srvveilarbregistrering")
+            .header("Nav-Personidenter", foedselsnummer.stringValue())
+            .build()
+    }
+
+    private fun buildAADRequest(path: String, foedselsnummer: Foedselsnummer): Request {
+        return Request.Builder()
+            .url(
+                HttpUrl.parse(baseUrl)!!.newBuilder()
+                    .addPathSegments(path)
+                    .addQueryParameter("inkluderSikkerDigitalPost", "false")
+                    .build()
+            )
+            .header(HttpHeaders.AUTHORIZATION, "Bearer " + tokenProvider())
+            .header("Nav-Consumer-Id", "srvveilarbregistrering")
+            .header("Nav-Personidenter", foedselsnummer.stringValue())
+            .build()
+    }
+
 
     override fun checkHealth(): HealthCheckResult {
         return HealthCheckUtils.pingUrl(UrlUtils.joinPaths(baseUrl, "/ping"), RestClient.baseClient())
