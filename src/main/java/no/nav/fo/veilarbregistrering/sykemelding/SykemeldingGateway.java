d8c6f4216680b9798f24e956ad34de5c0d63f6b3
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-11-05 13:17:45 +0100

    r sletter gjenværende ubrukt kode

--- src/main/java/no/nav/fo/veilarbregistrering/sykemelding/SykemeldingGateway.java
+++
@@ -1,8 +0,0 @@
-package no.nav.fo.veilarbregistrering.sykemelding;
-
-import no.nav.fo.veilarbregistrering.bruker.Foedselsnummer;
-
-public interface SykemeldingGateway {
-
-    Maksdato hentReberegnetMaksdato(Foedselsnummer fnr);
-}
