28239dd4319cad0201ac074c6c941c3ca333364f
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-11-08 11:51:11 +0100

    f Fikser deprecated lowercase

--- src/main/java/no/nav/fo/veilarbregistrering/kafka/KafkaConfig.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/kafka/KafkaConfig.kt
@@ -131,7 +131,7 @@ class KafkaConfig {
 
         private val envSuffix: String =
             System.getenv("APP_ENVIRONMENT_NAME")?.let {
-                "-" + it.toLowerCase()
+                "-" + it.lowercase(Locale.getDefault())
             } ?: ""
 
     }
