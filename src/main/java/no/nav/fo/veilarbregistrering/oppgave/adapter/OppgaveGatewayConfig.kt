ee3d400c8c20d7f854774c2381dc1b2ec8e45826
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-11-08 10:16:22 +0100

    r oppgave config til kotlin

--- src/main/java/no/nav/fo/veilarbregistrering/oppgave/adapter/OppgaveGatewayConfig.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/oppgave/adapter/OppgaveGatewayConfig.kt
@@ -1,24 +1,21 @@
-package no.nav.fo.veilarbregistrering.oppgave.adapter;
+package no.nav.fo.veilarbregistrering.oppgave.adapter
 
-import no.nav.common.sts.SystemUserTokenProvider;
-import no.nav.fo.veilarbregistrering.oppgave.OppgaveGateway;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import static no.nav.common.utils.EnvironmentUtils.getRequiredProperty;
+import no.nav.common.sts.SystemUserTokenProvider
+import no.nav.common.utils.EnvironmentUtils
+import no.nav.fo.veilarbregistrering.oppgave.OppgaveGateway
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @Configuration
-public class OppgaveGatewayConfig {
-
-    public static final String OPPGAVE_PROPERTY_NAME = "OPPGAVE_URL";
+class OppgaveGatewayConfig {
+    @Bean
+    fun oppgaveRestClient(systemUserTokenProvider: SystemUserTokenProvider?): OppgaveRestClient =
+        OppgaveRestClient(EnvironmentUtils.getRequiredProperty(OPPGAVE_PROPERTY_NAME), systemUserTokenProvider!!)
 
     @Bean
-    OppgaveRestClient oppgaveRestClient(SystemUserTokenProvider systemUserTokenProvider) {
-        return new OppgaveRestClient(getRequiredProperty(OPPGAVE_PROPERTY_NAME), systemUserTokenProvider);
-    }
+    fun oppgaveGateway(oppgaveRestClient: OppgaveRestClient?): OppgaveGateway = OppgaveGatewayImpl(oppgaveRestClient!!)
 
-    @Bean
-    OppgaveGateway oppgaveGateway(OppgaveRestClient oppgaveRestClient) {
-        return new OppgaveGatewayImpl(oppgaveRestClient);
+    companion object {
+        const val OPPGAVE_PROPERTY_NAME = "OPPGAVE_URL"
     }
 }
