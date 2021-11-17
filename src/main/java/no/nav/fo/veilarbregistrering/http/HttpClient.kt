87d1b28fe4dd2ca3aaee61b474991db92b54705a
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-11-17 11:51:01 +0100

    r Legg til funksjon som tar inn konfigurasjonsblock til klienten

--- src/main/java/no/nav/fo/veilarbregistrering/http/HttpClient.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/http/HttpClient.kt
@@ -4,12 +4,16 @@ import no.nav.fo.veilarbregistrering.metrics.LogInterceptor
 import okhttp3.OkHttpClient
 import java.util.concurrent.TimeUnit
 
-fun defaultHttpClient(): OkHttpClient {
+fun defaultHttpClient(): OkHttpClient =
+    defaultHttpClientBuilder()
+        .build()
 
-    return OkHttpClient.Builder()
+fun buildHttpClient(block: OkHttpClient.Builder.() -> Unit): OkHttpClient =
+    defaultHttpClientBuilder().apply(block).build()
+
+private fun defaultHttpClientBuilder() =
+    OkHttpClient.Builder()
         .readTimeout(120L, TimeUnit.SECONDS)
         .addInterceptor(LogInterceptor())
         .connectTimeout(10, TimeUnit.SECONDS)
         .followRedirects(false)
-        .build()
-}
