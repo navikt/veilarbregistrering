41190266a28a707289ba849db28f0c4c0cccd845
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-11-18 00:18:16 +0100

    t Endrer EnhetGatewayTest til å bruke mockserver

--- src/test/java/no/nav/fo/veilarbregistrering/enhet/adapter/EnhetGatewayTest.kt
+++ src/test/java/no/nav/fo/veilarbregistrering/enhet/adapter/EnhetGatewayTest.kt
@@ -1,6 +1,6 @@
 package no.nav.fo.veilarbregistrering.enhet.adapter
 
-import no.nav.fo.veilarbregistrering.FileToJson.toJson
+import no.nav.fo.veilarbregistrering.FileToJson
 import no.nav.fo.veilarbregistrering.arbeidsforhold.Organisasjonsnummer
 import no.nav.fo.veilarbregistrering.arbeidsforhold.Organisasjonsnummer.Companion.of
 import no.nav.fo.veilarbregistrering.enhet.Kommunenummer
@@ -8,58 +8,75 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockserver.integration.ClientAndServer
+import org.mockserver.junit.jupiter.MockServerExtension
+import org.mockserver.model.HttpRequest
+import org.mockserver.model.HttpResponse
+import org.mockserver.model.MediaType
 
-class EnhetGatewayTest {
+@ExtendWith(MockServerExtension::class)
+class EnhetGatewayTest(private val mockServer: ClientAndServer) {
     private lateinit var enhetGateway: EnhetGatewayImpl
 
     @BeforeEach
     fun setUp() {
-        val enhetStubClient: EnhetRestClient = EnhetStubClient()
-        enhetGateway = EnhetGatewayImpl(enhetStubClient)
+        val baseUrl = "http://" + mockServer.remoteAddress().address.hostName + ":" + mockServer.remoteAddress().port
+        enhetGateway = EnhetGatewayImpl(EnhetRestClient(baseUrl))
     }
 
     @Test
     fun `hentOrganisasjonsdetaljer skal kunne hente ut kommunenummer fra enhetsregisteret`() {
-        val organisasjonsdetaljer = enhetGateway.hentOrganisasjonsdetaljer(of("995298775"))
+        val org = of("995298775")
+        mockServer.`when`(
+            HttpRequest
+                .request()
+                .withMethod("GET")
+                .withPath("/api/v1/organisasjon/${org.asString()}"))
+            .respond(
+                HttpResponse.response()
+                .withStatusCode(200)
+                .withBody(OK_JSON, MediaType.JSON_UTF_8))
+
+        val organisasjonsdetaljer = enhetGateway.hentOrganisasjonsdetaljer(org)
         assertThat(organisasjonsdetaljer).isNotNull
         assertThat(organisasjonsdetaljer!!.kommunenummer()).hasValue(Kommunenummer.of("0301"))
     }
 
     @Test
     fun `hentOrganisasjonsdetaljer skal gi empty result ved ukjent org nr`() {
-        val organisasjonsdetaljer = enhetGateway.hentOrganisasjonsdetaljer(of("123456789"))
+        val ukjentOrg = of("123456789")
+
+        mockServer.`when`(
+            HttpRequest
+                .request()
+                .withMethod("GET")
+                .withPath("/api/v1/organisasjon/${ukjentOrg.asString()}"))
+            .respond(
+                HttpResponse.response()
+                    .withStatusCode(404))
+
+        val organisasjonsdetaljer = enhetGateway.hentOrganisasjonsdetaljer(ukjentOrg)
         assertThat(organisasjonsdetaljer).isNull()
     }
 
     @Test
     fun `hentOrganisasjonsdetaljer skal kaste runtimeException ved feil`() {
+        mockServer.`when`(
+            HttpRequest
+                .request()
+                .withMethod("GET")
+                .withPath("/api/v1/organisasjon/${FEILENDE_ORG.asString()}"))
+            .respond(
+                HttpResponse.response()
+                    .withStatusCode(500))
+
         val runtimeException = assertThrows<RuntimeException> { enhetGateway.hentOrganisasjonsdetaljer(FEILENDE_ORG) }
-        assertThat(runtimeException.message).isEqualTo("Hent organisasjon feilet")
+        assertThat(runtimeException.message).isEqualTo("Hent organisasjon feilet med status: 500")
     }
 
     companion object {
+        private val OK_JSON = FileToJson.toJson("/enhet/enhet.json")
         private val FEILENDE_ORG: Organisasjonsnummer = of("0")
-    }
-
-    private class EnhetStubClient : EnhetRestClient("foo") {
-        private val jsonResponse: MutableMap<Organisasjonsnummer, String> = HashMap()
-
-        override fun hentOrganisasjon(organisasjonsnummer: Organisasjonsnummer): OrganisasjonDetaljerDto? {
-            if (organisasjonsnummer.asString() == "0") throw RuntimeException("Hent organisasjon feilet")
-            val jsonResponse = jsonResponse[organisasjonsnummer]
-            if (jsonResponse == null) {
-                println("Stub: Fant ikke organisasjonsnummer")
-                return null
-            }
-            return parse(jsonResponse).organisasjonDetaljer
-        }
-
-        companion object {
-            private const val OK_JSON = "/enhet/enhet.json"
-        }
-
-        init {
-            jsonResponse[of("995298775")] = toJson(OK_JSON)
-        }
     }
 }
