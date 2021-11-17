f54bfdaf3b156ee8b0cf4eb24d8dd86327283b33
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-11-17 09:14:46 +0100

    r OppgaveResource til kotlin

--- src/main/java/no/nav/fo/veilarbregistrering/oppgave/resources/OppgaveApi.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/oppgave/resources/OppgaveApi.kt
@@ -1,19 +1,18 @@
-package no.nav.fo.veilarbregistrering.oppgave.resources;
+package no.nav.fo.veilarbregistrering.oppgave.resources
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 
 @Tag(name = "OppgaveResource")
-public interface OppgaveApi {
-
+interface OppgaveApi {
     @Operation(summary = "Oppretter 'kontakt bruker'-oppgave på vegne av bruker.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Oppgave opprettet OK"),
-            @ApiResponse(responseCode = "403", description = "Duplikat - fant tilsvarende oppgave " +
-                    "som ble opprettet innenfor de siste 2 arbeidsdager"),
-            @ApiResponse(responseCode = "500", description = "Ukjent teknisk feil")
-    })
-    OppgaveDto opprettOppgave(OppgaveDto oppgaveDto);
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "Oppgave opprettet OK"), ApiResponse(
+            responseCode = "403", description = "Duplikat - fant tilsvarende oppgave " +
+                    "som ble opprettet innenfor de siste 2 arbeidsdager"
+        ), ApiResponse(responseCode = "500", description = "Ukjent teknisk feil")
+    )
+    fun opprettOppgave(oppgaveDto: OppgaveDto?): OppgaveDto
 }
--- src/main/java/no/nav/fo/veilarbregistrering/oppgave/resources/OppgaveResource.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/oppgave/resources/OppgaveResource.kt
@@ -1,47 +1,28 @@
-package no.nav.fo.veilarbregistrering.oppgave.resources;
+package no.nav.fo.veilarbregistrering.oppgave.resources
 
-import no.nav.fo.veilarbregistrering.autorisasjon.AutorisasjonService;
-import no.nav.fo.veilarbregistrering.bruker.Bruker;
-import no.nav.fo.veilarbregistrering.bruker.UserService;
-import no.nav.fo.veilarbregistrering.oppgave.OppgaveResponse;
-import no.nav.fo.veilarbregistrering.oppgave.OppgaveService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import static no.nav.fo.veilarbregistrering.oppgave.resources.OppgaveMapper.map;
-
+import no.nav.fo.veilarbregistrering.autorisasjon.AutorisasjonService
+import no.nav.fo.veilarbregistrering.bruker.UserService
+import no.nav.fo.veilarbregistrering.oppgave.OppgaveService
+import no.nav.fo.veilarbregistrering.oppgave.resources.OppgaveMapper.map
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/oppgave")
-public class OppgaveResource implements OppgaveApi {
+class OppgaveResource(
+    private val userService: UserService,
+    private val oppgaveService: OppgaveService,
+    private val autorisasjonService: AutorisasjonService
+) : OppgaveApi {
 
-    private final OppgaveService oppgaveService;
-    private final UserService userService;
-    private final AutorisasjonService autorisasjonService;
-
-    public OppgaveResource(
-            UserService userService,
-            OppgaveService oppgaveService,
-            AutorisasjonService autorisasjonService) {
-        this.userService = userService;
-        this.oppgaveService = oppgaveService;
-        this.autorisasjonService = autorisasjonService;
-    }
-
-    @Override
     @PostMapping
-    public OppgaveDto opprettOppgave(@RequestBody OppgaveDto oppgaveDto) {
-        if (oppgaveDto == null || oppgaveDto.getOppgaveType() == null) {
-            throw new IllegalArgumentException("Oppgave m/ type må være angitt");
-        }
-        final Bruker bruker = userService.finnBrukerGjennomPdl();
-
-        autorisasjonService.sjekkSkrivetilgangTilBruker(bruker.getGjeldendeFoedselsnummer());
-
-        OppgaveResponse oppgaveResponse = oppgaveService.opprettOppgave(bruker, oppgaveDto.getOppgaveType());
-
-        return map(oppgaveResponse, oppgaveDto.getOppgaveType());
+    override fun opprettOppgave(@RequestBody oppgaveDto: OppgaveDto?): OppgaveDto {
+        if (oppgaveDto?.oppgaveType == null) { throw IllegalArgumentException("Oppgave m/ type må være angitt") }
+        val bruker = userService.finnBrukerGjennomPdl()
+        autorisasjonService.sjekkSkrivetilgangTilBruker(bruker.gjeldendeFoedselsnummer)
+        val oppgaveResponse = oppgaveService.opprettOppgave(bruker, oppgaveDto.oppgaveType)
+        return map(oppgaveResponse, oppgaveDto.oppgaveType)
     }
 }
