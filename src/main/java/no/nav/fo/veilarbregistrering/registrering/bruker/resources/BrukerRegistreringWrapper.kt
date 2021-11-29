c48b174997a93f443bb690f6a51f55d3cfa291a3
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-11-29 12:55:03 +0100

    r organize imports

--- src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/resources/BrukerRegistreringWrapper.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/resources/BrukerRegistreringWrapper.kt
@@ -1,8 +1,7 @@
 package no.nav.fo.veilarbregistrering.registrering.bruker.resources
 
-import com.fasterxml.jackson.annotation.JsonProperty
-import no.nav.fo.veilarbregistrering.registrering.bruker.BrukerRegistrering
 import no.nav.fo.veilarbregistrering.registrering.BrukerRegistreringType
+import no.nav.fo.veilarbregistrering.registrering.bruker.BrukerRegistrering
 
 data class BrukerRegistreringWrapper(val registrering: BrukerRegistrering) {
     //@JsonProperty(value = "type", access = JsonProperty.Access.READ_ONLY)
--- src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/resources/RegistreringResource.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/registrering/bruker/resources/RegistreringResource.kt
@@ -9,7 +9,6 @@ import no.nav.fo.veilarbregistrering.registrering.bruker.resources.BrukerRegistr
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/api")
