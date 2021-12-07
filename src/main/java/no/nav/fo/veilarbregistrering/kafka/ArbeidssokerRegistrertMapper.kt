a005a466bf4dfb681b899a9f9c8efb2e50a9c308
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-12-07 14:51:00 +0100

    r til kotlin

--- src/main/java/no/nav/fo/veilarbregistrering/kafka/ArbeidssokerRegistrertMapper.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/kafka/ArbeidssokerRegistrertMapper.kt
@@ -1,82 +1,56 @@
-package no.nav.fo.veilarbregistrering.kafka;
+package no.nav.fo.veilarbregistrering.kafka
 
-import no.nav.arbeid.soker.registrering.ArbeidssokerRegistrertEvent;
-import no.nav.fo.veilarbregistrering.besvarelse.UtdanningSvar;
-import no.nav.fo.veilarbregistrering.registrering.publisering.ArbeidssokerRegistrertInternalEvent;
+import no.nav.arbeid.soker.registrering.ArbeidssokerRegistrertEvent
+import no.nav.fo.veilarbregistrering.besvarelse.UtdanningBestattSvar
+import no.nav.fo.veilarbregistrering.besvarelse.UtdanningGodkjentSvar
+import no.nav.fo.veilarbregistrering.besvarelse.UtdanningSvar
+import no.nav.fo.veilarbregistrering.registrering.publisering.ArbeidssokerRegistrertInternalEvent
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-
-import static java.time.format.DateTimeFormatter.ISO_ZONED_DATE_TIME;
-
-class ArbeidssokerRegistrertMapper {
-
-    static ArbeidssokerRegistrertEvent map(ArbeidssokerRegistrertInternalEvent event) {
-        return ArbeidssokerRegistrertEvent.newBuilder()
-                .setAktorid(event.getAktorId().asString())
-                .setBrukersSituasjon(event.getBrukersSituasjon()
-                        .map(Enum::toString)
-                        .orElse(null))
-                .setRegistreringOpprettet(
-                        ZonedDateTime.of(
-                                event.getOpprettetDato(),
-                                ZoneId.systemDefault()).format(ISO_ZONED_DATE_TIME))
-                .setUtdanning(event.getUtdanningSvar()
-                        .map(ArbeidssokerRegistrertMapper::map)
-                        .orElse(null))
-                .setUtdanningBestatt(event.getUtdanningBestattSvar()
-                        .map(ArbeidssokerRegistrertMapper::map)
-                        .orElse(null))
-                .setUtdanningGodkjent(event.getUtdanningGodkjentSvar()
-                        .map(ArbeidssokerRegistrertMapper::map)
-                        .orElse(null))
-                .build();
+internal object ArbeidssokerRegistrertMapper {
+    @JvmStatic
+    fun map(event: ArbeidssokerRegistrertInternalEvent): ArbeidssokerRegistrertEvent {
+        return ArbeidssokerRegistrertEvent(
+            event.aktorId.asString(),
+            event.brukersSituasjon?.name,
+            ZonedDateTime.of(
+                event.opprettetDato,
+                ZoneId.systemDefault()
+            ).format(DateTimeFormatter.ISO_ZONED_DATE_TIME),
+            event.utdanningSvar?.let(::map),
+            event.utdanningBestattSvar?.let(::map),
+            event.utdanningGodkjentSvar?.let(::map)
+        )
     }
 
-    private static no.nav.arbeid.soker.registrering.UtdanningGodkjentSvar map(no.nav.fo.veilarbregistrering.besvarelse.UtdanningGodkjentSvar utdanningGodkjentSvar) {
-        switch (utdanningGodkjentSvar) {
-            case JA:
-                return no.nav.arbeid.soker.registrering.UtdanningGodkjentSvar.JA;
-            case NEI:
-                return no.nav.arbeid.soker.registrering.UtdanningGodkjentSvar.NEI;
-            case INGEN_SVAR:
-                return no.nav.arbeid.soker.registrering.UtdanningGodkjentSvar.INGEN_SVAR;
-            case VET_IKKE:
-                return no.nav.arbeid.soker.registrering.UtdanningGodkjentSvar.VET_IKKE;
+    private fun map(utdanningGodkjentSvar: UtdanningGodkjentSvar): no.nav.arbeid.soker.registrering.UtdanningGodkjentSvar {
+        return when (utdanningGodkjentSvar) {
+            UtdanningGodkjentSvar.JA -> no.nav.arbeid.soker.registrering.UtdanningGodkjentSvar.JA
+            UtdanningGodkjentSvar.NEI -> no.nav.arbeid.soker.registrering.UtdanningGodkjentSvar.NEI
+            UtdanningGodkjentSvar.INGEN_SVAR -> no.nav.arbeid.soker.registrering.UtdanningGodkjentSvar.INGEN_SVAR
+            UtdanningGodkjentSvar.VET_IKKE -> no.nav.arbeid.soker.registrering.UtdanningGodkjentSvar.VET_IKKE
         }
-        throw new IllegalArgumentException(String.format("Ukjent svar valgt %s", utdanningGodkjentSvar));
     }
 
-    private static no.nav.arbeid.soker.registrering.UtdanningBestattSvar map(no.nav.fo.veilarbregistrering.besvarelse.UtdanningBestattSvar utdanningBestattSvar) {
-        switch (utdanningBestattSvar) {
-            case JA:
-                return no.nav.arbeid.soker.registrering.UtdanningBestattSvar.JA;
-            case NEI:
-                return no.nav.arbeid.soker.registrering.UtdanningBestattSvar.NEI;
-            case INGEN_SVAR:
-                return no.nav.arbeid.soker.registrering.UtdanningBestattSvar.INGEN_SVAR;
+    private fun map(utdanningBestattSvar: UtdanningBestattSvar): no.nav.arbeid.soker.registrering.UtdanningBestattSvar {
+        return when (utdanningBestattSvar) {
+            UtdanningBestattSvar.JA -> no.nav.arbeid.soker.registrering.UtdanningBestattSvar.JA
+            UtdanningBestattSvar.NEI -> no.nav.arbeid.soker.registrering.UtdanningBestattSvar.NEI
+            UtdanningBestattSvar.INGEN_SVAR -> no.nav.arbeid.soker.registrering.UtdanningBestattSvar.INGEN_SVAR
         }
-        throw new IllegalArgumentException(String.format("Ukjent svar valgt %s", utdanningBestattSvar));
     }
 
-    private static no.nav.arbeid.soker.registrering.UtdanningSvar map(UtdanningSvar utdanningSvar) {
-        switch (utdanningSvar) {
-
-            case INGEN_UTDANNING:
-                return no.nav.arbeid.soker.registrering.UtdanningSvar.INGEN_UTDANNING;
-            case GRUNNSKOLE:
-                return no.nav.arbeid.soker.registrering.UtdanningSvar.GRUNNSKOLE;
-            case VIDEREGAENDE_GRUNNUTDANNING:
-                return no.nav.arbeid.soker.registrering.UtdanningSvar.VIDEREGAENDE_GRUNNUTDANNING;
-            case VIDEREGAENDE_FAGBREV_SVENNEBREV:
-                return no.nav.arbeid.soker.registrering.UtdanningSvar.VIDEREGAENDE_FAGBREV_SVENNEBREV;
-            case HOYERE_UTDANNING_1_TIL_4:
-                return no.nav.arbeid.soker.registrering.UtdanningSvar.HOYERE_UTDANNING_1_TIL_4;
-            case HOYERE_UTDANNING_5_ELLER_MER:
-                return no.nav.arbeid.soker.registrering.UtdanningSvar.HOYERE_UTDANNING_5_ELLER_MER;
-            case INGEN_SVAR:
-                return no.nav.arbeid.soker.registrering.UtdanningSvar.INGEN_SVAR;
+    private fun map(utdanningSvar: UtdanningSvar): no.nav.arbeid.soker.registrering.UtdanningSvar {
+        return when (utdanningSvar) {
+            UtdanningSvar.INGEN_UTDANNING -> no.nav.arbeid.soker.registrering.UtdanningSvar.INGEN_UTDANNING
+            UtdanningSvar.GRUNNSKOLE -> no.nav.arbeid.soker.registrering.UtdanningSvar.GRUNNSKOLE
+            UtdanningSvar.VIDEREGAENDE_GRUNNUTDANNING -> no.nav.arbeid.soker.registrering.UtdanningSvar.VIDEREGAENDE_GRUNNUTDANNING
+            UtdanningSvar.VIDEREGAENDE_FAGBREV_SVENNEBREV -> no.nav.arbeid.soker.registrering.UtdanningSvar.VIDEREGAENDE_FAGBREV_SVENNEBREV
+            UtdanningSvar.HOYERE_UTDANNING_1_TIL_4 -> no.nav.arbeid.soker.registrering.UtdanningSvar.HOYERE_UTDANNING_1_TIL_4
+            UtdanningSvar.HOYERE_UTDANNING_5_ELLER_MER -> no.nav.arbeid.soker.registrering.UtdanningSvar.HOYERE_UTDANNING_5_ELLER_MER
+            UtdanningSvar.INGEN_SVAR -> no.nav.arbeid.soker.registrering.UtdanningSvar.INGEN_SVAR
         }
-        throw new IllegalArgumentException(String.format("Ukjent utdanning valgt %s", utdanningSvar));
     }
 }
--- src/main/java/no/nav/fo/veilarbregistrering/registrering/publisering/ArbeidssokerRegistrertInternalEvent.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/registrering/publisering/ArbeidssokerRegistrertInternalEvent.kt
@@ -1,21 +1,20 @@
 package no.nav.fo.veilarbregistrering.registrering.publisering
 
-import no.nav.fo.veilarbregistrering.besvarelse.*
+import no.nav.fo.veilarbregistrering.besvarelse.Besvarelse
 import no.nav.fo.veilarbregistrering.bruker.AktorId
 import java.time.LocalDateTime
-import java.util.*
 
 class ArbeidssokerRegistrertInternalEvent(
     val aktorId: AktorId,
     private val besvarelse: Besvarelse,
     val opprettetDato: LocalDateTime
 ) {
-    val brukersSituasjon: Optional<DinSituasjonSvar>
-        get() = Optional.ofNullable(besvarelse.dinSituasjon)
-    val utdanningSvar: Optional<UtdanningSvar>
-        get() = Optional.ofNullable(besvarelse.utdanning)
-    val utdanningBestattSvar: Optional<UtdanningBestattSvar>
-        get() = Optional.ofNullable(besvarelse.utdanningBestatt)
-    val utdanningGodkjentSvar: Optional<UtdanningGodkjentSvar>
-        get() = Optional.ofNullable(besvarelse.utdanningGodkjent)
+    val brukersSituasjon
+        get() = besvarelse.dinSituasjon
+    val utdanningSvar
+        get() = besvarelse.utdanning
+    val utdanningBestattSvar
+        get() = besvarelse.utdanningBestatt
+    val utdanningGodkjentSvar
+        get() = besvarelse.utdanningGodkjent
 }
