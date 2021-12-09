49717b068f5cb1031e3a351384580e8bea420823
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-12-09 20:43:04 +0100

    f Caching logger factory wrapper

--- src/main/java/no/nav/fo/veilarbregistrering/log/Logger.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/log/Logger.kt
@@ -1,7 +1,27 @@
 package no.nav.fo.veilarbregistrering.log
 
 import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.slf4j.LoggerFactory.getLogger
 
 inline fun <reified T:Any> loggerFor(): Logger =
     getLogger(T::class.java) ?: throw IllegalStateException("Error creating logger")
+
+inline val <reified T : Any> T.logger get() = getCachedLogger(T::class.java.name)
+
+fun getCachedLogger(loggerName: String): Logger {
+    return LoggerCache.getLogger(loggerName)
+}
+
+private object LoggerCache {
+    private val existingLoggers = mutableMapOf<String, Logger>()
+
+    fun getLogger(className: String): Logger {
+        return existingLoggers[className]
+            ?: run {
+                val classLogger = LoggerFactory.getLogger(className)
+                existingLoggers[className] = classLogger
+                classLogger
+            }
+    }
+}
