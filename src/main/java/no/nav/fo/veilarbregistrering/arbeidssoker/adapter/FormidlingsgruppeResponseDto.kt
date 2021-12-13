0f500a108b93b04e5bc564644f7a3cd003eef112
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-12-13 11:03:34 +0100

    b Fjerner felter som ikke brukes og kan være null

--- src/main/java/no/nav/fo/veilarbregistrering/arbeidssoker/adapter/FormidlingsgruppeResponseDto.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/arbeidssoker/adapter/FormidlingsgruppeResponseDto.kt
@@ -1,7 +1,5 @@
 package no.nav.fo.veilarbregistrering.arbeidssoker.adapter
 
 data class FormidlingsgruppeResponseDto(
-    val personId: String,
-    val fodselsnr: String,
     val formidlingshistorikk: List<FormidlingshistorikkDto>?
 )
