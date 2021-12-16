843ccd8dff56f21a16308e8701ed6a9859b0eb9b
Jørgen Tu Sveli <jorgen.tu.sveli@nav.no>
2021-12-16 22:41:58 +0100

    Forsøker å definere egen meterregistry bean

--- src/main/java/no/nav/fo/veilarbregistrering/metrics/MetricsConfig.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/metrics/MetricsConfig.kt
@@ -1,12 +1,19 @@
 package no.nav.fo.veilarbregistrering.metrics
 
 import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.prometheus.PrometheusConfig
+import io.micrometer.prometheus.PrometheusMeterRegistry
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 
 @Configuration
 class MetricsConfig {
+
+    @Bean
+    fun meterRegistry() {
+        PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+    }
 
     @Bean
     fun prometheusMetricsService(meterRegistry: MeterRegistry): PrometheusMetricsService =
