88d6c93c41e5e7f256e91ddd2cba6b4c74ee58e3
Jørgen Tu Sveli <jsveli@gmail.com>
2022-01-20 20:42:02 +0100

    refaktorert til kotlin

---
+++ src/main/java/no/nav/fo/veilarbregistrering/arbeidssoker/adapter/ArenaOrdsTokenProviderClient.kt
@@ -0,0 +1,95 @@
+package no.nav.fo.veilarbregistrering.arbeidssoker.adapter
+
+import com.fasterxml.jackson.annotation.JsonAlias
+import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.fo.veilarbregistrering.config.objectMapper
+import no.nav.fo.veilarbregistrering.config.requireProperty
+import no.nav.fo.veilarbregistrering.http.defaultHttpClient
+import okhttp3.MediaType
+import okhttp3.Request
+import okhttp3.RequestBody
+import org.springframework.http.HttpHeaders
+import java.io.IOException
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+import java.util.*
+
+class ArenaOrdsTokenProviderClient(
+    private val arenaOrdsUrl: String,
+) {
+
+    private var tokenCache: TokenCache? = null
+    val token: String
+        get() {
+            if (tokenIsSoonExpired()) {
+                tokenCache = TokenCache(getRefreshedToken())
+            }
+            return tokenCache!!.ordsToken.accessToken
+        }
+
+    private fun getRefreshedToken(): OrdsToken {
+        val request = Request.Builder()
+            .url(arenaOrdsUrl)
+            .header(HttpHeaders.CACHE_CONTROL, "no-cache")
+            .header(HttpHeaders.AUTHORIZATION, basicAuth)
+            .post(
+                RequestBody.create(
+                    MediaType.get("application/x-www-form-urlencoded"),
+                    "grant_type=client_credentials"
+                )
+            )
+            .build()
+        return try {
+            defaultHttpClient().newCall(request).execute().use { response ->
+                when {
+                    response.isSuccessful -> {
+                        response.body()?.string()
+                            ?.let { bodyString -> objectMapper.readValue<OrdsToken>(bodyString) }
+                            ?: throw IOException("Token response body was null")
+                    }
+                    else -> throw IOException("Unexpected response code from ords token refresh")
+                }
+            }
+        } catch (e: IOException) {
+            throw RuntimeException(e)
+        }
+    }
+
+    private fun tokenIsSoonExpired(): Boolean {
+        return tokenCache == null || timeToRefresh().isBefore(LocalDateTime.now())
+    }
+
+    private fun timeToRefresh(): LocalDateTime {
+        return tokenCache!!.time.plus(
+            (
+                    tokenCache!!.ordsToken.expiresIn - MINIMUM_TIME_TO_EXPIRE_BEFORE_REFRESH).toLong(),
+            ChronoUnit.SECONDS
+        )
+    }
+
+    private class TokenCache(val ordsToken: OrdsToken) {
+        val time: LocalDateTime = LocalDateTime.now()
+
+    }
+
+    data class OrdsToken(
+        @JsonAlias("access_token")
+        val accessToken: String,
+
+        @JsonAlias("token_type")
+        val tokenType: String,
+
+        @JsonAlias("expires_in")
+        val expiresIn: Int = 0
+    )
+
+    companion object {
+        private const val MINIMUM_TIME_TO_EXPIRE_BEFORE_REFRESH = 60
+
+        private const val ARENA_ORDS_CLIENT_ID_PROPERTY = "ARENA_ORDS_CLIENT_ID"
+        private const val ARENA_ORDS_CLIENT_SECRET_PROPERTY = "ARENA_ORDS_CLIENT_SECRET"
+        private val username = requireProperty(ARENA_ORDS_CLIENT_ID_PROPERTY)
+        private val password = requireProperty(ARENA_ORDS_CLIENT_SECRET_PROPERTY)
+        private val basicAuth = "Basic ${Base64.getEncoder().encodeToString("$username:$password".toByteArray())}"
+    }
+}
