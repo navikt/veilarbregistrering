b7d9fd22d0e04e5c8f1787b3101e69bf183f4210
Terje Heen <terje.heen@nav.no>
2022-01-25 21:18:31 +0100

    Endrer rekkefølge på metodene i FilterConfig
    Sorterer innholdet i klassen etter filterrekkefølgen. Filter nr 1 kommer først, så filter nr 2 osv.
    Private hjelpemetode følger umiddelbart under sitt tilhørende Filter.

--- src/main/java/no/nav/fo/veilarbregistrering/config/filters/FilterConfig.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/config/filters/FilterConfig.kt
@@ -7,7 +7,9 @@ import no.nav.common.auth.oidc.filter.OidcAuthenticator
 import no.nav.common.auth.oidc.filter.OidcAuthenticatorConfig
 import no.nav.common.log.LogFilter
 import no.nav.common.rest.filter.SetStandardHttpHeadersFilter
-import no.nav.fo.veilarbregistrering.config.*
+import no.nav.fo.veilarbregistrering.config.isDevelopment
+import no.nav.fo.veilarbregistrering.config.requireApplicationName
+import no.nav.fo.veilarbregistrering.config.requireProperty
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -15,55 +17,7 @@ import javax.servlet.Filter
 
 @Configuration
 class FilterConfig {
-
-    private fun createOpenAmAuthenticatorConfig(): OidcAuthenticatorConfig? {
-        val discoveryUrl = requireProperty("OPENAM_DISCOVERY_URL")
-        val clientId = requireProperty("VEILARBLOGIN_OPENAM_CLIENT_ID")
-        val refreshUrl = requireProperty("VEILARBLOGIN_OPENAM_REFRESH_URL")
-        return OidcAuthenticatorConfig()
-            .withDiscoveryUrl(discoveryUrl)
-            .withClientId(clientId)
-            .withRefreshUrl(refreshUrl)
-            .withRefreshTokenCookieName(Constants.REFRESH_TOKEN_COOKIE_NAME)
-            .withIdTokenCookieName(Constants.OPEN_AM_ID_TOKEN_COOKIE_NAME) //FIXME: Verifiser riktig bruk
-            .withUserRole(UserRole.INTERN)
-    }
-
-    private fun createVeilarbloginAADConfig(): OidcAuthenticatorConfig? {
-        val discoveryUrl = requireProperty("AAD_DISCOVERY_URL")
-        val clientId = requireProperty("VEILARBLOGIN_AAD_CLIENT_ID")
-        return OidcAuthenticatorConfig()
-            .withDiscoveryUrl(discoveryUrl)
-            .withClientId(clientId)
-            .withIdTokenCookieName(Constants.AZURE_AD_ID_TOKEN_COOKIE_NAME)
-            .withUserRole(UserRole.INTERN)
-    }
-
-    /*
-    * 24.01.2022 : This config should work for aad tokens obtained with both
-    * client-credentials- and on-behalf-of flow
-    */
-    private fun createAadTokenConfig(): OidcAuthenticatorConfig? {
-        val discoveryUrl = requireProperty("AZURE_APP_WELL_KNOWN_URL")
-        val allowedAudience =
-            requireProperty("AZURE_APP_CLIENT_ID")
-
-        return OidcAuthenticatorConfig()
-            .withDiscoveryUrl(discoveryUrl)
-            .withClientId(allowedAudience)
-            .withUserRoleResolver(AzureAdUserRoleResolver())
-    }
-
-    private fun createAzureAdB2CConfig(): OidcAuthenticatorConfig? {
-        val discoveryUrl = requireProperty("LOGINSERVICE_IDPORTEN_DISCOVERY_URL")
-        val clientId = requireProperty("LOGINSERVICE_IDPORTEN_AUDIENCE")
-        return OidcAuthenticatorConfig()
-            .withDiscoveryUrl(discoveryUrl)
-            .withClientId(clientId)
-            .withIdTokenCookieName(Constants.AZURE_AD_B2C_ID_TOKEN_COOKIE_NAME)
-            .withUserRole(UserRole.EKSTERN)
-    }
-
+    
     @Bean
     fun pingFilter(): FilterRegistrationBean<*>? {
         // Veilarbproxy trenger dette endepunktet for å sjekke at tjenesten lever
@@ -72,6 +26,18 @@ class FilterConfig {
         registration.filter = PingFilter()
         registration.order = 1
         registration.addUrlPatterns("/api/ping")
+        return registration
+    }
+
+    @Bean
+    fun logFilterRegistrationBean(): FilterRegistrationBean<*> {
+        val registration = FilterRegistrationBean<LogFilter>()
+        registration.filter = LogFilter(
+            requireApplicationName(),
+            isDevelopment()
+        )
+        registration.order = 2
+        registration.addUrlPatterns("/*")
         return registration
     }
 
@@ -101,19 +67,51 @@ class FilterConfig {
         return registration
     }
 
-    @Bean
-    fun logFilterRegistrationBean(): FilterRegistrationBean<*> {
-        val registration = FilterRegistrationBean<LogFilter>()
-        registration.filter = LogFilter(
-            requireApplicationName(),
-            isDevelopment()
-        )
-        registration.order = 2
-        registration.addUrlPatterns("/*")
-        return registration
+    private fun createOpenAmAuthenticatorConfig(): OidcAuthenticatorConfig? {
+        val discoveryUrl = requireProperty("OPENAM_DISCOVERY_URL")
+        val clientId = requireProperty("VEILARBLOGIN_OPENAM_CLIENT_ID")
+        val refreshUrl = requireProperty("VEILARBLOGIN_OPENAM_REFRESH_URL")
+        return OidcAuthenticatorConfig()
+            .withDiscoveryUrl(discoveryUrl)
+            .withClientId(clientId)
+            .withRefreshUrl(refreshUrl)
+            .withRefreshTokenCookieName(Constants.REFRESH_TOKEN_COOKIE_NAME)
+            .withIdTokenCookieName(Constants.OPEN_AM_ID_TOKEN_COOKIE_NAME) //FIXME: Verifiser riktig bruk
+            .withUserRole(UserRole.INTERN)
     }
 
+    private fun createVeilarbloginAADConfig(): OidcAuthenticatorConfig? {
+        val discoveryUrl = requireProperty("AAD_DISCOVERY_URL")
+        val clientId = requireProperty("VEILARBLOGIN_AAD_CLIENT_ID")
+        return OidcAuthenticatorConfig()
+            .withDiscoveryUrl(discoveryUrl)
+            .withClientId(clientId)
+            .withIdTokenCookieName(Constants.AZURE_AD_ID_TOKEN_COOKIE_NAME)
+            .withUserRole(UserRole.INTERN)
+    }
 
+    private fun createAzureAdB2CConfig(): OidcAuthenticatorConfig? {
+        val discoveryUrl = requireProperty("LOGINSERVICE_IDPORTEN_DISCOVERY_URL")
+        val clientId = requireProperty("LOGINSERVICE_IDPORTEN_AUDIENCE")
+        return OidcAuthenticatorConfig()
+            .withDiscoveryUrl(discoveryUrl)
+            .withClientId(clientId)
+            .withIdTokenCookieName(Constants.AZURE_AD_B2C_ID_TOKEN_COOKIE_NAME)
+            .withUserRole(UserRole.EKSTERN)
+    }
+
+    /**
+    * 24.01.2022 : This config should work for aad tokens obtained with both
+    * client-credentials- and on-behalf-of flow
+    */
+    private fun createAadTokenConfig(): OidcAuthenticatorConfig? {
+        val discoveryUrl = requireProperty("AZURE_APP_WELL_KNOWN_URL")
+        val allowedAudience = requireProperty("AZURE_APP_CLIENT_ID")
+        return OidcAuthenticatorConfig()
+            .withDiscoveryUrl(discoveryUrl)
+            .withClientId(allowedAudience)
+            .withUserRoleResolver(AzureAdUserRoleResolver())
+    }
 
     @Bean
     fun setStandardHeadersFilterRegistrationBean(): FilterRegistrationBean<*> {
