d287acf61a56f892a7d53f4f2075e8bb845aa1c5
Terje Heen <terje.heen@nav.no>
2022-01-31 12:50:08 +0100

    Flytter FnrUtils inn i Foedselsnummer
    Ønsker ikke å eksponere FnrUtils til omverden mer enn nødvendig. Pakker den derfor inn i Foedselsnummer, slik at den ikke blir liggende fritt.
    På sikt bør denne forsvinne helt, da alder bør hentes fra PDL og ikke utledes fra fødselsnummer.

--- src/main/java/no/nav/fo/veilarbregistrering/bruker/Foedselsnummer.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/bruker/Foedselsnummer.kt
@@ -1,7 +1,9 @@
 package no.nav.fo.veilarbregistrering.bruker
 
 
+import no.bekk.bekkopen.person.FodselsnummerValidator
 import java.time.LocalDate
+import java.time.Period
 
 data class Foedselsnummer(val foedselsnummer: String) {
 
@@ -13,7 +15,7 @@ data class Foedselsnummer(val foedselsnummer: String) {
         return foedselsnummer.replace("[0-9]{11}".toRegex(), "***********")
     }
 
-    fun alder(dato: LocalDate?): Int {
+    fun alder(dato: LocalDate): Int {
         return FnrUtils.utledAlderForFnr(foedselsnummer, dato)
     }
 
@@ -22,5 +24,24 @@ data class Foedselsnummer(val foedselsnummer: String) {
         fun of(foedselsnummer: String): Foedselsnummer {
             return Foedselsnummer(foedselsnummer)
         }
+    }
+}
+
+internal object FnrUtils {
+    fun utledAlderForFnr(fnr: String, dagensDato: LocalDate): Int {
+        return antallAarSidenDato(utledFodselsdatoForFnr(fnr), dagensDato)
+    }
+
+    fun utledFodselsdatoForFnr(fnr: String): LocalDate {
+        val fodselsnummer = FodselsnummerValidator.getFodselsnummer(fnr)
+        return LocalDate.of(
+            fodselsnummer.birthYear.toInt(),
+            fodselsnummer.month.toInt(),
+            fodselsnummer.dayInMonth.toInt()
+        )
+    }
+
+    fun antallAarSidenDato(dato: LocalDate?, dagensDato: LocalDate): Int {
+        return Period.between(dato, dagensDato).years
     }
 }
