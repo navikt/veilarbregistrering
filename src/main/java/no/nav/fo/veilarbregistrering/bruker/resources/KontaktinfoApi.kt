61b13c8add59c4d58dfa81217e48fc3440981f32
Terje Heen <terje.heen@nav.no>
2022-01-31 21:23:32 +0100

    Skriver om KontaktinfoApi og KontaktinfoResource til kotlin

--- src/main/java/no/nav/fo/veilarbregistrering/bruker/resources/KontaktinfoApi.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/bruker/resources/KontaktinfoApi.kt
@@ -1,18 +1,17 @@
-package no.nav.fo.veilarbregistrering.bruker.resources;
+package no.nav.fo.veilarbregistrering.bruker.resources
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 
 @Tag(name = "KontaktinfoResource")
-public interface KontaktinfoApi {
-
+interface KontaktinfoApi {
     @Operation(summary = "Henter kontaktinformasjon (telefonnummer) til bruker.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "403", description = "Ingen tilgang"),
-            @ApiResponse(responseCode = "404", description = "Ikke funnet"),
-            @ApiResponse(responseCode = "500", description = "Ukjent feil")
-    })
-    KontaktinfoDto hentKontaktinfo();
+    @ApiResponses(
+        ApiResponse(responseCode = "403", description = "Ingen tilgang"),
+        ApiResponse(responseCode = "404", description = "Ikke funnet"),
+        ApiResponse(responseCode = "500", description = "Ukjent feil")
+    )
+    fun hentKontaktinfo(): KontaktinfoDto?
 }
--- src/main/java/no/nav/fo/veilarbregistrering/bruker/resources/KontaktinfoResource.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/bruker/resources/KontaktinfoResource.kt
@@ -1,39 +1,26 @@
-package no.nav.fo.veilarbregistrering.bruker.resources;
+package no.nav.fo.veilarbregistrering.bruker.resources
 
-import no.nav.fo.veilarbregistrering.autorisasjon.AutorisasjonService;
-import no.nav.fo.veilarbregistrering.bruker.Bruker;
-import no.nav.fo.veilarbregistrering.bruker.Kontaktinfo;
-import no.nav.fo.veilarbregistrering.bruker.KontaktinfoService;
-import no.nav.fo.veilarbregistrering.bruker.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import no.nav.fo.veilarbregistrering.autorisasjon.AutorisasjonService
+import no.nav.fo.veilarbregistrering.bruker.KontaktinfoService
+import no.nav.fo.veilarbregistrering.bruker.UserService
+import no.nav.fo.veilarbregistrering.bruker.resources.KontaktinfoMapper.map
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/person")
-public class KontaktinfoResource implements KontaktinfoApi {
+class KontaktinfoResource(
+    private val userService: UserService,
+    private val kontaktinfoService: KontaktinfoService,
+    private val autorisasjonService: AutorisasjonService
+) : KontaktinfoApi {
 
-    private final KontaktinfoService kontaktinfoService;
-    private final UserService userService;
-    private final AutorisasjonService autorisasjonService;
-
-    public KontaktinfoResource(
-            UserService userService,
-            KontaktinfoService kontaktinfoService,
-            AutorisasjonService autorisasjonService) {
-        this.autorisasjonService = autorisasjonService;
-        this.userService = userService;
-        this.kontaktinfoService = kontaktinfoService;
-    }
-
-    @Override
     @GetMapping("/kontaktinfo")
-    public KontaktinfoDto hentKontaktinfo() {
-        final Bruker bruker = userService.finnBrukerGjennomPdl();
-
-        autorisasjonService.sjekkLesetilgangTilBruker(bruker.getGjeldendeFoedselsnummer());
-
-        Kontaktinfo kontaktinfo = kontaktinfoService.hentKontaktinfo(bruker);
-        return KontaktinfoMapper.map(kontaktinfo);
+    override fun hentKontaktinfo(): KontaktinfoDto? {
+        val bruker = userService.finnBrukerGjennomPdl()
+        autorisasjonService.sjekkLesetilgangTilBruker(bruker.gjeldendeFoedselsnummer)
+        val kontaktinfo = kontaktinfoService.hentKontaktinfo(bruker)
+        return map(kontaktinfo)
     }
 }
