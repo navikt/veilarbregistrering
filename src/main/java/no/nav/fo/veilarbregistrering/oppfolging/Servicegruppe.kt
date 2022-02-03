a604b4f1aac4eaa4576a2d3f33c2b179ad321cb5
Michael Gundersen <21148282+theagilepadawan@users.noreply.github.com>
2022-02-03 15:38:06 +0100

    Konverterer Servicegruppe til Kotlin

--- src/main/java/no/nav/fo/veilarbregistrering/oppfolging/Servicegruppe.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/oppfolging/Servicegruppe.kt
@@ -1,39 +1,14 @@
-package no.nav.fo.veilarbregistrering.oppfolging;
+package no.nav.fo.veilarbregistrering.oppfolging
 
-import java.util.Objects;
+data class Servicegruppe (val servicegruppe: String) {
 
-public class Servicegruppe {
-
-    private final String servicegruppe;
-
-    public static Servicegruppe of(String servicegruppe) {
-        return new Servicegruppe(servicegruppe);
+    fun stringValue(): String {
+        return servicegruppe
     }
 
-    private Servicegruppe(String servicegruppe) {
-        if (servicegruppe == null) {
-            throw new IllegalArgumentException("Servicegruppe skal ikke kunne være null. " +
-                    "Hvis null, kan NullableServicegruppe brukes i stedet.");
+    companion object {
+        fun of(servicegruppe: String): Servicegruppe {
+            return Servicegruppe(servicegruppe)
         }
-        this.servicegruppe = servicegruppe;
     }
-
-    public String stringValue() {
-        return servicegruppe;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Servicegruppe that = (Servicegruppe) o;
-        return Objects.equals(servicegruppe, that.servicegruppe);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(servicegruppe);
-    }
-
-
 }
