f56434ab0624c9a8858fe3d50cc8b81175583e84
Jørgen Tu Sveli <jhsveli@gmail.com>
2022-02-03 07:41:24 +0100

    Oppretter en klient for veilarbarena

---
+++ src/main/java/no/nav/fo/veilarbregistrering/oppfolging/adapter/veilarbarena/SammensattOppfolgingStatusException.kt
@@ -0,0 +1,3 @@
+package no.nav.fo.veilarbregistrering.oppfolging.adapter.veilarbarena
+
+class SammensattOppfolgingStatusException(s: String) : Throwable(s)
---
+++ src/main/java/no/nav/fo/veilarbregistrering/oppfolging/adapter/veilarbarena/VeilarbarenaClient.kt
@@ -0,0 +1,70 @@
+package no.nav.fo.veilarbregistrering.oppfolging.adapter.veilarbarena
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.common.health.HealthCheck
+import no.nav.common.health.HealthCheckResult
+import no.nav.common.health.HealthCheckUtils
+import no.nav.fo.veilarbregistrering.config.objectMapper
+import no.nav.fo.veilarbregistrering.http.defaultHttpClient
+import okhttp3.Request
+import org.springframework.http.HttpStatus
+import java.io.IOException
+
+class VeilarbarenaClient(
+    private val baseUrl: String,
+    private val tokenProvider: () -> String
+) : HealthCheck {
+    internal fun arenaStatus(): ArenaStatusDto {
+        val aadToken = tokenProvider()
+
+        val request = Request.Builder()
+            .url("$baseUrl/api/arena/status")
+            .header("Authorization", "Bearer $aadToken")
+            .build()
+
+        return try {
+            defaultHttpClient().newCall(request).execute().use { response ->
+                if (response.code() != HttpStatus.OK.value()) {
+                    throw SammensattOppfolgingStatusException("Henting av arena status for bruker feilet: " + response.code() + " - " + response)
+                } else {
+                    response.body()?.string()?.let { objectMapper.readValue(it) }
+                        ?: throw SammensattOppfolgingStatusException("Henting av arenastatus returnerte tom body")
+                }
+            }
+        }
+        catch (e: IOException) {
+            throw RuntimeException(e)
+        }
+    }
+
+    internal fun kanReaktiveres(): KanReaktiveresDto {
+        val aadToken = tokenProvider()
+
+        val request = Request.Builder()
+            .url("$baseUrl/api/arena/status")
+            .header("Authorization", "Bearer $aadToken")
+            .build()
+
+        return try {
+            defaultHttpClient().newCall(request).execute().use { response ->
+                if (response.code() != HttpStatus.OK.value()) {
+                    throw SammensattOppfolgingStatusException("Henting av arena status for bruker feilet: " + response.code() + " - " + response)
+                } else {
+                    response.body()?.string()?.let { objectMapper.readValue(it) }
+                        ?: throw SammensattOppfolgingStatusException("Henting av arenastatus returnerte tom body")
+                }
+            }
+        }
+        catch (e: IOException) {
+            throw RuntimeException(e)
+        }
+    }
+
+    override fun checkHealth(): HealthCheckResult {
+        return HealthCheckUtils.pingUrl(baseUrl, defaultHttpClient())
+    }
+
+}
+
+class ArenaStatusDto(val formidlingsgruppe: String, val kvalifiseringsgruppe: String, val rettighetsgruppe: String)
+class KanReaktiveresDto(val kanEnkeltReaktiveres: Boolean)
