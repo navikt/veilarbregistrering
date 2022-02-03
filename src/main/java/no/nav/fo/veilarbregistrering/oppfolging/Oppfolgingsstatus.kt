3f3d8f3c777340f53b8d5b36ff86ae288368daff
Terje Heen <terje.heen@nav.no>
2022-02-03 22:10:12 +0100

    Utbedrer toString for Servicegruppe

--- src/main/java/no/nav/fo/veilarbregistrering/oppfolging/Oppfolgingsstatus.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/oppfolging/Oppfolgingsstatus.kt
@@ -8,4 +8,15 @@ data class Oppfolgingsstatus(
     val erSykmeldtMedArbeidsgiver: Boolean? = null,
     val formidlingsgruppe: Formidlingsgruppe? = null,
     val servicegruppe: Servicegruppe? = null,
-    val rettighetsgruppe: Rettighetsgruppe? = null)
+    val rettighetsgruppe: Rettighetsgruppe? = null) {
+
+    override fun toString(): String {
+        return "Oppfolgingsstatus(" +
+                "isUnderOppfolging=$isUnderOppfolging, " +
+                "kanReaktiveres=$kanReaktiveres, " +
+                "erSykmeldtMedArbeidsgiver=$erSykmeldtMedArbeidsgiver, " +
+                "formidlingsgruppe=$formidlingsgruppe, " +
+                "${servicegruppe ?: "servicegruppe=null"}" +
+                "rettighetsgruppe=$rettighetsgruppe)"
+    }
+}
--- src/main/java/no/nav/fo/veilarbregistrering/oppfolging/Servicegruppe.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/oppfolging/Servicegruppe.kt
@@ -1,3 +1,8 @@
 package no.nav.fo.veilarbregistrering.oppfolging
 
-data class Servicegruppe (val servicegruppe: String)
+data class Servicegruppe (val servicegruppe: String) {
+
+    override fun toString(): String {
+        return "servicegruppe='$servicegruppe'"
+    }
+}
