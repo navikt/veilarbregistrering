afa0de8ef06b701f91c44e0fad80547847f93a8d
Michael Gundersen <21148282+theagilepadawan@users.noreply.github.com>
2022-02-04 09:51:18 +0100

    Skriver om RsEnhet til Kotlin

--- src/main/java/no/nav/fo/veilarbregistrering/orgenhet/adapter/RsEnhet.kt
+++ src/main/java/no/nav/fo/veilarbregistrering/orgenhet/adapter/RsEnhet.kt
@@ -1,26 +1,3 @@
-package no.nav.fo.veilarbregistrering.orgenhet.adapter;
+package no.nav.fo.veilarbregistrering.orgenhet.adapter
 
-public class RsEnhet {
-
-    private String enhetNr;
-    private String navn;
-
-    public RsEnhet() {
-    }
-
-    public String getEnhetNr() {
-        return enhetNr;
-    }
-
-    public void setEnhetNr(String enhetNr) {
-        this.enhetNr = enhetNr;
-    }
-
-    public String getNavn() {
-        return navn;
-    }
-
-    public void setNavn(String navn) {
-        this.navn = navn;
-    }
-}
+data class RsEnhet (val enhetNr: String, val navn: String)
