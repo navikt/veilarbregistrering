76175867f1e0483c15e4516978f19fa79fafd465
Martine Enger <marenger@student.matnat.uio.no>
2022-04-26 16:27:47 +0200

    Revert "Logg resultat av ByUserIdStrategy. Kombinasjon av ulike strategier gjøres ved OR og ikke AND som vi trodde, så vi gjør en liten kvalitetssikring for å se om vi kan bruke byUserId-strategien alene når vi skal lansere asr for veileder i GCP"
    This reverts commit a4929910f245fb6860287d1201b05b0c5919113d.

--- src/main/kotlin/no/nav/fo/veilarbregistrering/featuretoggle/ByUserIdStrategy.kt
+++ src/main/kotlin/no/nav/fo/veilarbregistrering/featuretoggle/ByUserIdStrategy.kt
@@ -2,7 +2,6 @@ package no.nav.fo.veilarbregistrering.featuretoggle
 
 import no.finn.unleash.strategy.Strategy
 import no.nav.common.auth.context.AuthContextHolder
-import org.slf4j.LoggerFactory
 
 class ByUserIdStrategy(private val authContextHolder: AuthContextHolder) : Strategy {
 
@@ -10,15 +9,9 @@ class ByUserIdStrategy(private val authContextHolder: AuthContextHolder) : Strat
 
     override fun isEnabled(parameters: MutableMap<String, String>): Boolean {
         val navIdentIKontekst: String = authContextHolder.navIdent.get().get()
-        val result = parameters["user"]
+        return parameters["user"]
             ?.split(',')
             ?.any { navIdentIKontekst == it }
             ?: false
-        LOG.info("ByUserId strategi er enabled: $result")
-        return result
-    }
-
-    companion object {
-        private val LOG = LoggerFactory.getLogger(this::class.java)
     }
 }
