2b931e2fb79947c107d12a4ee2edc21e2c5739cd
Terje Heen <terje.heen@nav.no>
2022-05-03 11:07:50 +0200

    Legger til logging av nav-ident ifm. feilsøking av ByUserIdStrategy

--- src/main/kotlin/no/nav/fo/veilarbregistrering/featuretoggle/ByUserIdStrategy.kt
+++ src/main/kotlin/no/nav/fo/veilarbregistrering/featuretoggle/ByUserIdStrategy.kt
@@ -2,6 +2,8 @@ package no.nav.fo.veilarbregistrering.featuretoggle
 
 import no.finn.unleash.strategy.Strategy
 import no.nav.common.auth.context.AuthContextHolder
+import no.nav.fo.veilarbregistrering.arbeidssoker.ArbeidssokerService
+import org.slf4j.LoggerFactory
 
 class ByUserIdStrategy(private val authContextHolder: AuthContextHolder) : Strategy {
 
@@ -9,9 +11,16 @@ class ByUserIdStrategy(private val authContextHolder: AuthContextHolder) : Strat
 
     override fun isEnabled(parameters: MutableMap<String, String>): Boolean {
         val navIdentIKontekst: String = authContextHolder.navIdent.get().get()
+
+        LOG.info("Nav Ident i kontekst: {}", navIdentIKontekst)
+
         return parameters["user"]
             ?.split(',')
             ?.any { navIdentIKontekst == it }
             ?: false
+    }
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(ByUserIdStrategy::class.java)
     }
 }
