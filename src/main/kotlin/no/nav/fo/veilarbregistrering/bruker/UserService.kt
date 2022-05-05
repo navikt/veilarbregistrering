5f46f2c6c1ef88aa389ed08fa96e6d427488d1f6
Martine Enger <marenger@student.matnat.uio.no>
2022-05-05 11:02:03 +0200

    Skill på intern og ekstern kontekst når vi slår opp bruker i PDL. Ved ekstern kontekst forsøker vi å hente FNR fra PID-claim, hvis ikke fra sub-claim. Ved intern kontekst henter vi FNR fra URL. Dry-run mot prod

--- src/main/kotlin/no/nav/fo/veilarbregistrering/bruker/UserService.kt
+++ src/main/kotlin/no/nav/fo/veilarbregistrering/bruker/UserService.kt
@@ -6,7 +6,6 @@ import no.nav.common.auth.context.AuthContextHolder
 import no.nav.fo.veilarbregistrering.bruker.Bruker.Companion.of
 import no.nav.fo.veilarbregistrering.bruker.feil.ManglendeBrukerInfoException
 import no.nav.fo.veilarbregistrering.config.RequestContext.servletRequest
-import no.nav.fo.veilarbregistrering.config.isDevelopment
 import no.nav.fo.veilarbregistrering.log.logger
 import org.springframework.stereotype.Service
 import java.util.*
@@ -27,7 +26,13 @@ class UserService(
         }
     }
 
-    fun finnBrukerGjennomPdl(): Bruker = finnBrukerGjennomPdl(hentFnrFraUrlEllerToken())
+    fun finnBrukerGjennomPdl(): Bruker {
+        return if (authContextHolder.erInternBruker() || authContextHolder.erSystemBruker()) {
+            finnBrukerGjennomPdl(hentFnrFraUrl())
+        } else {
+            finnBrukerGjennomPdl(hentFnrFraToken())
+        }
+    }
     fun finnBrukerGjennomPdl(fnr: Foedselsnummer): Bruker = map(pdlOppslagGateway.hentIdenter(fnr))
 
     fun hentBruker(aktorId: AktorId): Bruker = map(pdlOppslagGateway.hentIdenter(aktorId))
@@ -35,16 +40,26 @@ class UserService(
     fun getEnhetIdFromUrlOrThrow(): String =
         servletRequest().getParameter("enhetId") ?: throw ManglendeBrukerInfoException("Mangler enhetId")
 
-    private fun hentFnrFraUrlEllerToken(): Foedselsnummer {
+    private fun hentFnrFraUrl(): Foedselsnummer {
         val fnr: String =
             servletRequest().getParameter("fnr")
-            ?: authContextHolder.subject.orElseThrow { IllegalArgumentException() }
-        if (isDevelopment()) {
-            logger.info("PID i token: ${authContextHolder.hentFnrFraPid()}")
-            logger.info("Fødselsnummer hentet fra token eller URL: $fnr")
-        }
         if (!FodselsnummerValidator.isValid(fnr)) {
-            throw ManglendeBrukerInfoException("Fødselsnummer hentet fra URL eller subject i token er ikke gyldig. Kan ikke gjøre oppslag i PDL.")
+            throw ManglendeBrukerInfoException("Fødselsnummer hentet fra URL er ikke gyldig. Kan ikke gjøre oppslag i PDL.")
+        }
+        return Foedselsnummer(fnr)
+    }
+
+    private fun hentFnrFraToken(): Foedselsnummer {
+        val fnr: String = if (authContextHolder.hentFnrFraPid().isPresent) {
+            logger.info("Henter FNR fra PID-claim")
+            authContextHolder.hentFnrFraPid()
+        } else {
+            logger.info("Henter FNR fra subject-claim")
+            authContextHolder.hentFnrFraSubject()
+        }.orElseThrow { IllegalStateException("Fant ikke FNR hverken i PID-claim eller subject i token for ekstern bruker. Kan ikke gjøre oppslag i PDL") }
+
+        if (!FodselsnummerValidator.isValid(fnr)) {
+            throw ManglendeBrukerInfoException("Fødselsnummer hentet fra token er ikke gyldig. Kan ikke gjøre oppslag i PDL.")
         }
         return Foedselsnummer(fnr)
     }
@@ -62,4 +77,8 @@ class UserService(
 
 fun AuthContextHolder.hentFnrFraPid(): Optional<String> {
     return idTokenClaims.flatMap { getStringClaim(it, Constants.ID_PORTEN_PID_CLAIM) }
+}
+
+fun AuthContextHolder.hentFnrFraSubject(): Optional<String> {
+    return subject
 }
