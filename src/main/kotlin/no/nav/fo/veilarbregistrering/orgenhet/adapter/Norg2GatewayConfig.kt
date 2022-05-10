e0f377c5d677fafec2c6b92fc6484183e415fd05
Terje Heen <terje.heen@nav.no>
2022-05-10 22:42:14 +0200

    Retter feil ifm parsing av json fra hent alle enheter fra NORG2
    Parsing av Json feilet, og feilen ble ikke logget så den var ikke synlig.
    Feilen skyldes mest sannsynlig at vi brukte Kotlin i kombinasjon med RestUtils fra nav.common som gjorde at instansiering av DTOene våre feilet.

--- src/main/kotlin/no/nav/fo/veilarbregistrering/orgenhet/adapter/Norg2GatewayConfig.kt
+++ src/main/kotlin/no/nav/fo/veilarbregistrering/orgenhet/adapter/Norg2GatewayConfig.kt
@@ -1,5 +1,6 @@
 package no.nav.fo.veilarbregistrering.orgenhet.adapter
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import no.nav.fo.veilarbregistrering.config.requireProperty
 import no.nav.fo.veilarbregistrering.orgenhet.Norg2Gateway
 import org.springframework.context.annotation.Bean
@@ -7,9 +8,10 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class Norg2GatewayConfig {
+
     @Bean
-    fun norgRestClient(): Norg2RestClient {
-        return Norg2RestClient(requireProperty(NORG2_PROPERTY_NAME))
+    fun norgRestClient(objectMapper: ObjectMapper): Norg2RestClient {
+        return Norg2RestClient(requireProperty(NORG2_PROPERTY_NAME), objectMapper)
     }
 
     @Bean
