1e64f31bc53975da109a05d50a5211fe1617c864
Terje Heen <terje.heen@nav.no>
2022-06-20 16:10:00 +0200

    Fjerner gammel metode i FormidlingsgruppeRestClient
    Den gamle metoden er ikke lenger i bruk, etter at ny metode er togglet på.

--- src/main/kotlin/no/nav/fo/veilarbregistrering/arbeidssoker/adapter/FormidlingsgruppeRestClient.kt
+++ src/main/kotlin/no/nav/fo/veilarbregistrering/arbeidssoker/adapter/FormidlingsgruppeRestClient.kt
@@ -3,22 +3,18 @@ package no.nav.fo.veilarbregistrering.arbeidssoker.adapter
 import no.nav.common.health.HealthCheck
 import no.nav.common.health.HealthCheckResult
 import no.nav.common.health.HealthCheckUtils
-import no.nav.common.rest.client.RestUtils
 import no.nav.common.utils.UrlUtils
 import no.nav.fo.veilarbregistrering.bruker.Foedselsnummer
 import no.nav.fo.veilarbregistrering.bruker.Periode
 import no.nav.fo.veilarbregistrering.config.objectMapper
-import no.nav.fo.veilarbregistrering.config.parse
 import no.nav.fo.veilarbregistrering.http.buildHttpClient
 import no.nav.fo.veilarbregistrering.http.defaultHttpClient
 import no.nav.fo.veilarbregistrering.metrics.MetricsService
 import no.nav.fo.veilarbregistrering.metrics.TimedMetric
 import okhttp3.HttpUrl
 import okhttp3.Request
-import org.slf4j.LoggerFactory
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
-import java.io.IOException
 import java.util.concurrent.TimeUnit
 import java.util.function.Supplier
 
@@ -52,42 +48,6 @@ class FormidlingsgruppeRestClient internal constructor(
         }
     }
 
-    fun hentFormidlingshistorikk(
-        foedselsnummer: Foedselsnummer,
-        periode: Periode
-    ): FormidlingsgruppeResponseDto? {
-        return doTimedCall {
-            try {
-                val response = utfoerRequest(foedselsnummer, periode)
-
-                response?.let(::parse) ?: run {
-                    LOG.warn("Søk på fødselsnummer gav ingen treff i Arena")
-                    null
-                }
-            } catch (e: RuntimeException) {
-                throw RuntimeException("Hent formidlingshistorikk feilet", e)
-            }
-        }
-    }
-
-    private fun utfoerRequest(foedselsnummer: Foedselsnummer, periode: Periode): String? {
-        val request = buildRequest(foedselsnummer, periode)
-        val httpClient = buildHttpClient { readTimeout(HTTP_READ_TIMEOUT.toLong(), TimeUnit.MILLISECONDS) }
-
-        try {
-            httpClient.newCall(request).execute().use { response ->
-                return when {
-                    response.code() == HttpStatus.NOT_FOUND.value() -> null
-                    !response.isSuccessful -> throw RuntimeException("Feilkode: " + response.code())
-                    else -> RestUtils.getBodyStr(response)
-                        .orElseThrow { RuntimeException("Feil ved uthenting av response body") }
-                }
-            }
-        } catch (e: IOException) {
-            throw RuntimeException(e)
-        }
-    }
-
     private fun buildRequest(foedselsnummer: Foedselsnummer, periode: Periode): Request {
         return Request.Builder()
             .url(
@@ -108,7 +68,6 @@ class FormidlingsgruppeRestClient internal constructor(
 
     companion object {
         private const val HTTP_READ_TIMEOUT = 120000
-        private val LOG = LoggerFactory.getLogger(FormidlingsgruppeRestClient::class.java)
     }
 
     override fun value() = "arenaords"
