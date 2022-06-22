e87db0a74375d7b7356717922e86a32a2dcb524d
Ane Halse Sporastøyl <47738086+anehs@users.noreply.github.com>
2022-06-22 10:49:19 +0200

    Fjern mulighet for å velge formidlingsgruppe i testdatabuilder
    Arbeidssokerperioder skal kun være perioder hvor man er ARBS.
    På sikt ønsker vi å fjerne formidlingsgruppe-feltet, men fjerner i første omgang muligheten til å velge noe annet enn ARBS her.
    Andre grupper enn ARBS filtreres bort med en gang vi henter og mapper om data fra databasen, og det fins egne tester for dette.

--- src/test/kotlin/no/nav/fo/veilarbregistrering/arbeidssoker/ArbeidssokerperioderTest.kt
+++ src/test/kotlin/no/nav/fo/veilarbregistrering/arbeidssoker/ArbeidssokerperioderTest.kt
@@ -1,7 +1,6 @@
 package no.nav.fo.veilarbregistrering.arbeidssoker
 
-import no.nav.fo.veilarbregistrering.arbeidssoker.ArbeidssokerperiodeTestdataBuilder.Companion.medArbs
-import no.nav.fo.veilarbregistrering.arbeidssoker.ArbeidssokerperiodeTestdataBuilder.Companion.medIserv
+import no.nav.fo.veilarbregistrering.arbeidssoker.ArbeidssokerperiodeTestdataBuilder.Companion.arbeidssokerperiode
 import no.nav.fo.veilarbregistrering.arbeidssoker.ArbeidssokerperioderTestdataBuilder.Companion.arbeidssokerperioder
 import no.nav.fo.veilarbregistrering.bruker.Periode
 import org.assertj.core.api.Assertions.assertThat
@@ -69,26 +68,26 @@ class ArbeidssokerperioderTest {
     fun `gitt flere perioder, skal kun de som overlapper med forespurt periode returneres`() {
         val arbeidssokerperioder = arbeidssokerperioder()
             .periode(
-                medArbs()
+                arbeidssokerperiode()
                     .fra(LocalDate.of(2020, 1, 19))
                     .til(LocalDate.of(2020, 2, 20))
             )
             .periode(
-                medArbs()
+                arbeidssokerperiode()
                     .fra(LocalDate.of(2020, 3, 19))
                     .til(LocalDate.of(2020, 4, 20))
             )
             .periode(
-                medArbs()
+                arbeidssokerperiode()
                     .fra(LocalDate.of(2020, 4, 21))
                     .til(LocalDate.of(2020, 4, 29))
             )
             .periode(
-                medArbs()
+                arbeidssokerperiode()
                     .fra(LocalDate.of(2020, 4, 30))
             )
             .periode(
-                medArbs()
+                arbeidssokerperiode()
                     .fra(LocalDate.of(2020, 8, 30))
             )
             .build()
@@ -103,7 +102,7 @@ class ArbeidssokerperioderTest {
 
     companion object {
         private val ARBEIDSSOKERPERIODE_1 = Arbeidssokerperiode(
-            Formidlingsgruppe("ISERV"),
+            Formidlingsgruppe("ARBS"),
             Periode(LocalDate.of(2016, 9, 24), null)
         )
         private val ARBEIDSSOKERPERIODE_2 = Arbeidssokerperiode(
