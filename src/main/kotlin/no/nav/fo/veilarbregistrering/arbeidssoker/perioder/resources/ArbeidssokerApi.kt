da82e6e3fde6fad75ac5347d65e3bbb7f534961f
Terje Heen <terje.heen@nav.no>
2022-11-17 10:10:52 +0100

    r Sletter test-endepunkt for perioder
    Vi hadde et endepunkt /perioder/test som vi ikke lenger trenger. Sletter derfor dette.

--- src/main/kotlin/no/nav/fo/veilarbregistrering/arbeidssoker/perioder/resources/ArbeidssokerApi.kt
+++ src/main/kotlin/no/nav/fo/veilarbregistrering/arbeidssoker/perioder/resources/ArbeidssokerApi.kt
@@ -52,17 +52,4 @@ interface ArbeidssokerApi {
         @Parameter(required = true, description = "Fra og med dato") fraOgMed: LocalDate,
         @Parameter(description = "Til og med dato") tilOgMed: LocalDate?
     ): ArbeidssokerperioderDto
-
-    @Operation(summary = "Henter alle perioder hvor bruker er registrert som arbeidssøker.")
-    @ApiResponses(
-        ApiResponse(responseCode = "200", description = "Ok"),
-        ApiResponse(responseCode = "400", description = "Ugyldig periode - fra og med dato må være før til dato"),
-        ApiResponse(responseCode = "401", description = "Unauthorized - bruker er ikke autorisert"),
-        ApiResponse(responseCode = "403", description = "Forbidden - ingen tilgang"),
-        ApiResponse(responseCode = "500", description = "Ukjent feil")
-    )
-    fun hentArbeidssokerperioderTest(
-        @Parameter(required = true, description = "Fra og med dato") fraOgMed: LocalDate,
-        @Parameter(description = "Til og med dato") tilOgMed: LocalDate?
-    ): ArbeidssokerperioderDto
 }
