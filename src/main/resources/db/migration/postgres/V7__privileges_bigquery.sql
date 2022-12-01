b81a063c4f18bcaa902a86e0c094745430506d3e
Martine Enger <marenger@student.matnat.uio.no>
2022-12-01 10:24:30 +0100

    Gi dataprodukt_paw tilgang til å selecte fra meldekort-tabellen
    dataprodukt_paw er ikke en cloudsqliamuser, så den har ikke hatt tilgang til meldekort-tabellen selv om alle våre personlige brukere har hatt det. dataprodukt_paw er postgres-brukeren som brukes av BigQuery.

---
+++ src/main/resources/db/migration/postgres/V7__privileges_bigquery.sql
@@ -0,0 +1,2 @@
+GRANT SELECT ON public.meldekort TO dataprodukt_paw;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO dataprodukt_paw;
--- src/test/resources/db/migration/postgres/beforeMigrate.sql
+++ src/test/resources/db/migration/postgres/beforeMigrate.sql
@@ -9,5 +9,13 @@ BEGIN
 ELSE
 CREATE ROLE cloudsqliamuser LOGIN PASSWORD 'my_password';
 END IF;
+IF EXISTS (
+      SELECT FROM pg_catalog.pg_roles
+      WHERE  rolname = 'dataprodukt_paw') THEN
+
+      RAISE NOTICE 'Role "dataprodukt_paw" already exists. Skipping.';
+ELSE
+CREATE ROLE dataprodukt_paw LOGIN PASSWORD 'my_password';
+END IF;
 END
 $do$;
