467e6cd990d727aae0399676a028d868dcd0b832
Jonas Enge <jonas.maccyber.enge@nav.no>
2023-01-02 12:57:58 +0100

    r forsøker å sette funksjonen i ReaktiveringBrukerService til åpen
    Fikk `ava.lang.NullPointerException: Cannot invoke
    "no.nav.fo.veilarbregistrering.oppfolging.OppfolgingGateway.kanReaktiveres(no.nav.fo.veilarbregistrering.bruker.Foedselsnummer)"
    because "this.oppfolgingGateway" is null"` da vi kallet
    `/api/kan-reaktiveres`

--- src/main/kotlin/no/nav/fo/veilarbregistrering/registrering/reaktivering/ReaktiveringBrukerService.kt
+++ src/main/kotlin/no/nav/fo/veilarbregistrering/registrering/reaktivering/ReaktiveringBrukerService.kt
@@ -36,7 +36,7 @@ open class ReaktiveringBrukerService(
         metricsService.registrer(Events.REGISTRERING_FULLFORING_REGISTRERINGSTYPE, RegistreringType.REAKTIVERING)
     }
 
-    fun kanReaktiveres(bruker: Bruker): Boolean {
+    open fun kanReaktiveres(bruker: Bruker): Boolean {
         val kanReaktiveres = oppfolgingGateway.kanReaktiveres(bruker.gjeldendeFoedselsnummer)
         return kanReaktiveres ?: false
     }
