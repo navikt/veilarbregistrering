f5ab07e158d0b6879ca6bf8097968ef8815a3d35
Aurora Christine Hofman <aurora.christine.hofman@nav.no>
2023-01-24 14:55:48 +0100

    f skeduler populer aktor_id_cache i dev

--- src/main/kotlin/no/nav/fo/veilarbregistrering/aktorIdCache/PopulerAktorIdWorker.kt
+++ src/main/kotlin/no/nav/fo/veilarbregistrering/aktorIdCache/PopulerAktorIdWorker.kt
@@ -4,8 +4,10 @@ import no.nav.common.featuretoggle.UnleashClient
 import no.nav.fo.veilarbregistrering.arbeidssoker.formidlingsgruppe.FormidlingsgruppeRepository
 import no.nav.fo.veilarbregistrering.bruker.Foedselsnummer
 import no.nav.fo.veilarbregistrering.bruker.PdlOppslagGateway
+import no.nav.fo.veilarbregistrering.config.isProduction
 import no.nav.fo.veilarbregistrering.log.logger
 import no.nav.fo.veilarbregistrering.log.secureLogger
+import org.springframework.scheduling.annotation.Scheduled
 import java.time.LocalDateTime
 
 class PopulerAktorIdWorker(
@@ -14,18 +16,23 @@ class PopulerAktorIdWorker(
     val aktorIdCacheRepository: AktorIdCacheRepository,
     val unleashClient: UnleashClient
 ) {
+    @Scheduled(fixedDelay = Long.MAX_VALUE, initialDelay = 180000)
     fun populereAktorId() {
+        if (isProduction()) {
+            return
+        }
+        logger.info("Startet jobb for å populere aktor_id_cache")
         var foedselsnummer: List<Foedselsnummer> = formidlingsgruppeRepository.hentUnikeFoedselsnummer()
 
         var teller = 1
-        while(foedselsnummer.isNotEmpty() && unleashClient.isEnabled("veilarbregistrering.populere-aktorid")){
+        while (foedselsnummer.isNotEmpty() && unleashClient.isEnabled("veilarbregistrering.populere-aktorid")) {
             val foedselsnummerBolk = foedselsnummer.take(100)
             foedselsnummer = foedselsnummer.drop(100)
 
             val aktorIdFnrMap = pdlOppslagGateway.hentIdenterBolk(foedselsnummerBolk, true)
             val fnrUtenTreff = foedselsnummerBolk.subtract(aktorIdFnrMap.keys)
 
-            if (fnrUtenTreff.isNotEmpty()){
+            if (fnrUtenTreff.isNotEmpty()) {
                 secureLogger.warn("Aktor_id ikke funnet for foedselsnummer $fnrUtenTreff i bolk $teller")
             }
             if (aktorIdFnrMap.isEmpty()) {
@@ -35,7 +42,13 @@ class PopulerAktorIdWorker(
             }
 
             val oppdaterteRader =
-                aktorIdCacheRepository.lagreBolk(aktorIdFnrMap.map { AktorIdCache(it.key, it.value, LocalDateTime.now()) })
+                aktorIdCacheRepository.lagreBolk(aktorIdFnrMap.map {
+                    AktorIdCache(
+                        it.key,
+                        it.value,
+                        LocalDateTime.now()
+                    )
+                })
 
             logger.info("Oppdaterte $oppdaterteRader i jobb som populerer AktørId-cache for bolk nr $teller")
             teller += 1
