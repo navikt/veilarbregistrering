f0e2c21f48dd4c3daca98c842c36ef826d97078c
Martine Enger <marenger@student.matnat.uio.no>
2023-03-02 09:24:23 +0100

    f Legg på callId på batchjobb så vi kan tracke loggene

--- src/main/kotlin/no/nav/fo/veilarbregistrering/arbeidssoker/perioder/PopulerHistoriskePerioderScheduler.kt
+++ src/main/kotlin/no/nav/fo/veilarbregistrering/arbeidssoker/perioder/PopulerHistoriskePerioderScheduler.kt
@@ -7,8 +7,10 @@ import no.nav.fo.veilarbregistrering.arbeidssoker.formidlingsgruppe.Formidlingsg
 import no.nav.fo.veilarbregistrering.bruker.AktorId
 import no.nav.fo.veilarbregistrering.bruker.Bruker
 import no.nav.fo.veilarbregistrering.bruker.PdlOppslagGateway
+import no.nav.fo.veilarbregistrering.log.CallId
 import no.nav.fo.veilarbregistrering.log.logger
 import no.nav.fo.veilarbregistrering.log.secureLogger
+import org.slf4j.MDC
 import org.springframework.scheduling.annotation.Scheduled
 import java.time.LocalDateTime
 
@@ -23,52 +25,62 @@ class PopulerHistoriskePerioderScheduler(
 
     //@Scheduled(initialDelay = 180000, fixedDelay = Long.MAX_VALUE)
     fun populerHistoriskePerioder() {
-
-        if (!leaderElectionClient.isLeader) {
-            logger.info("Starter ikke jobb for å populere perioder, pod er ikke leader")
-            return
-        }
-
-        logger.info("Starter jobb for å populere arbeidssøkerperioder")
-        val alleFnr = formidlingsgruppeRepository.hentDistinkteFnrForArbeidssokere()
-        logger.info("Populerer perioder for ${alleFnr.size} distinkte fødselsnumre")
-
-        alleFnr.forEach {
-            if (!unleashClient.isEnabled("veilarbregistrering.populer-perioder")) {
-                logger.info("Toggle for jobb for populering er skrudd av - avbryter jobben")
+        try {
+            CallId.leggTilCallId()
+            if (!leaderElectionClient.isLeader) {
+                logger.info("Starter ikke jobb for å populere perioder, pod er ikke leader")
                 return
             }
-            val identer = try {
-                 pdlOppslagGateway.hentIdenter(fnr = it, erSystemKontekst = true)
-            } catch (e: Exception) {
-                logger.warn("Fant ikke identer i PDL for person")
-                secureLogger.warn("Fant ikke identer i PDL for person med fnr $it")
-                return@forEach
-            }
 
-            val aktorId = try {
-                identer.finnGjeldendeAktorId()
-            } catch (e: Exception) {
-                AktorId("1234")
-            }
-            val gjeldendeFnr = try {
-                identer.finnGjeldendeFnr()
-            } catch (e: Exception) {
-                it
-            }
+            logger.info("Starter jobb for å populere arbeidssøkerperioder")
+            val alleFnr = formidlingsgruppeRepository.hentDistinkteFnrForArbeidssokere()
+            logger.info("Populerer perioder for ${alleFnr.size} distinkte fødselsnumre")
 
-            val arbeidssoker = populerArbeidssokerperioderService.hentArbeidssøker(
-                Bruker(
-                    gjeldendeFnr,
-                    aktorId,
-                    identer.finnHistoriskeFoedselsnummer()
+            alleFnr.forEach {
+                if (!unleashClient.isEnabled("veilarbregistrering.populer-perioder")) {
+                    logger.info("Toggle for jobb for populering er skrudd av - avbryter jobben")
+                    return
+                }
+                val identer = try {
+                    pdlOppslagGateway.hentIdenter(fnr = it, erSystemKontekst = true)
+                } catch (e: Exception) {
+                    logger.warn("Fant ikke identer i PDL for person, går videre til neste person i jobben")
+                    secureLogger.warn("Fant ikke identer i PDL for person med fnr $it")
+                    return@forEach
+                }
+
+                val aktorId = try {
+                    identer.finnGjeldendeAktorId()
+                } catch (e: Exception) {
+                    AktorId("1234")
+                }
+                val gjeldendeFnr = try {
+                    identer.finnGjeldendeFnr()
+                } catch (e: Exception) {
+                    it
+                }
+
+                val arbeidssoker = populerArbeidssokerperioderService.hentArbeidssøker(
+                    Bruker(
+                        gjeldendeFnr,
+                        aktorId,
+                        identer.finnHistoriskeFoedselsnummer()
+                    )
                 )
-            )
 
-            arbeidssoker.allePerioder()
-                .filter { periode -> periode.fraDato.isBefore(LocalDateTime.of(2023, 3, 1, 13, 40, 12)) }
-                .forEach { periode -> arbeidssokerperiodeService.lagrePeriode(gjeldendeFnr, periode.fraDato, periode.tilDato) }
+                arbeidssoker.allePerioder()
+                    .filter { periode -> periode.fraDato.isBefore(LocalDateTime.of(2023, 3, 1, 13, 40, 12)) }
+                    .forEach { periode ->
+                        arbeidssokerperiodeService.lagrePeriode(
+                            gjeldendeFnr,
+                            periode.fraDato,
+                            periode.tilDato
+                        )
+                    }
+            }
+            logger.info("Fullført jobb for populering av perioder")
+        } finally {
+            MDC.clear()
         }
-        logger.info("Fullført jobb populering av perioder")
     }
 }
