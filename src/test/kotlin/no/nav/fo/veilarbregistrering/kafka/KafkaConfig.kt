3f123b4da9e8566929041607d743aad407e5533f
Jonas Enge <jonas.maccyber.enge@nav.no>
2023-03-22 14:10:14 +0100

    r legger til bean for testing av arbeidssokerRegistrertKafkaProducerv2

--- src/test/kotlin/no/nav/fo/veilarbregistrering/kafka/KafkaConfig.kt
+++ src/test/kotlin/no/nav/fo/veilarbregistrering/kafka/KafkaConfig.kt
@@ -3,6 +3,7 @@ package no.nav.fo.veilarbregistrering.kafka
 import io.mockk.mockk
 import no.nav.fo.veilarbregistrering.registrering.publisering.ArbeidssokerRegistrertProducer
 import no.nav.fo.veilarbregistrering.registrering.publisering.ArbeidssokerProfilertProducer
+import no.nav.fo.veilarbregistrering.registrering.publisering.ArbeidssokerRegistrertProducerV2
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -10,6 +11,9 @@ import org.springframework.context.annotation.Configuration
 class KafkaConfig {
     @Bean
     fun arbeidssokerRegistrertKafkaProducer(): ArbeidssokerRegistrertProducer = mockk()
+
+    @Bean
+    fun arbeidssokerRegistrertKafkaProducerv2(): ArbeidssokerRegistrertProducerV2 = mockk()
 
     @Bean
     fun arbeidssokerProfilertKafkaProducer(): ArbeidssokerProfilertProducer = mockk()
