26cbcd8fc333096341f55913b54b31a042e6afec
Martine Enger <marenger@student.matnat.uio.no>
2023-03-24 10:57:21 +0100

    r Fjern jobb, ferdig kjørt

--- src/main/kotlin/no/nav/fo/veilarbregistrering/config/ServiceBeansConfig.kt
+++ src/main/kotlin/no/nav/fo/veilarbregistrering/config/ServiceBeansConfig.kt
@@ -3,13 +3,11 @@ package no.nav.fo.veilarbregistrering.config
 import no.nav.common.auth.context.AuthContextHolder
 import no.nav.common.featuretoggle.UnleashClient
 import no.nav.common.health.selftest.SelfTestChecks
-import no.nav.common.job.leader_election.LeaderElectionClient
 import no.nav.fo.veilarbregistrering.aktorIdCache.AktorIdCacheService
 import no.nav.fo.veilarbregistrering.arbeidsforhold.ArbeidsforholdGateway
 import no.nav.fo.veilarbregistrering.arbeidsforhold.resources.ArbeidsforholdResource
 import no.nav.fo.veilarbregistrering.arbeidssoker.ArbeidssokerperiodeRepository
 import no.nav.fo.veilarbregistrering.arbeidssoker.ArbeidssokerperiodeService
-import no.nav.fo.veilarbregistrering.arbeidssoker.ReparerArenaDataScheduler
 import no.nav.fo.veilarbregistrering.arbeidssoker.formidlingsgruppe.FormidlingsgruppeMottakService
 import no.nav.fo.veilarbregistrering.arbeidssoker.formidlingsgruppe.FormidlingsgruppeRepository
 import no.nav.fo.veilarbregistrering.arbeidssoker.meldekort.MeldekortMottakService
@@ -440,15 +438,5 @@ class ServiceBeansConfig {
         userService: UserService
     ): ArbeidssokerperiodeService {
         return ArbeidssokerperiodeService(arbeidssokerperiodeRepository, userService)
-    }
-
-    @Bean
-    fun reparerArenaDataScheduler(
-        formidlingsgruppeRepository: FormidlingsgruppeRepository,
-        arbeidssokerperiodeRepository: ArbeidssokerperiodeRepository,
-        pdlOppslagGateway: PdlOppslagGateway,
-        leaderElectionClient: LeaderElectionClient
-    ): ReparerArenaDataScheduler {
-        return ReparerArenaDataScheduler(formidlingsgruppeRepository, arbeidssokerperiodeRepository, pdlOppslagGateway, leaderElectionClient)
     }
 }
